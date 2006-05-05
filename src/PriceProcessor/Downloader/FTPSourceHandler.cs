89fe78fa59c3b5336a6ed8fc53160bbdc949bca5
Morozov <Morozov@c0a36a64-a7cb-d643-ba0a-49e1e726fa57>
2006-05-05 07:13:00 +0000

    В качестве клиента используется FTP_Client от LumiSoft.NET.

--- src/PriceProcessor/Downloader/FTPSourceHandler.cs
+++ src/PriceProcessor/Downloader/FTPSourceHandler.cs
@@ -4,125 +4,73 @@ using System.Text;
 using System.Net;
 using System.IO;
 using System.Data;
+using LumiSoft.Net.FTP.Client;
 
 namespace Inforoom.Downloader
 {
     public class FTPSourceHandler : PathSourceHandler
     {
+        private FTP_Client m_pFtpClient = null;
+
         public FTPSourceHandler(string sourceType)
             : base(sourceType)
         { }
 
-        protected string[] GetFileList(string FTPDir, string FTPUser, string FTPPassword, string Mask)
-        {
-            FtpWebRequest request = (FtpWebRequest)FtpWebRequest.Create(FTPDir);
-            request.Method = WebRequestMethods.Ftp.ListDirectory;
-
-            if (!String.IsNullOrEmpty(FTPUser))
-                request.Credentials = new NetworkCredential(FTPUser, FTPPassword);
-
-            request.Proxy = null;
-
-            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
-
-            Stream responseStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(responseStream);
-            List<string> fl = new List<string>();
-            while (!reader.EndOfStream)
-            {
-                string input = reader.ReadLine();
-                if (WildcardsHlp.Matched(Mask, input))
-                    fl.Add(input);
-            }
-
-            reader.Close();
-            response.Close();
-
-            return fl.ToArray();
-        }
-
-
-        protected DateTime GetFileDateTime(string FTPDir, string FTPUser, string FTPPassword)
-        {
-            FtpWebRequest request = (FtpWebRequest)FtpWebRequest.Create(FTPDir);
-            request.Method = WebRequestMethods.Ftp.GetDateTimestamp;
-
-            if (!String.IsNullOrEmpty(FTPUser))
-                request.Credentials = new NetworkCredential(FTPUser, FTPPassword);
-
-            request.Proxy = null;
-
-            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
-            DateTime fdt = response.LastModified;
-
-            response.Close();
-
-            return fdt;
-        }
-
-        protected void GetFile(string FTPDir, string SaveFileName, string FTPUser, string FTPPassword)
-        {
-            FtpWebRequest request = (FtpWebRequest)FtpWebRequest.Create(FTPDir);
-            request.Method = WebRequestMethods.Ftp.DownloadFile;
-
-            if (!String.IsNullOrEmpty(FTPUser))
-                request.Credentials = new NetworkCredential(FTPUser, FTPPassword);
-
-            request.Proxy = null;
-
-            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
-            Stream responseStream = response.GetResponseStream();
-            using (FileStream fs = new FileStream(SaveFileName, FileMode.Create))
-            {
-                CopyStreams(responseStream, fs);
-                fs.Close();
-            }
-            response.Close();
-        }
-
-
         protected override void GetFileFromSource()
         {
             CurrFileName = String.Empty;
-            string PricePath = (string)dtSources.Rows[0][SourcesTable.colPricePath];
-            if (!PricePath.StartsWith(@"ftp://", StringComparison.OrdinalIgnoreCase))
-                PricePath = @"ftp://" + PricePath;
-            if (!PricePath.EndsWith(@"/"))
-                PricePath += @"/";
-            PricePath += (string)dtSources.Rows[0][SourcesTable.colFTPDir];
-            if (!PricePath.EndsWith(@"/"))
-                PricePath += @"/";
+            string FTPHost = (string)dtSources.Rows[0][SourcesTable.colPricePath];
+            if (FTPHost.StartsWith(@"ftp://", StringComparison.OrdinalIgnoreCase))
+                FTPHost = FTPHost.Substring(6);
+            if (FTPHost.EndsWith(@"/"))
+                FTPHost = FTPHost.Substring(0, FTPHost.Length-1);
+
+            string PricePath = (string)dtSources.Rows[0][SourcesTable.colFTPDir];
+            if (!PricePath.StartsWith(@"/", StringComparison.OrdinalIgnoreCase))
+                PricePath = @"/" + PricePath;
 
             string FTPFileName = String.Empty;
             string DownFileName = String.Empty;
-            string[] files;
+            string ShortFileName = String.Empty;
             try
             {
-                if (WildcardsHlp.IsWildcards((string)dtSources.Rows[0][SourcesTable.colPriceMask]))
-                {
-                    files = GetFileList(PricePath, dtSources.Rows[0][SourcesTable.colFTPLogin].ToString(), dtSources.Rows[0][SourcesTable.colFTPPassword].ToString(), dtSources.Rows[0][SourcesTable.colPriceMask].ToString());
-                }
-                else
-                    files = new string[] {(string)dtSources.Rows[0][SourcesTable.colPriceMask]};
+                m_pFtpClient = new FTP_Client();
+                m_pFtpClient.Connect(FTPHost, 21);
+                m_pFtpClient.Authenticate(dtSources.Rows[0][SourcesTable.colFTPLogin].ToString(), dtSources.Rows[0][SourcesTable.colFTPPassword].ToString());
+                m_pFtpClient.SetCurrentDir(PricePath);
+
+                DataSet dsEntries = m_pFtpClient.GetList();
 
                 DateTime pdt = ((MySql.Data.Types.MySqlDateTime)dtSources.Rows[0][SourcesTable.colPriceDateTime]).GetDateTime();
 
                 DateTime fdt = pdt;
 
-                foreach(string s in files)
+                foreach (DataRow drEnt in dsEntries.Tables["DirInfo"].Rows)
                 {
-                    DateTime ndt = GetFileDateTime(PricePath + s, dtSources.Rows[0][SourcesTable.colFTPLogin].ToString(), dtSources.Rows[0][SourcesTable.colFTPPassword].ToString());
-                    if (ndt.CompareTo(fdt) > 0)
+                    if (!Convert.ToBoolean(drEnt["IsDirectory"]))
                     {
-                        fdt = ndt;
-                        FTPFileName = s;
+                        ShortFileName = drEnt["Name"].ToString();
+                        if ((WildcardsHlp.IsWildcards((string)dtSources.Rows[0][SourcesTable.colPriceMask]) && WildcardsHlp.Matched((string)dtSources.Rows[0][SourcesTable.colPriceMask], ShortFileName)) ||
+                            (String.Compare(ShortFileName, (string)dtSources.Rows[0][SourcesTable.colPriceMask], true) == 0))
+                        {
+                            DateTime ndt = Convert.ToDateTime(drEnt["Date"]);
+                            if (ndt.CompareTo(fdt) > 0)
+                            {
+                                fdt = ndt;
+                                FTPFileName = ShortFileName;
+                            }
+                        }
                     }
                 }
 
                 if (!String.IsNullOrEmpty(FTPFileName))
                 {
                     DownFileName = DownHandlerPath + FTPFileName;
-                    GetFile(PricePath + FTPFileName, DownFileName, dtSources.Rows[0][SourcesTable.colFTPLogin].ToString(), dtSources.Rows[0][SourcesTable.colFTPPassword].ToString());
+                    using (FileStream fs = new FileStream(DownFileName, FileMode.Create))
+                    {
+                        m_pFtpClient.ReceiveFile(FTPFileName, fs);
+                        fs.Close();
+                    }
                     CurrFileName = DownFileName;
                     CurrPriceDate = fdt;
                 }
@@ -130,6 +78,18 @@ namespace Inforoom.Downloader
             catch (Exception ex)
             {
                 Logging(Convert.ToInt32(dtSources.Rows[0][SourcesTable.colPriceCode]), ex.ToString());
+            }
+            finally
+            { 
+                if (m_pFtpClient != null)
+                {
+                    try
+                    {
+                        m_pFtpClient.Disconnect();
+                    }
+                    catch { }
+                    m_pFtpClient = null;
+                }
             }
         }
 
