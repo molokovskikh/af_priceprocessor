1d232d3db6a2f95c3b579d14665a904f5b326b06
Morozov <Morozov@c0a36a64-a7cb-d643-ba0a-49e1e726fa57>
2006-08-25 10:50:00 +0000

    Вспомогательный класс для работы с масками файлов.

---
+++ src/PriceProcessor/Common/WildcardsHelper.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Inforoom.Downloader
+{
+    public sealed class WildcardsHlp
+    {
+        public static bool IsWildcards(string InputStr)
+        {
+            return (InputStr.IndexOfAny(new char[] { '*', '?' }) > -1);
+        }
+
+        public static bool Matched(string Mask, string Input)
+        {
+            Mask = "^" + Mask.Replace('?', '.').Replace("*", ".*?") + "$";
+            try
+            {
+                Regex re = new Regex(Mask, RegexOptions.IgnoreCase);
+                Match m = re.Match(Input);
+                return m.Success;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
