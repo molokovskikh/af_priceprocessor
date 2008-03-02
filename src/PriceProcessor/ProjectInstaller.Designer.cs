92cf0690cefd5fa338fa6b454aae1f1df7640987
Morozov <Morozov@c0a36a64-a7cb-d643-ba0a-49e1e726fa57>
2008-03-02 19:52:00 +0000

    Добавил инсталятор к сервису.

---
+++ src/PriceProcessor/ProjectInstaller.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Inforoom.PriceProcessor
+{
+	partial class ProjectInstaller
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.PriceProcessorServiceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+			this.PriceProcessorServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+			// 
+			// PriceProcessorServiceProcessInstaller
+			// 
+			this.PriceProcessorServiceProcessInstaller.Password = null;
+			this.PriceProcessorServiceProcessInstaller.Username = null;
+			// 
+			// PriceProcessorServiceInstaller
+			// 
+			this.PriceProcessorServiceInstaller.Description = "Служба для загрузки и формализации прайс-листов";
+			this.PriceProcessorServiceInstaller.DisplayName = "Служба загрузки и формализации прайс-листов";
+			this.PriceProcessorServiceInstaller.ServiceName = "PriceProcessorService";
+			this.PriceProcessorServiceInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+			// 
+			// ProjectInstaller
+			// 
+			this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.PriceProcessorServiceProcessInstaller,
+            this.PriceProcessorServiceInstaller});
+
+		}
+
+		#endregion
+
+		private System.ServiceProcess.ServiceProcessInstaller PriceProcessorServiceProcessInstaller;
+		private System.ServiceProcess.ServiceInstaller PriceProcessorServiceInstaller;
+	}
+}
