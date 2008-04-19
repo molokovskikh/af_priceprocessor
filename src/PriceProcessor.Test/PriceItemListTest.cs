e61dfa7dbd71bb1118e4696544fc5dd6692a5020
Morozov <Morozov@c0a36a64-a7cb-d643-ba0a-49e1e726fa57>
2008-04-19 10:02:00 +0000

    Добавил метод проверки сортировки списка.

--- src/PriceProcessor.Test/PriceItemListTest.cs
+++ src/PriceProcessor.Test/PriceItemListTest.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Inforoom.PriceProcessor;
+using NUnit.Framework;
 
 namespace PriceProcessor.Test
 {
-	public class Class1
+	[TestFixture]
+	public class PriceItemListTest
 	{
+		[Test]
+		public void SortListTest()
+		{
+			PriceProcessItem a = new PriceProcessItem(true, 1, 1, 1, "test.txt");
+			a.FileTime = DateTime.Now.AddHours(-1);
+			PriceItemList.AddItem(a);
+			PriceProcessItem b = new PriceProcessItem(true, 1, 1, 1, "test.txt");
+			b.FileTime = DateTime.Now;
+			PriceItemList.AddItem(b);
+			PriceProcessItem c = PriceItemList.GetLastestDownloaded(1);
+			//Assert.AreEqual(double.PositiveInfinity, double.PositiveInfinity);
+			Assert.AreEqual(c, b, "Последний добавленный прайс-лист выбран некорреткно.");
+		}
 	}
 }
