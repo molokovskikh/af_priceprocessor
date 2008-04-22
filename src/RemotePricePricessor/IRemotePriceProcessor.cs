07ec3c6e8031064bc2f98eed8b5107babb32f7ad
Morozov <Morozov@c0a36a64-a7cb-d643-ba0a-49e1e726fa57>
2008-04-22 06:59:00 +0000

    Добавил проект RemotePriceProcessor.

---
+++ src/RemotePricePricessor/IRemotePriceProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemotePricePricessor
+{
+	/// <summary>
+	/// интерфейс для удаленного взаимодействия с PriceProcessor'ом
+	/// </summary>
+	public interface IRemotePricePricessor
+	{
+		/// <summary>
+		/// Метод позволяющий переслать прайс-лист из истории так, как будто он был скачен
+		/// </summary>
+		/// <param name="DownLogId">Id из таблицы logs.downlogs</param>
+		void ResendPrice(ulong DownLogId);
+	}
+}
