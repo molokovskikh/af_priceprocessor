6587d96423ca93099c46739c606510f7cabc3918
morozov <morozov@c0a36a64-a7cb-d643-ba0a-49e1e726fa57>
2009-06-25 08:59:41 +0000

    Восстановил кол-во SQL-команд в 300.

--- src/PriceProcessor/Properties/AssemblyInfo.cs
+++ src/PriceProcessor/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using log4net.Config;
 
 [assembly: AssemblyTitle("PriceProcessor")]
 [assembly: AssemblyProduct("PriceProcessor")]
-[assembly: AssemblyVersion("1.1.41.69")]
+[assembly: AssemblyVersion("1.1.41.70")]
 [assembly: XmlConfigurator(Watch = true)]
