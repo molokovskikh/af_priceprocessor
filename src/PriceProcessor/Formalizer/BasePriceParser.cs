a35a92f7ed5e46da701a4489326c90f390e0e69f
morozov <morozov@c0a36a64-a7cb-d643-ba0a-49e1e726fa57>
2009-07-07 09:11:29 +0000

    #1486 Осуществить переход на мультилогиность: сделано.

--- src/PriceProcessor/Formalizer/BasePriceParser.cs
+++ src/PriceProcessor/Formalizer/BasePriceParser.cs
@@ -1475,13 +1475,15 @@ and a.ProductId is null";
 							{
 								mcClear.CommandText = String.Format(
 									"UPDATE usersettings.PriceItems SET RowCount={0}, LastFormalization=now(), UnformCount={1} WHERE Id={2};", formCount, unformCount, priceItemId);
-								if (costCode.HasValue)
-									mcClear.CommandText += String.Format(
-										"update usersettings.intersection_update_info, usersettings.intersection set lastsent = default where intersection_update_info.id = intersection.id and intersection.PriceCode = {0} and intersection.CostCode = {1}", priceCode, costCode);
-								else
-									mcClear.CommandText += String.Format(
-										"update usersettings.intersection_update_info set lastsent = default where intersection_update_info.PriceCode = {0}", priceCode);
 							}
+							mcClear.CommandText += String.Format(@"
+UPDATE usersettings.AnalitFReplicationInfo A, usersettings.PricesData P
+SET
+  a.ForceReplication = 1
+where
+    p.PriceCode = {0}
+and a.FirmCode = p.FirmCode;", priceCode);
+
 							sbLog.AppendFormat("UpdatePriceItemsAndIntersections={0}  ", StatCommand(mcClear));
 
 							_logger.InfoFormat("Statistica: {0}", sbLog.ToString());
--- src/PriceProcessor/Properties/AssemblyInfo.cs
+++ src/PriceProcessor/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using log4net.Config;
 
 [assembly: AssemblyTitle("PriceProcessor")]
 [assembly: AssemblyProduct("PriceProcessor")]
-[assembly: AssemblyVersion("1.1.41.71")]
+[assembly: AssemblyVersion("1.1.42.72")]
 [assembly: XmlConfigurator(Watch = true)]
