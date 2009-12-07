e3abcf897c9d461cbdce9e7a4ef36710a382f94d
kvasov <kvasov@c0a36a64-a7cb-d643-ba0a-49e1e726fa57>
2009-12-07 14:51:50 +0000

    Исправил обработку писем, если письмо не удалось обработать то оно будет переслано а затем удалено

--- src/PriceProcessor/Downloader/EMAILSourceHandler.cs
+++ src/PriceProcessor/Downloader/EMAILSourceHandler.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Data;
+using System.Linq;
 using LumiSoft.Net.IMAP.Client;
 using LumiSoft.Net.Mime;
 using Inforoom.PriceProcessor.Properties;
@@ -16,9 +16,6 @@ namespace Inforoom.Downloader
 {
 	public class EMAILSourceHandler : BasePriceSourceHandler
     {
-		//Список ошибочных UID, по которым не надо еще раз отправлять письма
-		protected List<int> errorUIDs;
-
 		//UID текущего обрабатываемого письма
 		protected int currentUID;
 
@@ -27,7 +24,6 @@ namespace Inforoom.Downloader
 		public EMAILSourceHandler()
 		{
 			sourceType = "EMAIL";
-			errorUIDs = new List<int>();
 		}
 
 		protected string GetCorrectEmailAddress(string Source)
@@ -50,103 +46,85 @@ namespace Inforoom.Downloader
 			}
 		}
 
-        protected override void ProcessData()
-        {
-        	string imapSourceFolder = Settings.Default.IMAPSourceFolder;
-			using (var c = new IMAP_Client())
+		protected override void ProcessData()
+		{
+			var imapSourceFolder = Settings.Default.IMAPSourceFolder;
+			using (var imapClient = new IMAP_Client())
 			{
-				c.Connect(Settings.Default.IMAPHost, 143);
-				IMAPAuth(c);
-				c.SelectFolder(imapSourceFolder);
+				imapClient.Connect(Settings.Default.IMAPHost, 143);
+				IMAPAuth(imapClient);
+				imapClient.SelectFolder(imapSourceFolder);
 
 				try
 				{
-					IMAP_FetchItem[] items = null;
-					List<string> ProcessedUID = null;
+					IMAP_FetchItem[] items;
 					do
 					{
 						Ping();
-						ProcessedUID = new List<string>();
 						var sequence_set = new IMAP_SequenceSet();
 						sequence_set.Parse("1:*", long.MaxValue);
-						items = c.FetchMessages(sequence_set, IMAP_FetchItem_Flags.UID, false, false);
+						items = imapClient.FetchMessages(sequence_set, IMAP_FetchItem_Flags.UID, false, false);
 						Ping();
 
-						if ((items != null) && (items.Length > 0))
+						if (items == null || items.Length == 0)
+							continue;
+
+
+						foreach (var item in items)
 						{
-							foreach (IMAP_FetchItem item in items)
+							IMAP_FetchItem[] OneItem = null;
+							try
 							{
-								//Mime m = null;
-								IMAP_FetchItem[] OneItem = null;
-								try
-								{
-									var sequence_Mess = new IMAP_SequenceSet();
-									sequence_Mess.Parse(item.UID.ToString(), long.MaxValue);
-									OneItem = c.FetchMessages(sequence_Mess, IMAP_FetchItem_Flags.Message, false, true);
-									_message = Mime.Parse(OneItem[0].MessageData);
-									currentUID = item.UID;
-									ProcessedUID.Add(item.UID.ToString());
-									Ping();
-								}
-								catch (Exception ex)
-								{
-									if (!errorUIDs.Contains(item.UID))
-									{
-										_message = null;
-										MemoryStream ms = null;
-										if ((OneItem != null) && (OneItem.Length > 0) && (OneItem[0].MessageData != null))
-											ms = new MemoryStream(OneItem[0].MessageData);
-										ErrorMailSend(item.UID, ex.ToString(), ms);
-										errorUIDs.Add(item.UID);
-									}
-									_logger.Error("On Parse", ex);
-								}
+								var sequence_Mess = new IMAP_SequenceSet();
+								sequence_Mess.Parse(item.UID.ToString(), long.MaxValue);
+								OneItem = imapClient.FetchMessages(sequence_Mess, IMAP_FetchItem_Flags.Message, false, true);
+								_message = Mime.Parse(OneItem[0].MessageData);
+								currentUID = item.UID;
+								Ping();
+							}
+							catch (Exception ex)
+							{
+								_logger.Error("On Parse", ex);
+								_message = null;
+								SendBrokenMessage(item, OneItem, ex);
+							}
 
-								if (_message != null)
-								{
-									try
-									{
-										ProcessMime(_message);
-									}
-									catch (Exception ex)
-									{
-										if (ProcessedUID.Contains(item.UID.ToString()))
-											ProcessedUID.Remove(item.UID.ToString());
-										if (!errorUIDs.Contains(item.UID))
-										{
-											MemoryStream ms = null;
-											if ((OneItem != null) && (OneItem.Length > 0) && (OneItem[0].MessageData != null))
-												ms = new MemoryStream(OneItem[0].MessageData);
-											ErrorMailSend(item.UID, ex.ToString(), ms);
-											errorUIDs.Add(item.UID);
-										}
-										_logger.Error("On Process", ex);
-									}
-								}
+							if (_message == null)
+								continue;
 
-							}//foreach (IMAP_FetchItem) 
-
-						}//(items != null) && (items.Length > 0)
+							try
+							{
+								ProcessMime(_message);
+							}
+							catch (Exception ex)
+							{
+								_logger.Error("On Process", ex);
+								SendBrokenMessage(item, OneItem, ex);
+							}
+						}//foreach (IMAP_FetchItem)
 
 						//Производим удаление писем
-						if ((items != null) && (items.Length > 0) && (ProcessedUID.Count > 0))
-						{
-							string uidseq = String.Empty;
-							uidseq = String.Join(",", ProcessedUID.ToArray());
-							var sequence_setDelete = new IMAP_SequenceSet();
-							sequence_setDelete.Parse(uidseq, long.MaxValue);
-							c.DeleteMessages(sequence_setDelete, true);
-						}
+						var sequence = new IMAP_SequenceSet();
 
+						sequence.Parse(String.Join(",", items.Select(i => i.UID.ToString()).ToArray()), long.MaxValue);
+						imapClient.DeleteMessages(sequence, true);
 					}
-					while ((items != null) && (items.Length > 0));
+					while (items != null && items.Length > 0);
 				}
 				finally
 				{
 					_message = null;
 				}
 			}
-        }
+		}
+
+		private void SendBrokenMessage(IMAP_FetchItem item, IMAP_FetchItem[] OneItem, Exception ex)
+		{
+			MemoryStream ms = null;
+			if (OneItem != null && OneItem.Length > 0 && OneItem[0].MessageData != null)
+				ms = new MemoryStream(OneItem[0].MessageData);
+			ErrorMailSend(item.UID, ex.ToString(), ms);
+		}
 
 		protected virtual void IMAPAuth(IMAP_Client c)
 		{
@@ -155,9 +133,7 @@ namespace Inforoom.Downloader
 
         private void ProcessMime(Mime m)
         {
-            AddressList FromList = GetAddressList(m);
-
-            string ShortFileName = string.Empty;
+            var FromList = GetAddressList(m);
 
             // Название аттачментов
             string AttachNames = String.Empty;
@@ -167,7 +143,7 @@ namespace Inforoom.Downloader
 
 			// Формируем список приложений, чтобы использовать 
 			// его при отчете о нераспознанном письме
-			foreach (MimeEntity ent in m.Attachments)
+			foreach (var ent in m.Attachments)
 				if (!String.IsNullOrEmpty(ent.ContentDisposition_FileName) ||
 					!String.IsNullOrEmpty(ent.ContentType_Name))
 				{
@@ -179,7 +155,6 @@ namespace Inforoom.Downloader
 			{
 				// Если в письме есть вложения
 				FillSourcesTable();
-
 				if (!ProcessAttachs(m, FromList, ref _causeSubject, ref _causeBody))
 					ErrorOnProcessAttachs(m, FromList, AttachNames, _causeSubject, _causeBody);
 			}
@@ -202,7 +177,7 @@ namespace Inforoom.Downloader
 		{
 			try
 			{
-				MemoryStream ms = new MemoryStream(m.ToByteData());
+				var ms = new MemoryStream(m.ToByteData());
 				try
 				{
 					LumiSoft.Net.SMTP.Client.SmtpClientEx.QuickSendSmartHost(
@@ -421,7 +396,7 @@ namespace Inforoom.Downloader
 				}
 				return false;
 			}
-			return true;			
+			return true;
 		}
 
 		protected string SaveAttachement(MimeEntity ent)
@@ -433,7 +408,7 @@ namespace Inforoom.Downloader
 				ent.DataToStream(fs);
 				fs.Close();
 			}
-			return attachmentFileName;			 
+			return attachmentFileName;
 		}
 
 		protected string GetShortFileNameFromAttachement(MimeEntity ent)
