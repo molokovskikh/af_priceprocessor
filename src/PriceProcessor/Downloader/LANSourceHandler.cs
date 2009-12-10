f48fd1fc8296f9f7465df7d82a16a6284ca747ef
kvasov <kvasov@c0a36a64-a7cb-d643-ba0a-49e1e726fa57>
2009-12-10 09:18:28 +0000

    Переформатировал код

--- src/PriceProcessor/Downloader/LANSourceHandler.cs
+++ src/PriceProcessor/Downloader/LANSourceHandler.cs
@@ -7,35 +7,29 @@ using Inforoom.Common;
 
 namespace Inforoom.Downloader
 {
-    public class LANSourceHandler : PathSourceHandler
-    {
-        public LANSourceHandler()
-        {
+	public class LANSourceHandler : PathSourceHandler
+	{
+		public LANSourceHandler()
+		{
 			sourceType = "LAN";
 		}
 
-        protected override void GetFileFromSource(PriceSource source)
-        {
-            CurrFileName = String.Empty;
-        	try
-            {
+		protected override void GetFileFromSource(PriceSource source)
+		{
+			CurrFileName = String.Empty;
+			try
+			{
 				var PricePath = FileHelper.NormalizeDir(Settings.Default.FTPOptBoxPath) + source.FirmCode.ToString().PadLeft(3, '0') + Path.DirectorySeparatorChar;
-                var ff = Directory.GetFiles(PricePath, source.PriceMask);
+				var files = Directory.GetFiles(PricePath, source.PriceMask);
 
 				//Сортированный список файлов из директории, подходящих по маске, файл со старшей датой будет первым
 				var sortedFileList = new SortedList<DateTime, string>();
 
-				foreach (var fs in ff)
+				foreach (var file in files)
 				{
-					var fileLastWriteTime = File.GetLastWriteTime(fs);
+					var fileLastWriteTime = File.GetLastWriteTime(file);
 					if (DateTime.Now.Subtract(fileLastWriteTime).TotalMinutes > Settings.Default.FileDownloadInterval)
-						try
-						{
-							sortedFileList.Add(fileLastWriteTime, fs);
-						}
-						catch (ArgumentException)
-						{
-						}
+						sortedFileList.Add(fileLastWriteTime, file);
 				}
 
 				//Если в списке есть файлы, то берем первый и скачиваем
@@ -43,17 +37,17 @@ namespace Inforoom.Downloader
 				{
 					var downloadedFileName = sortedFileList.Values[0];
 					var downloadedLastWriteTime = sortedFileList.Keys[0];
-					var NewFile = DownHandlerPath + Path.GetFileName(downloadedFileName);
+					var newFile = DownHandlerPath + Path.GetFileName(downloadedFileName);
 					try
 					{
-						if (File.Exists(NewFile))
+						if (File.Exists(newFile))
 						{
-							FileHelper.ClearReadOnly(NewFile);
-							File.Delete(NewFile);
+							FileHelper.ClearReadOnly(newFile);
+							File.Delete(newFile);
 						}
 						FileHelper.ClearReadOnly(downloadedFileName);
-						File.Move(downloadedFileName, NewFile);
-						CurrFileName = NewFile;
+						File.Move(downloadedFileName, newFile);
+						CurrFileName = newFile;
 						CurrPriceDate = downloadedLastWriteTime;
 					}
 					catch (Exception ex)
@@ -61,22 +55,22 @@ namespace Inforoom.Downloader
 						Logging(source.PriceItemId, String.Format("Не удалось скопировать файл {0} : {1}", System.Runtime.InteropServices.Marshal.GetLastWin32Error(), ex));
 					}
 				}
-            }
-            catch(Exception exDir)
-            {
+			}
+			catch(Exception exDir)
+			{
 				Logging(source.PriceItemId, String.Format("Не удалось получить список файлов : {0}", exDir));
-            }
-        }
+			}
+		}
 
-        protected override DataRow[] GetLikeSources(PriceSource source)
-        {
-        	if (String.IsNullOrEmpty(source.PriceMask))
+		protected override DataRow[] GetLikeSources(PriceSource source)
+		{
+			if (String.IsNullOrEmpty(source.PriceMask))
 				return dtSources.Select(String.Format("({0} = {1}) and ({2} is null)",
 					SourcesTableColumns.colFirmCode, source.FirmCode,
 					SourcesTableColumns.colPriceMask));
-        	return dtSources.Select(String.Format("({0} = {1}) and ({2} = '{3}')",
-        	                                      SourcesTableColumns.colFirmCode, source.FirmCode,
-        	                                      SourcesTableColumns.colPriceMask, source.PriceMask));
-        }
-    }
+			return dtSources.Select(String.Format("({0} = {1}) and ({2} = '{3}')",
+				SourcesTableColumns.colFirmCode, source.FirmCode,
+				SourcesTableColumns.colPriceMask, source.PriceMask));
+		}
+	}
 }
