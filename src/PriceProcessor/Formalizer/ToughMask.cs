81e06d66b84273d9d8c97e814981ee505ab44d1f
kvasov <kvasov@c0a36a64-a7cb-d643-ba0a-49e1e726fa57>
2010-02-26 10:56:17 +0000

    Немного причесал

--- src/PriceProcessor/Formalizer/ToughMask.cs
+++ src/PriceProcessor/Formalizer/ToughMask.cs
@@ -15,40 +15,35 @@ namespace Inforoom.Formalizer
 		Doc
 	}
 
-	/// <summary>
-	/// Summary description for ToughMask.
-	/// </summary>
 	public class ToughMask
 	{
-		private Regex re;
-		private Match m = null;
-		private string [] reGroupName;
+		private readonly Regex re;
+		private Match m;
+		private readonly string [] reGroupName;
 
-
-
-		public ToughMask(string Mask, long clientCode, long priceCode, string clientShortName, string priceName)
+		public ToughMask(string mask, PriceFormalizationInfo priceInfo)
 		{
 			reGroupName = new string[Enum.GetNames(typeof(NameGroup)).Length];
 			try
 			{
-				re = new Regex(Mask);
+				re = new Regex(mask);
 			}
 			catch(Exception e)
 			{
-				throw new WarningFormalizeException(String.Format(Settings.Default.ParseMaskError, e), clientCode, priceCode, clientShortName, priceName);
+				throw new WarningFormalizeException(String.Format(Settings.Default.ParseMaskError, e), priceInfo);
 			}
-			foreach(string gname in re.GetGroupNames())
+			foreach(var gname in re.GetGroupNames())
 			{
-				string gnameU = gname.ToUpper();
+				var gnameU = gname.ToUpper();
 				foreach(NameGroup ng in Enum.GetValues(typeof(NameGroup)))
 				{
-					string sNG = ng.ToString().ToUpper();
+					var sNG = ng.ToString().ToUpper();
 					if (gnameU.Equals(sNG))
 					{
 						if (null == reGroupName[(int)ng])
 							reGroupName[(int)ng] = gname;
 						else
-							throw new WarningFormalizeException(String.Format(Settings.Default.DoubleGroupMaskError, ng), clientCode, priceCode, clientShortName, priceName);
+							throw new WarningFormalizeException(String.Format(Settings.Default.DoubleGroupMaskError, ng), priceInfo);
 						break;
 					}
 				}
@@ -69,42 +64,41 @@ namespace Inforoom.Formalizer
 		{
 			if (null == m)
 				return String.Empty;
-			else
-				try
+			try
+			{
+				switch((int)PF)
 				{
-					switch((int)PF)
-					{
-						case (int)PriceFields.Code:
-							return m.Groups[reGroupName[(int)NameGroup.Code]].Value;
+					case (int)PriceFields.Code:
+						return m.Groups[reGroupName[(int)NameGroup.Code]].Value;
 
-						case (int)PriceFields.Doc:
-							return m.Groups[reGroupName[(int)NameGroup.Doc]].Value;
+					case (int)PriceFields.Doc:
+						return m.Groups[reGroupName[(int)NameGroup.Doc]].Value;
 
-						case (int)PriceFields.FirmCr:
-							return m.Groups[reGroupName[(int)NameGroup.FirmCr]].Value;
+					case (int)PriceFields.FirmCr:
+						return m.Groups[reGroupName[(int)NameGroup.FirmCr]].Value;
 
-						case (int)PriceFields.Name1:
-						case (int)PriceFields.Name2:
-						case (int)PriceFields.Name3:
-							return m.Groups[reGroupName[(int)NameGroup.Name]].Value;
+					case (int)PriceFields.Name1:
+					case (int)PriceFields.Name2:
+					case (int)PriceFields.Name3:
+						return m.Groups[reGroupName[(int)NameGroup.Name]].Value;
 
-						case (int)PriceFields.Note:
-							return m.Groups[reGroupName[(int)NameGroup.Note]].Value;
+					case (int)PriceFields.Note:
+						return m.Groups[reGroupName[(int)NameGroup.Note]].Value;
 
-						case (int)PriceFields.Period:
-							return m.Groups[reGroupName[(int)NameGroup.Period]].Value;
+					case (int)PriceFields.Period:
+						return m.Groups[reGroupName[(int)NameGroup.Period]].Value;
 
-						case (int)PriceFields.Unit:
-							return m.Groups[reGroupName[(int)NameGroup.Unit]].Value;
+					case (int)PriceFields.Unit:
+						return m.Groups[reGroupName[(int)NameGroup.Unit]].Value;
 
-						default:
-							return String.Empty;
-					}
+					default:
+						return String.Empty;
 				}
-				catch
-				{
-					return String.Empty;
-				}
+			}
+			catch
+			{
+				return String.Empty;
+			}
 		}
 	}
 }
