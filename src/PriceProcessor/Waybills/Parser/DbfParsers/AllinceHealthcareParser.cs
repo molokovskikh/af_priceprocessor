428ffd90093f1e2c6d1cfb9ca0e4bc858ea6529f
kvasov <kvasov@c0a36a64-a7cb-d643-ba0a-49e1e726fa57>
2010-05-21 08:16:32 +0000

    Исправил #2500. Формат накладной Альянс Хелскеа Рус (Москва)

---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/AllinceHealthcareParser.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Data;
+using System.Linq;
+using Common.Tools;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class AllinceHealthcareParser : IDocumentParser
+	{
+		public Document Parse(string file, Document document)
+		{
+			var data = Dbf.Load(file);
+			document.ProviderDocumentId = data.Rows[0]["SERIA"].ToString();
+			document.DocumentDate = Convert.ToDateTime(data.Rows[0]["SROK_GOD"]);
+			data.Rows.Remove(data.Rows[0]);
+			data.Rows.Cast<DataRow>().Each(r => {
+				if (r["VID"].ToString() == "ÆÂËÑ")
+					r["VID"] = "True";
+				else
+					r["VID"] = "False";
+			});
+			new DbfParser()
+				.Line(l => l.Code, "CODE")
+				.Line(l => l.Product, "TOVAR")
+				.Line(l => l.Producer, "PROIZV")
+				.Line(l => l.Country, "STRANA")
+				.Line(l => l.Period, "SROK_GOD")
+				.Line(l => l.Quantity, "KOLVO")
+				.Line(l => l.ProducerCost, "CENA_PRB")
+				.Line(l => l.SupplierCost, "CENA_POB")
+				.Line(l => l.Nds, "ST_NDS")
+				.Line(l => l.RegistryCost, "CENA_GRU")
+				.Line(l => l.Certificates, "SER_NOM")
+				.Line(l => l.SerialNumber, "SERIA")
+				.Line(l => l.VitallyImportant, "VID")
+				.ToDocument(document, data);
+			return document;
+		}
+
+		public static bool CheckFileFormat(DataTable table)
+		{
+			return table.Columns.Contains("CENA_GRU")
+				&& table.Columns.Contains("CENA_POB");
+		}
+	}
+}
