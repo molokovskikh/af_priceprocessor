13551b17643f5a7123ebfe698f1a0d2b5197a373
dorofeev <dorofeev@c0a36a64-a7cb-d643-ba0a-49e1e726fa57>
2010-05-31 06:40:15 +0000

    Исправил #2535. Формат накладной СИА Интернейшнл Казань

---
+++ src/PriceProcessor.Test/Waybills/Parser/SiaKazanParserFixture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser
+{
+	public class SiaKazanParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var doc = WaybillParser.Parse("3879289_СИА_Интернейшнл_Р-424409_.DBF");
+			Assert.That(doc.ProviderDocumentId, Is.EqualTo("Р-424409"));
+			Assert.That(doc.DocumentDate, Is.EqualTo(DateTime.Parse("28.05.2010")));
+			var line = doc.Lines[0];
+			Assert.That(line.Code, Is.EqualTo("79460"));
+			Assert.That(line.Product, Is.EqualTo("Вильпрафен Солютаб дисперг. 1000мг Таб. Х10"));
+			Assert.That(line.Producer, Is.EqualTo("Yamanouchi"));
+			Assert.That(line.Country, Is.EqualTo("Италия"));
+			Assert.That(line.Quantity, Is.EqualTo(1));
+			Assert.That(line.Nds, Is.EqualTo(10));
+			Assert.That(line.Period, Is.EqualTo("01.10.2011"));
+			Assert.That(line.Certificates, Is.EqualTo("РОСС IT.ФМ09.Д03420 ()"));
+			Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(552.0300));
+			Assert.That(line.SupplierCost, Is.EqualTo(607.2300));
+			Assert.That(line.SerialNumber, Is.EqualTo("09J01/87"));
+			Assert.That(line.RegistryCost, Is.EqualTo(0));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/SiaKazanParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class SiaKazanParser : BaseDbfParser
+	{
+		public override DbfParser GetParser()
+		{
+			return new DbfParser()
+				.DocumentHeader(d => d.DocumentDate, "DATA_NAKL")
+				.DocumentHeader(d => d.ProviderDocumentId, "NUM_NAKL")
+				.Line(l => l.Code, "KOD")
+				.Line(l => l.Product, "NAME")
+				.Line(l => l.Producer, "PROIZV")
+				.Line(l => l.Country, "COUNTRY")
+				.Line(l => l.SupplierCostWithoutNDS, "CENABNDS")
+				.Line(l => l.SupplierCost, "CENASNDS")
+				.Line(l => l.Quantity, "KOLVO")
+				.Line(l => l.ProducerCost, "CENAPROIZ")
+				.Line(l => l.Period, "SROK_GODN")
+				.Line(l => l.Certificates, "SERTIF")
+				.Line(l => l.RegistryCost, "REESTR")
+				.Line(l => l.SerialNumber, "SERII");
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("KOD")
+				&& data.Columns.Contains("NAME")
+				&& data.Columns.Contains("NUM_NAKL")
+				&& data.Columns.Contains("CENASNDS")
+				&& data.Columns.Contains("SROK_GODN")
+				&& data.Columns.Contains("PROIZV");
+		}
+	}
+}
