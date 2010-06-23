bcce8a67bdfb260c3c565586bb7f4c0a0bf9d05a
dorofeev <dorofeev@c0a36a64-a7cb-d643-ba0a-49e1e726fa57>
2010-06-23 08:21:26 +0000

    Исправлена ошибка при разборе некоторых двухфайловых накладных (тех, где не указан срок годности)

--- src/PriceProcessor/Waybills/Parser/DbfParsers/AptekaHoldingParser.cs
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/AptekaHoldingParser.cs
@@ -24,18 +24,20 @@ namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
 				var line = document.NewLine();
 				line.Code = Convert.ToString(r["GOODSID"], CultureInfo.InvariantCulture);
 				line.Product = r["GOODSN"].ToString();
-				line.Producer = r["FIRMN"].ToString();
+				line.Producer = Convert.IsDBNull(r["FIRMN"]) ? null : r["FIRMN"].ToString();
 				line.Country = Convert.IsDBNull(r["COUNTRYN"]) ? null : r["COUNTRYN"].ToString();
 				line.ProducerCost = Convert.ToDecimal(r["PRICEF"], CultureInfo.InvariantCulture);
 				line.SupplierCost = Convert.ToDecimal(r["PRICE"], CultureInfo.InvariantCulture);
 				line.Quantity = Convert.ToUInt32(r["QUANTITY"]);
-				line.Period = Convert.ToDateTime(r["BESTBEFORE"]).ToShortDateString();
+				if (!Convert.IsDBNull(r["BESTBEFORE"]))
+					line.Period = Convert.ToDateTime(r["BESTBEFORE"]).ToShortDateString();
 				line.Certificates = Convert.IsDBNull(r["ANALYSIS"]) ? null : r["ANALYSIS"].ToString();
 				line.SetNds(Convert.ToUInt32(r["NDS"], CultureInfo.InvariantCulture));
 				line.RegistryCost = Convert.IsDBNull(r["PRICEREG"]) ? null : (decimal?) Convert.ToDecimal(r["PRICEREG"]);
 				if (data.Columns.Contains("ISDEC"))
 					line.VitallyImportant = Convert.IsDBNull(r["ISDEC"]) ? null : (bool?) (Convert.ToUInt32(r["ISDEC"]) == 1);
-				line.SerialNumber = r["SERIES"].ToString();
+				if (!Convert.IsDBNull(r["SERIES"]))
+					line.SerialNumber = r["SERIES"].ToString();
 				return line;
 			}).ToList();
 			return document;
