cdc2fdcd3a085450e2ae96bdaacb6c838af2166c
dorofeev <dorofeev@c0a36a64-a7cb-d643-ba0a-49e1e726fa57>
2010-07-01 11:42:06 +0000

    Исправил #2615 (Разбор накладной СИА Орел)

--- src/PriceProcessor/Waybills/Parser/DbfParsers/SiaParser.cs
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/SiaParser.cs
@@ -30,6 +30,8 @@ namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
 				vitallyImportantColumn = "Priznak_pr";
 			else if (data.Columns.Contains("VITAL"))
 				vitallyImportantColumn = "VITAL";
+			else if (data.Columns.Contains("GVLS"))
+				vitallyImportantColumn = "GVLS";
 
 			if (data.Columns.Contains("REESTR"))
 				registryCostColumn = "REESTR";
