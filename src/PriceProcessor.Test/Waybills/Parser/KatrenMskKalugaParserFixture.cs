3aa0d83863999dd298a3d649a59f59fa391a11a9
dorofeev <dorofeev@c0a36a64-a7cb-d643-ba0a-49e1e726fa57>
2010-07-06 09:10:41 +0000

    Добавлен разбор накладных для Катрен москва - калуга (требование #2623)

---
+++ src/PriceProcessor.Test/Waybills/Parser/KatrenMskKalugaParserFixture.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser
+{
+	[TestFixture]
+	public class KatrenMskKalugaParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var document = WaybillParser.Parse(@"454666.dbf");
+			Assert.That(document.Lines.Count, Is.EqualTo(2));
+			Assert.That(document.ProviderDocumentId, Is.EqualTo("454666"));
+			Assert.That(document.DocumentDate, Is.EqualTo(Convert.ToDateTime("08.08.2006")));
+			Assert.That(document.Lines[0].Code, Is.EqualTo("1031022"));
+			Assert.That(document.Lines[0].Product, Is.EqualTo("ЛЕДИ-С ФОРМУЛА БОЛЬШЕ ЧЕМ П/ВИТАМИНЫ N60 КАПС"));
+			Assert.That(document.Lines[0].Producer, Is.EqualTo("ВИТАФАРМ КАНАДА Инк."));
+			Assert.That(document.Lines[0].Country, Is.EqualTo("Канада"));
+			Assert.That(document.Lines[0].Quantity, Is.EqualTo(1));
+			Assert.That(document.Lines[0].SupplierCost, Is.EqualTo(372.85));
+			Assert.That(document.Lines[0].SupplierCostWithoutNDS, Is.EqualTo(315.97));
+			Assert.That(document.Lines[0].ProducerCost, Is.EqualTo(289.90));
+			Assert.That(document.Lines[0].ProducerCost, Is.LessThanOrEqualTo(document.Lines[0].SupplierCost));
+			Assert.That(document.Lines[0].SupplierCostWithoutNDS, Is.LessThanOrEqualTo(document.Lines[0].SupplierCost));
+			Assert.That(document.Lines[0].Period, Is.EqualTo("01.11.2008"));
+			Assert.That(document.Lines[0].VitallyImportant, Is.False);
+			Assert.That(document.Lines[0].Nds.Value, Is.EqualTo(18));
+			Assert.That(document.Lines[0].Certificates, Is.EqualTo("77.99.23.3.У.7647.12.04"));
+			Assert.That(document.Lines[0].RegistryCost, Is.EqualTo(0));
+			Assert.That(document.Lines[0].SerialNumber, Is.EqualTo("410010811"));
+
+			Assert.That(document.Lines[1].VitallyImportant, Is.False);
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/KatrenMskKalugaParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class KatrenMskKalugaParser : BaseDbfParser
+	{
+		public override DbfParser GetParser()
+		{
+			return new DbfParser()
+				.DocumentHeader(d => d.DocumentDate, "DocDate")
+				.DocumentHeader(d => d.ProviderDocumentId, "DocNum")
+				.Line(l => l.Code, "GoodId")
+				.Line(l => l.Product, "GoodName")
+				.Line(l => l.Producer, "Vendor")
+				.Line(l => l.Country, "Country")
+				.Line(l => l.SupplierCost, "Price")
+				.Line(l => l.Quantity, "Amount")
+				.Line(l => l.ProducerCost, "VendorPric")
+				.Line(l => l.Nds, "NDS")
+				.Line(l => l.Period, "LifeTime")
+				.Line(l => l.Certificates, "Sertif")
+				.Line(l => l.RegistryCost, "ReestrPr")
+				.Line(l => l.VitallyImportant, "VitImport")
+				.Line(l => l.SerialNumber, "Series");
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("DocNum")
+				&& data.Columns.Contains("GoodId")
+				&& data.Columns.Contains("GoodName")
+				&& data.Columns.Contains("Vendor")
+				&& data.Columns.Contains("Amount");
+		}
+	}
+}
