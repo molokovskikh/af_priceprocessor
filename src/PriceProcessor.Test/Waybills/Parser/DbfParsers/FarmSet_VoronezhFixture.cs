96fcc18715d87e92936a01c5888a219e50c7af7a
Золотарев Александр <Lichcomp@mail.ru>
2010-10-28 09:15:59 +0400

    Реализовано требование 3105 Формат накладных Фармкомплект-Воронеж

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/FarmSet_VoronezhFixture.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	public class FarmSet_VoronezhFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var doc = WaybillParser.Parse("PY362019.dbf");
+			Assert.That(doc.ProviderDocumentId, Is.EqualTo("ФК000362019"));
+			Assert.That(doc.DocumentDate.Value.ToShortDateString(), Is.EqualTo("25.10.2010"));
+			var line = doc.Lines[0];
+            //Assert.That(.S, Is.EqualTo("815575"));
+			Assert.That(line.Code, Is.EqualTo("7045"));
+			Assert.That(line.Product, Is.EqualTo("Апилак 0,01г №10 таб."));
+			Assert.That(line.Producer, Is.EqualTo("Вифитех"));
+			Assert.That(line.Country, Is.EqualTo("РОССИЯ"));
+			Assert.That(line.Quantity, Is.EqualTo(1));
+			Assert.That(line.Nds, Is.EqualTo(10));
+			Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(11.19));
+			Assert.That(line.SupplierCost, Is.EqualTo(12.31));
+			Assert.That(line.SerialNumber, Is.EqualTo("060410"));
+			Assert.That(line.Certificates, Is.EqualTo("РОСС RU.ФМ08.Д98276"));
+			Assert.That(line.Period, Is.EqualTo("01.05.2013"));
+            Assert.That(line.ProducerCost, Is.EqualTo(10.4));
+            Assert.That(line.RegistryCost, Is.EqualTo(0));
+            Assert.That(line.VitallyImportant, !Is.True);
+		}
+	}
+}
+
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/FarmSet_VoronezhParser.cs
@@ -0,0 +1,46 @@
+﻿	using System;
+	using System.Data;
+	using System.Linq;
+	using Common.Tools;
+
+	namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+	{
+		public class FarmSet_VoronezhParser : IDocumentParser
+		{
+			public Document Parse(string file, Document document)
+			{
+				var data = Dbf.Load(file);
+				new DbfParser()
+					.DocumentHeader(h => h.ProviderDocumentId, "DOCNO")
+					.DocumentHeader(h => h.DocumentDate, "DOCDAT")
+					.Line(l => l.Code, "CODTOVAR")
+					.Line(l => l.Product, "TOVARNAME")
+					.Line(l => l.Producer, "PROIZV")
+					.Line(l => l.Country, "STRANA")
+					.Line(l => l.Quantity, "KOLVO")
+					.Line(l => l.Nds, "NDS")
+					.Line(l => l.SupplierCostWithoutNDS, "CENAPOST")
+					.Line(l => l.SupplierCost, "CENASNDS")
+					.Line(l => l.SerialNumber, "SERIA")
+					.Line(l => l.Certificates, "SERT")
+					.Line(l => l.Period, "SROK")
+					.Line(l => l.RegistryCost, "CENAREESTR")
+					.Line(l => l.ProducerCost, "CENAPROIZ")
+					.Line(l => l.VitallyImportant, "PV")
+					//.Line(l => l., "ST10")
+					.ToDocument(document, data);
+				return document;
+			}
+
+			//private string 
+
+			public static bool CheckFileFormat(DataTable table)
+			{
+				return table.Columns.Contains("CODTOVAR")
+				       && table.Columns.Contains("CENAPOST")
+				       && table.Columns.Contains("CENASNDS")
+				       && table.Columns.Contains("SROK")
+					   && table.Columns.Contains("PV");
+			}
+		}
+	}
