ae5d647ec58195e282b6a258fc55749c3da37590
giorgadze <m.giorgadze@analit.net>
2010-11-13 16:22:32 +0300

    добавлен формат накладной для Морон Саратов

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/MoronSaratovParserFixture.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	public class MoronSaratovParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var doc = WaybillParser.Parse("00841624.dbf");
+			Assert.That(doc.ProviderDocumentId, Is.EqualTo("841624,00"));
+			Assert.That(doc.DocumentDate.Value.ToShortDateString(), Is.EqualTo("02.11.2010"));
+			Assert.That(doc.Lines.Count, Is.EqualTo(14));
+			var line = doc.Lines[0];
+			Assert.That(line.Code, Is.EqualTo("47089,00"));
+			Assert.That(line.Product, Is.EqualTo("Аскорутин таб. №50"));
+			Assert.That(line.Producer, Is.EqualTo("Фармстандарт-Уфимский витам. з-д"));
+			Assert.That(line.Country, Is.EqualTo("Россия"));
+			Assert.That(line.Quantity, Is.EqualTo(5));
+			Assert.That(line.Nds, Is.EqualTo(10));
+			Assert.That(line.SupplierCost, Is.EqualTo(17.60));
+			//Assert.That(line.ProducerCost, Is.EqualTo(16.50));
+			Assert.That(line.SerialNumber, Is.EqualTo("410910"));
+			Assert.That(line.Certificates, Is.EqualTo("РОСС.RU.ФМ05.Д09976"));
+			Assert.That(line.SupplierPriceMarkup, Is.EqualTo(5.5));
+			Assert.That(line.RegistryCost, Is.EqualTo(0));
+			Assert.That(line.Period, Is.EqualTo("01.10.2013"));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/MoronSaratovParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class MoronSaratovParser : BaseDbfParser
+	{
+		public override DbfParser GetParser()
+		{
+			return new DbfParser()
+				.DocumentHeader(d => d.ProviderDocumentId, "NUM_NAK")
+				.DocumentHeader(d => d.DocumentDate, "DATE_NAK")
+				.Line(l => l.Code, "KOD")
+				.Line(l => l.Product, "NAM_LEK")
+				.Line(l => l.Producer, "ZAVOD")
+				.Line(l => l.Country, "STRANA")
+				.Line(l => l.Period, "SROK_GOD")
+				.Line(l => l.SerialNumber, "SERIY")
+				.Line(l => l.Quantity, "COUNT")
+				.Line(l => l.ProducerCost, "CENA_ZAVODA") 
+				.Line(l => l.SupplierCost, "CENA")
+				.Line(l => l.Nds, "PRC_NDS")
+				.Line(l => l.RegistryCost, "REESTR")
+				.Line(l => l.Certificates, "SERTIF")
+				.Line(l => l.SupplierPriceMarkup, "SUMMA_NAC");
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("NUM_NAK")
+				&& data.Columns.Contains("DATE_NAK")
+				&& data.Columns.Contains("NAM_LEK")
+				&& data.Columns.Contains("STRANA")
+				&& data.Columns.Contains("ZAVOD");
+		}
+	}
+}
