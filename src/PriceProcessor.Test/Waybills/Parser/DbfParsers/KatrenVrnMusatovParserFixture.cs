c706540a793a291d032b29af57d02083da36ae93
giorgadze <m.giorgadze@analit.net>
2010-12-20 09:47:53 +0300

    реализовано #3225: добавлен новый парсер

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/KatrenVrnMusatovParserFixture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	class KatrenVrnMusatovParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var doc = WaybillParser.Parse("203176.dbf");
+			Assert.That(doc.Lines.Count, Is.EqualTo(6));
+			var line = doc.Lines[0];
+			Assert.That(line.Code, Is.EqualTo("16881"));
+			Assert.That(line.Product, Is.EqualTo("ДИОКСИДИНА 5% 30,0 МАЗЬ"));
+			Assert.That(line.Quantity, Is.EqualTo(3));
+			Assert.That(line.Certificates, Is.EqualTo("РОСС RU.ФМ01.Д51084"));
+			Assert.That(line.Country, Is.EqualTo("Россия"));
+			Assert.That(line.Producer, Is.EqualTo("Биосинтез ОАО"));
+			Assert.That(line.Period, Is.EqualTo("01.08.2013"));
+			Assert.That(line.SerialNumber, Is.EqualTo("30710"));
+			Assert.That(line.SupplierCost, Is.EqualTo(83.38));
+			Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(75.8));
+			Assert.That(line.ProducerCost, Is.EqualTo(83.38));
+			Assert.That(line.RegistryCost, Is.EqualTo(0));
+			Assert.That(line.Nds, Is.EqualTo(10));
+			Assert.That(line.SupplierPriceMarkup, Is.EqualTo(0));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/KatrenVrnMusatovParser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Common.Tools;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class KatrenVrnMusatovParser : IDocumentParser
+	{
+		public Document Parse(string file, Document document)
+		{
+			var data = Dbf.Load(file);
+			new DbfParser()
+				.Line(l => l.Code, "CODE")
+				.Line(l => l.Product, "GOODE")
+				.Line(l => l.SerialNumber, "SERIAL")
+				.Line(l => l.Period, "DATEB")
+				.Line(l => l.SupplierCostWithoutNDS, "PPRICEWT")
+				.Line(l => l.SupplierCost, "PRICE")
+				.Line(l => l.Quantity, "QUANT")
+				.Line(l => l.SupplierPriceMarkup, "MARGIN")
+				.Line(l => l.Nds, "NDS")
+				.Line(l => l.RegistryCost, "REESTR")
+				.Line(l => l.Certificates, "SERT")
+				.Line(l => l.Country, "COUNTRY")
+				.Line(l => l.Producer, "PRODUSER")
+				.Line(l => l.ProducerCost, "PPRICENDS")
+				.ToDocument(document, data);
+			return document;;
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("CODE")
+				&& data.Columns.Contains("GOODE")
+				&& data.Columns.Contains("SERIAL")
+				&& data.Columns.Contains("PPRICEWT")
+				&& data.Columns.Contains("PRICE");
+		}
+	}
+}
