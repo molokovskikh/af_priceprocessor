bf08e36a2cac7ff24dff3e256477c2f3179f7563
giorgadze <m.giorgadze@analit.net>
2010-12-21 16:33:46 +0300

    реализовано #3269: добавлен новый парсер

---
+++ src/PriceProcessor.Test/Waybills/Parser/Txt/AptekaHoldingKazanParserFixture.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.Txt
+{
+	[TestFixture]
+	public class AptekaHoldingKazanParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var doc = WaybillParser.Parse("969533.txt");
+			Assert.That(doc.Lines.Count, Is.EqualTo(3));
+			Assert.That(doc.ProviderDocumentId, Is.EqualTo("00000969533"));
+			Assert.That(doc.DocumentDate, Is.EqualTo(Convert.ToDateTime("17.12.10")));
+
+			Assert.That(doc.Lines[0].Code, Is.EqualTo("26781"));
+			Assert.That(doc.Lines[0].Product, Is.EqualTo("Азитрокс капс. 500мг N3 Россия"));
+			Assert.That(doc.Lines[0].Producer, Is.EqualTo("Фармстандарт- Лексредства, г. Курск"));
+			Assert.That(doc.Lines[0].Country, Is.EqualTo("Россия"));
+			Assert.That(doc.Lines[0].Quantity, Is.EqualTo(4));
+			Assert.That(doc.Lines[0].ProducerCost, Is.EqualTo(179));
+			Assert.That(doc.Lines[0].SupplierCostWithoutNDS, Is.EqualTo(167.99));
+			Assert.That(doc.Lines[0].Nds, Is.EqualTo(10));
+			Assert.That(doc.Lines[0].SerialNumber, Is.EqualTo("120710"));
+			Assert.That(doc.Lines[0].Period, Is.EqualTo("01.08.12"));
+			Assert.That(doc.Lines[0].Certificates, Is.EqualTo("POCC RU.ФМ05.Д69302"));
+			Assert.That(doc.Lines[0].RegistryCost, Is.EqualTo(193.32));
+			Assert.That(doc.Lines[0].VitallyImportant, Is.EqualTo(true));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/TxtParsers/AptekaHoldingKazanParser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.TxtParsers
+{
+	public class AptekaHoldingKazanParser : BaseIndexingParser
+	{
+		protected override void SetIndexes()
+		{
+			CodeIndex = 0;
+			ProductIndex = 1;
+			ProducerIndex = 2;
+			CountryIndex = 3;
+			QuantityIndex = 4;
+			ProducerCostIndex = 5;
+			SupplierCostWithoutNdsIndex = 6;
+			NdsIndex = 7;
+			SerialNumberIndex = 9;
+			PeriodIndex = 10;
+			CertificatesIndex = 12;
+			RegistryCostIndex = 22;
+			VitallyImportantIndex = 26;
+		}
+
+		public static bool CheckFileFormat(string file)
+		{
+			using (var reader = new StreamReader(file, Encoding.GetEncoding(1251)))
+			{
+				var headerCaption = reader.ReadLine();
+				if (!headerCaption.ToLower().Equals("[header]"))
+					return false;
+				var header = reader.ReadLine().Split(';');
+				if (header.Length != 6)
+					return false;
+				if (!header[3].ToLower().Equals("аптека-холдинг"))
+					return false;
+				var bodyCaption = reader.ReadLine();
+				if (!bodyCaption.ToLower().Equals("[body]"))
+					return false;
+			}
+			return true;
+		}
+	}
+}
