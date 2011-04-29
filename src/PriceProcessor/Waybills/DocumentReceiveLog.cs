7294394cc3b23a2a34f622031c37f17b24ccdd5d
Квасов Роман <r.kvasov@analit.net>
2011-04-29 16:49:57 +0400

    добавил новую опцию для выкладки документов

--- src/PriceProcessor/Waybills/DocumentReceiveLog.cs
+++ src/PriceProcessor/Waybills/DocumentReceiveLog.cs
@@ -81,7 +81,7 @@ namespace Inforoom.PriceProcessor.Waybills
 		private string GetDocumentDir()
 		{
 			var code = AddressId.HasValue ? AddressId.Value : ClientCode;
-			var clientDir = Path.Combine(Settings.Default.FTPOptBoxPath, code.ToString().PadLeft(3, '0'));
+			var clientDir = Path.Combine(Settings.Default.DocumentPath, code.ToString().PadLeft(3, '0'));
 			return Path.Combine(clientDir, DocumentType + "s");
 		}
 
