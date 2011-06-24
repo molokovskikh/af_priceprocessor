85bbb9e76d78279695ab59fd350d946abec8ff26
Тютин Алексей <a.tyutin@analit.net>
2011-06-24 10:36:27 +0400

    Доп. тест для VectorGroupKalugaParser

--- src/PriceProcessor.Test/Waybills/Parser/Txt/VectorGroupKalugaParserFixture.cs
+++ src/PriceProcessor.Test/Waybills/Parser/Txt/VectorGroupKalugaParserFixture.cs
@@ -31,7 +31,35 @@ namespace PriceProcessor.Test.Waybills.Parser.Txt
             Assert.That(doc.Lines[0].Certificates, Is.EqualTo("РОСС IT АЕ95 В 00535"));
             Assert.That(doc.Lines[0].Country, Is.EqualTo("Польша"));
             Assert.That(doc.Lines[0].CertificatesDate, Is.EqualTo("17.01.2009"));
-            Assert.That(doc.Lines[0].VitallyImportant, Is.False);            
+            Assert.That(doc.Lines[0].VitallyImportant, Is.False);
+        }
+
+        [Test]
+        public void Parse2()
+        {
+            var doc = WaybillParser.Parse("a34202.txt");
+            Assert.That(doc.Lines.Count, Is.EqualTo(6));
+            Assert.That(doc.ProviderDocumentId, Is.EqualTo("34202"));
+            Assert.That(doc.DocumentDate.Value.ToShortDateString(), Is.EqualTo("22.06.2011"));
+
+            Assert.That(doc.Lines[0].Code, Is.Null);
+            Assert.That(doc.Lines[0].Product, Is.EqualTo("Крем ВИНОГРАД увлажняющий 40мл НК нор/комб/к*36"));
+            Assert.That(doc.Lines[0].Quantity, Is.EqualTo(3));
+            Assert.That(doc.Lines[0].Producer, Is.EqualTo("НЕВСКАЯ КОСМЕТИКА"));
+            Assert.That(doc.Lines[0].SupplierCostWithoutNDS, Is.EqualTo(18.53));
+            Assert.That(doc.Lines[0].SupplierCost, Is.EqualTo(21.86));
+            Assert.That(doc.Lines[0].SupplierPriceMarkup, Is.Null);
+            Assert.That(doc.Lines[0].RegistryCost, Is.Null);
+            Assert.That(doc.Lines[0].ProducerCost, Is.Null);
+            Assert.That(doc.Lines[0].Nds, Is.EqualTo(18));
+            Assert.That(doc.Lines[0].NdsAmount, Is.EqualTo(10.00));
+            Assert.That(doc.Lines[0].Amount, Is.EqualTo(65.58));
+            Assert.That(doc.Lines[0].SerialNumber, Is.Null);
+            Assert.That(doc.Lines[0].Period, Is.EqualTo("28.10.2012"));
+            Assert.That(doc.Lines[0].Certificates, Is.EqualTo("РОСС RU АЕ45 В 27744"));
+            Assert.That(doc.Lines[0].Country, Is.EqualTo("Россия"));
+            Assert.That(doc.Lines[0].CertificatesDate, Is.EqualTo("14.12.2012"));
+            Assert.That(doc.Lines[0].VitallyImportant, Is.False);
         }
 
         [Test]
