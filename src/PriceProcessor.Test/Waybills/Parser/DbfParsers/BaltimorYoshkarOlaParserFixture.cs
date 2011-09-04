7fa50de0e6c097df58b2be98503c18208268de1b
Тютин Алексей <a.tyutin@analit.net>
2011-09-04 14:05:45 +0400

    Исправил #5725. Формат накладных для поставщика Балтимор Йошкар-Ола

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/BaltimorYoshkarOlaParserFixture.cs
@@ -0,0 +1,41 @@
+﻿using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Parser.DbfParsers;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	public class BaltimorYoshkarOlaParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			Assert.IsTrue(BaltimorYoshkarOlaParser.CheckFileFormat(Dbf.Load(@"..\..\Data\Waybills\00033318.dbf")));
+			var doc = WaybillParser.Parse("00033318.dbf");
+			var line = doc.Lines[0];
+			Assert.That(doc.ProviderDocumentId, Is.EqualTo("ЧБ000033318"));
+			Assert.That(doc.DocumentDate.Value.ToShortDateString(), Is.EqualTo("29.08.2011"));
+			Assert.That(line.Code, Is.EqualTo("36869"));
+			Assert.That(line.Product, Is.EqualTo("Окситоцин МЭЗ р-р д/ин. 5МЕ амп. 1мл. №10"));
+			Assert.That(line.Producer, Is.EqualTo("Московский эндокринный з-д"));
+			Assert.That(line.Country, Is.EqualTo("РОССИЯ"));
+			Assert.That(line.BillOfEntryNumber, Is.Null);
+			Assert.That(doc.Lines[2].BillOfEntryNumber, Is.EqualTo("10130130/200611/0012231/1"));
+			Assert.That(line.SerialNumber, Is.EqualTo("70411"));
+			Assert.That(line.Period, Is.EqualTo("01.05.2013"));
+			Assert.That(line.Certificates, Is.EqualTo("РОСС RU.ФМ01.Д42360"));
+			Assert.That(line.Quantity, Is.EqualTo(2));
+			Assert.That(line.SupplierPriceMarkup, Is.EqualTo(0.00));
+			Assert.That(doc.Lines[1].SupplierPriceMarkup, Is.EqualTo(12.57));
+			Assert.That(line.SupplierCost, Is.EqualTo(22.69));
+			Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(20.63));
+			Assert.That(line.Nds, Is.EqualTo(10));
+			Assert.That(line.NdsAmount, Is.EqualTo(4.13));
+			Assert.That(line.Amount, Is.EqualTo(45.38));
+			Assert.That(line.EAN13, Is.EqualTo("4602676001761"));
+			Assert.That(line.ProducerCost, Is.EqualTo(21.45));
+			Assert.That(line.VitallyImportant, Is.True);
+			Assert.That(line.RegistryCost, Is.EqualTo(21.45));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/BaltimorYoshkarOlaParser.cs
@@ -0,0 +1,49 @@
+﻿using System.Data;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class BaltimorYoshkarOlaParser : BaseDbfParser
+	{
+		public override DbfParser GetParser()
+		{
+			return new DbfParser()
+				.DocumentHeader(d => d.ProviderDocumentId, "NOM_NAKL")
+				.DocumentHeader(d => d.DocumentDate, "DATA_SHET")
+				.Line(l => l.Code, "KOD")
+				.Line(l => l.Product, "NAME")
+				.Line(l => l.Producer, "ZAVOD")
+				.Line(l => l.Country, "STRANA")
+				.Line(l => l.BillOfEntryNumber, "GTD")
+				.Line(l => l.SerialNumber, "SERIA")
+				.Line(l => l.Period, "SROK")
+				.Line(l => l.Certificates, "SERTIF")
+				.Line(l => l.Quantity, "KOLVO")
+				.Line(l => l.SupplierPriceMarkup, "OPT_NAC")
+				.Line(l => l.SupplierCost, "CENA_OTP")
+				.Line(l => l.SupplierCostWithoutNDS, "CENA_BNDS")
+				.Line(l => l.Nds, "NDS")
+				.Line(l => l.NdsAmount, "SUMNDS")
+				.Line(l => l.Amount, "SUMSNDS")
+				.Line(l => l.EAN13, "BAR")
+				.Line(l => l.ProducerCost, "CIZG")
+				.Line(l => l.VitallyImportant, "JV")
+				.Line(l => l.RegistryCost, "GOSREESTR");
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("KOD")
+				&& data.Columns.Contains("NAME")
+				&& data.Columns.Contains("ZAVOD")
+				&& data.Columns.Contains("STRANA")
+				&& data.Columns.Contains("GTD")
+				&& data.Columns.Contains("SROK")
+				&& data.Columns.Contains("KOLVO")
+				&& data.Columns.Contains("CENA_OTP")
+				&& data.Columns.Contains("CENA_BNDS")
+				&& data.Columns.Contains("GOSREESTR")
+				&& data.Columns.Contains("CIZG")
+				&& data.Columns.Contains("JV");
+		}
+	}
+}
