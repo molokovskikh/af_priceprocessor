9b62d1b80e47870c7b76d5d22a1f9172e075a429
Тютин Алексей <a.tyutin@analit.net>
2011-09-22 11:46:13 +0400

    Исправил #6173. казмедфарм

---
+++ src/PriceProcessor.Test/Waybills/Parser/Txt/KazMedFarmParserFixture.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.Txt
+{
+	[TestFixture]
+	public class KazMedFarmParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var doc = WaybillParser.Parse(@"18296308_001.txt");
+			Assert.That(doc.Lines.Count, Is.EqualTo(18));
+			Assert.That(doc.ProviderDocumentId, Is.EqualTo("18296308-001"));
+			Assert.That(doc.DocumentDate, Is.EqualTo(Convert.ToDateTime("21.09.2011")));
+			Assert.That(doc.Lines[0].Code, Is.EqualTo("202867"));
+			Assert.That(doc.Lines[0].Product, Is.EqualTo("ВАЛИДОЛ КАПС. 50МГ №40"));
+			Assert.That(doc.Lines[0].Quantity, Is.EqualTo(2));
+			Assert.That(doc.Lines[0].SupplierCost, Is.EqualTo(16.41));
+			Assert.That(doc.Lines[0].SupplierCostWithoutNDS, Is.EqualTo(14.92));
+			Assert.That(doc.Lines[0].Nds, Is.EqualTo(10));
+			Assert.That(doc.Lines[0].NdsAmount, Is.EqualTo(2.98));
+			Assert.That(doc.Lines[0].Amount, Is.EqualTo(32.82));
+			Assert.That(doc.Lines[0].Certificates, Is.EqualTo("POCC RU.ФM05.Д61964"));
+			Assert.That(doc.Lines[0].SerialNumber, Is.EqualTo("050511"));
+			Assert.That(doc.Lines[0].Period, Is.EqualTo("01.06.2013"));
+			Assert.That(doc.Lines[0].BillOfEntryNumber, Is.Null);
+			Assert.That(doc.Lines[2].BillOfEntryNumber, Is.EqualTo("10130032/100211/0000358"));
+			Assert.That(doc.Lines[0].EAN13, Is.EqualTo("04607004431395"));
+			Assert.That(doc.Lines[0].Producer, Is.EqualTo("Люми ООО"));
+			Assert.That(doc.Lines[0].Country, Is.EqualTo("Россия"));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/TxtParsers/KazMedFarmParser.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.TxtParsers
+{
+	public class KazMedFarmParser : BaseIndexingParser
+	{
+		protected override void SetIndexes()
+		{
+			ProviderDocumentIdIndex = 0;
+			DocumentDateIndex = 1;
+			CodeIndex = 0;
+			ProductIndex = 1;
+			QuantityIndex = 2;
+			SupplierCostIndex = 3;
+			NdsIndex = 4;
+			NdsAmountIndex = 5;
+			AmountIndex = 6;
+			CertificatesIndex = 7;
+			SerialNumberIndex = 8;
+			PeriodIndex = 9;
+			BillOfEntryNumberIndex = 10;
+			EAN13Index = 11;
+			ProducerIndex = 12;
+			CountryIndex = 14;
+		}
+
+		public static bool CheckFileFormat(string file)
+		{
+			using (var reader = new StreamReader(file, Encoding.GetEncoding(1251)))
+			{
+				var headerCaption = reader.ReadLine();
+				if (!headerCaption.ToLower().Equals("[header]"))
+					return false;
+				var header = reader.ReadLine().Split(';');
+				if (header.Length != 6)
+					return false;
+				if (GetDateTime(header[1]) == null)
+					return false;
+				var bodyCaption = reader.ReadLine();
+				if (!bodyCaption.ToLower().Equals("[body]"))
+					return false;
+				var body = reader.ReadLine().Split(';');
+				if (body.Length != 15)
+					return false;
+				if (GetDecimal(body[3]) == null)
+					return false;
+				if (GetDecimal(body[6]) == null)
+					return false;
+				if (GetDateTime(body[9]) == null)
+					return false;				
+			}
+			return true;
+		}
+	}
+}
