0379e106fc4ed93921b825f068cc266c92901718
Morozov Sergey <morozov@analit.net>
2011-09-23 12:10:44 +0400

    В DocumentBodies добавил внешний ключ на ProductId и индекс на SerialNumber.

---
+++ migrations/264_00_AddProductIdKeyToDocumentBodies.sql
@@ -0,0 +1,3 @@
+﻿alter table documents.DocumentBodies 
+  add index IDX_DocumentBodies_SerialNumber (SerialNumber),
+  add CONSTRAINT FK_DocumentBodies_ProductId FOREIGN KEY (ProductId) REFERENCES catalogs.Products (Id) ON DELETE RESTRICT ON UPDATE RESTRICT;
