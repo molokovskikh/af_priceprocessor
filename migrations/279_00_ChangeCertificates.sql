1db06c9d8a6cddaaf5878469c23e4c035707967c
Morozov Sergey <morozov@analit.net>
2011-10-07 10:10:21 +0400

    Написал миграции.

---
+++ migrations/279_00_ChangeCertificates.sql
@@ -0,0 +1,48 @@
+﻿create table Documents.CertificateSources(
+  Id int unsigned not null auto_increment,
+  SourceSupplierId int unsigned not null,
+  SourceClassName varchar(255) not null,
+  primary key (Id),
+  CONSTRAINT FK_CertificateSources_SourceSupplierId FOREIGN KEY (SourceSupplierId) REFERENCES future.Suppliers (Id) ON DELETE RESTRICT ON UPDATE RESTRICT
+);
+
+
+insert into  Documents.CertificateSources (Id, SourceSupplierId, SourceClassName) values (1, 39, 'AptekaHoldingVoronezhCertificateSource');
+
+
+create table Documents.SourceSuppliers(
+  CertificateSourceId int unsigned not null,
+  SupplierId int unsigned not null,
+  CONSTRAINT FK_SourceSuppliers_CertificateSourceId FOREIGN KEY (CertificateSourceId) REFERENCES Documents.CertificateSources (Id) ON DELETE cascade ON UPDATE cascade,
+  CONSTRAINT FK_SourceSuppliers_SourceSupplierId FOREIGN KEY (SupplierId) REFERENCES future.Suppliers (Id) ON DELETE cascade ON UPDATE cascade
+);
+
+insert into  Documents.SourceSuppliers (CertificateSourceId, SupplierId) values (1, 39);
+
+
+alter table Documents.CertificateTasks
+  modify column SupplierId int unsigned default null;
+
+alter table Documents.CertificateTasks
+  add column CertificateSourceId int unsigned default null after SupplierId,
+  add CONSTRAINT FK_CertificateTasks_CertificateSourceId FOREIGN KEY (CertificateSourceId) REFERENCES Documents.CertificateSources (Id) ON DELETE cascade ON UPDATE cascade;
+
+update Documents.CertificateTasks
+set
+  CertificateSourceId = 1
+where
+  SupplierId = 39;
+
+
+
+create table Documents.FileCertificates(
+  CertificateId int unsigned not null,
+  CertificateFileId int unsigned not null,
+  CONSTRAINT FK_FileCertificates_CertificateId FOREIGN KEY (CertificateId) REFERENCES Documents.Certificates (Id) ON DELETE cascade ON UPDATE cascade,
+  CONSTRAINT FK_FileCertificates_CertificateFileId FOREIGN KEY (CertificateFileId) REFERENCES Documents.CertificateFiles (Id) ON DELETE cascade ON UPDATE cascade
+);
+
+
+insert into Documents.FileCertificates (CertificateId, CertificateFileId)
+select CertificateId, Id
+from Documents.CertificateFiles;
---
+++ migrations/279_01_ChangeCertificateFilesIndex.sql
@@ -0,0 +1,26 @@
+﻿alter table Documents.CertificateFiles
+  drop FOREIGN KEY FK_CertificateFiles_CertificateId,
+  drop FOREIGN KEY FK_CertificateFiles_SupplierId;
+
+
+alter table Documents.CertificateFiles
+  drop index FK_CertificateFiles_CertificateId;
+
+alter table Documents.CertificateFiles
+  drop index FK_CertificateFiles_SupplierId;
+
+
+alter table Documents.CertificateFiles
+  add column ExternalFileId varchar(255) default null,
+  add column CertificateSourceId int unsigned default null,
+  modify column CertificateId int unsigned default null,
+  modify column SupplierId int unsigned default null,
+  add CONSTRAINT FK_CertificateFiles_CertificateSourceId FOREIGN KEY (CertificateSourceId) REFERENCES Documents.CertificateSources (Id) ON DELETE cascade ON UPDATE cascade,
+  add index `IDX_CertificateFiles_ExternalFileId` (ExternalFileId);
+
+update Documents.CertificateFiles
+set
+  ExternalFileId = OriginFileName,
+  CertificateSourceId = 1
+where
+  SupplierId = 39;
