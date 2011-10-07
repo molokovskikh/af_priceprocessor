bba56d8a7ffc54d7e2fbd155e5b8baefe6bba94f
Morozov Sergey <morozov@analit.net>
2011-10-07 11:38:53 +0400

    Изменил уникальный ключ на CertificateTasks.

---
+++ migrations/279_02_ChangeIndexOnCertificateTasks.sql
@@ -0,0 +1,17 @@
+﻿alter table Documents.CertificateTasks
+  drop FOREIGN KEY FK_CertificateTasks_CatalogId,
+  drop FOREIGN KEY FK_CertificateTasks_DocumentBodyId,
+  drop FOREIGN KEY FK_CertificateTasks_SupplierId,
+  drop FOREIGN KEY FK_CertificateTasks_CertificateSourceId;
+
+alter table Documents.CertificateTasks
+  drop index IDX_CertificateTask;
+
+alter table Documents.CertificateTasks
+  drop index FK_CertificateTasks_DocumentBodyId;
+
+alter table Documents.CertificateTasks
+  drop index FK_CertificateTasks_CatalogId;
+
+alter table Documents.CertificateTasks
+  drop index FK_CertificateTasks_CertificateSourceId;
---
+++ migrations/279_03_ChangeUniqueIndexCertificateTasks.sql
@@ -0,0 +1,8 @@
+﻿alter table Documents.CertificateTasks
+  add CONSTRAINT FK_CertificateTasks_CertificateSourceId FOREIGN KEY (CertificateSourceId) REFERENCES Documents.CertificateSources (Id) ON DELETE cascade ON UPDATE cascade,
+  add CONSTRAINT FK_CertificateTasks_CatalogId FOREIGN KEY (CatalogId) REFERENCES catalogs.catalog (Id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  add CONSTRAINT FK_CertificateTasks_DocumentBodyId FOREIGN KEY (DocumentBodyId) REFERENCES Documents.DocumentBodies (Id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+
+alter table Documents.CertificateTasks
+  add UNIQUE KEY `IDX_CertificateTask` (CertificateSourceId, CatalogId, SerialNumber);
