98ca98d7a509d0cd3a6ac0459696815821cebd45
Morozov Sergey <morozov@analit.net>
2011-11-09 12:14:14 +0400

    Корректно переписал миграцию, т.к. она не применялась на пустой базе данных.

--- migrations/306_00_AddSuppliersToCertificate.sql
+++ migrations/306_00_AddSuppliersToCertificate.sql
@@ -1,37 +1,49 @@
 ﻿insert into documents.SourceSuppliers (CertificateSourceId, SupplierId)
 SELECT 
-1,
+cs.Id,
 s.Id
 FROM 
+  (
+  documents.CertificateSources cs,
   future.Suppliers s
-  left join documents.SourceSuppliers ss on s.Id = ss.SupplierId 
+  )
+  left join documents.SourceSuppliers ss on s.Id = ss.SupplierId and ss.CertificateSourceId = cs.Id
 where
-  s.Segment = 0
+    cs.SourceClassName = 'AptekaHoldingVoronezhCertificateSource'
+and s.Segment = 0
 and s.Name like 'Аптека-Холдинг%'
 and ss.CertificateSourceId is null;
 
 
 insert into documents.SourceSuppliers (CertificateSourceId, SupplierId)
 SELECT 
-4,
+cs.Id,
 s.Id
 FROM 
+  (
+  documents.CertificateSources cs,
   future.Suppliers s
-  left join documents.SourceSuppliers ss on s.Id = ss.SupplierId 
+  )
+  left join documents.SourceSuppliers ss on s.Id = ss.SupplierId and ss.CertificateSourceId = cs.Id
 where
-  s.Segment = 0
+    cs.SourceClassName = 'ProtekSource'
+and s.Segment = 0
 and s.Name like 'Протек%'
 and ss.CertificateSourceId is null;
 
 
 insert into documents.SourceSuppliers (CertificateSourceId, SupplierId)
 SELECT 
-2,
+cs.Id,
 s.Id
 FROM 
+  (
+  documents.CertificateSources cs,
   future.Suppliers s
-  left join documents.SourceSuppliers ss on s.Id = ss.SupplierId 
+  )
+  left join documents.SourceSuppliers ss on s.Id = ss.SupplierId and ss.CertificateSourceId = cs.Id
 where
-  s.Segment = 0
+    cs.SourceClassName = 'SiaSource'
+and s.Segment = 0
 and s.Name like 'СИА%'
 and ss.CertificateSourceId is null;
