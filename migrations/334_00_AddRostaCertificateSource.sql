8fce83ea81b873b5fca3a44088c0ed8406c3e2b7
Morozov Sergey <morozov@analit.net>
2011-11-30 15:26:07 +0400

    Исправил #6827 Новшество Загрузка сертификатов по Роста
    добавил миграцию на создание источника сертификатов Роста.

---
+++ migrations/334_00_AddRostaCertificateSource.sql
@@ -0,0 +1,18 @@
+﻿insert into Documents.CertificateSources (SourceClassName) values ('RostaCertificateSource');
+
+insert into documents.SourceSuppliers (CertificateSourceId, SupplierId)
+SELECT 
+cs.Id,
+s.Id
+FROM 
+  (
+  documents.CertificateSources cs,
+  future.Suppliers s
+  )
+  left join documents.SourceSuppliers ss on s.Id = ss.SupplierId and ss.CertificateSourceId = cs.Id
+where
+    cs.SourceClassName = 'RostaCertificateSource'
+and s.Segment = 0
+and s.Name like 'Роста%'
+and s.Disabled = 0
+and ss.CertificateSourceId is null;
