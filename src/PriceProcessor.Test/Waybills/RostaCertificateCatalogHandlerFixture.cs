9b48d9377fa60ab5920ea5dea992956a22b10834
Morozov Sergey <morozov@analit.net>
2011-11-30 13:56:42 +0400

    Новшество #6827 Загрузка сертификатов по Роста
    закончил с тестами для RostaCertificateCatalogHandler.

--- src/PriceProcessor.Test/Waybills/RostaCertificateCatalogHandlerFixture.cs
+++ src/PriceProcessor.Test/Waybills/RostaCertificateCatalogHandlerFixture.cs
@@ -1,10 +1,118 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Castle.ActiveRecord;
+using Inforoom.PriceProcessor.Downloader;
+using Inforoom.PriceProcessor.Models;
+using Inforoom.PriceProcessor.Waybills.CertificateSources;
+using Inforoom.PriceProcessor.Waybills.Models;
 using NUnit.Framework;
+using Test.Support.Suppliers;
 
 namespace PriceProcessor.Test.Waybills
 {
+	public class TestRostaCertificateCatalogHandler : RostaCertificateCatalogHandler
+	{
+
+		public void TestProcessData()
+		{
+			ProcessData();
+		}
+
+		public void TestImportCatalogFile(CertificateCatalogFile catalogFile)
+		{
+			ImportCatalogFile(catalogFile);
+		}
+
+		public CertificateCatalogFile TestGetCatalogFile(CertificateSource source)
+		{
+			return GetCatalogFile(source);
+		}
+
+		public void TestCreateDownHandlerPath()
+		{
+			CreateDownHandlerPath();
+			Cleanup();
+		}
+	}
+
 	[TestFixture]
 	public class RostaCertificateCatalogHandlerFixture
 	{
-		
+		private CertificateSource _source;
+
+		[SetUp]
+		public void SetUp()
+		{
+			var supplier = TestSupplier.Create();
+			var realSupplier = Supplier.Find(supplier.Id);
+
+			using (new TransactionScope()) {
+				var certificateSources = CertificateSource.Queryable.Where(s => s.SourceClassName == typeof(RostaCertificateSource).Name).ToList();
+				certificateSources.ForEach(c => c.Delete());
+
+				_source = new CertificateSource{
+					SourceClassName = typeof(RostaCertificateSource).Name
+				};
+				_source.Suppliers = new List<Supplier>();
+				_source.Suppliers.Add(realSupplier);
+				_source.Create();
+			}
+		}
+
+		[Test(Description = "проверяем заполнение таблицы каталога сертификатов")]
+		public void ImportCatalogFile()
+		{
+			Assert.That(_source.FtpFileDate, Is.Null, "Дата файла с ftp не должна быть заполнена");
+			var catalogs = CertificateSourceCatalog.Queryable.Where(c => c.CertificateSource == _source).ToList();
+			Assert.That(catalogs.Count, Is.EqualTo(0), "Таблица не должна быть заполнена");
+
+			using (new SessionScope()) {
+				var catalogFile = new CertificateCatalogFile{
+					Source = _source,
+					FileDate = DateTime.Now,
+					LocalFileName = Path.GetFullPath(@"..\..\Data\RostaSertList.dbf")
+				};
+
+				var handler = new TestRostaCertificateCatalogHandler();
+				handler.TestImportCatalogFile(catalogFile);
+
+				_source.Refresh();
+				Assert.That(_source.FtpFileDate, Is.Not.Null, "Дата файла с ftp должна быть заполнена");
+				Assert.That(_source.FtpFileDate.Value.Subtract(catalogFile.FileDate).TotalSeconds, Is.LessThan(1), "Дата файла не совпадает");
+
+				var existsCatalogs = CertificateSourceCatalog.Queryable.Where(c => c.CertificateSource == _source).ToList();
+				Assert.That(existsCatalogs.Count, Is.GreaterThan(0), "Таблица должна быть заполнена");
+			}
+		}
+
+		[Test(Description = "проверяем закачку файла с каталогов сертификатов"), Ignore("Проверять надо в ручном режиме")]
+		public void GetCatalogFile()
+		{
+			using (new SessionScope()) {
+
+				var handler = new TestRostaCertificateCatalogHandler();
+				handler.TestCreateDownHandlerPath();
+
+				//Производим первую закачку и закачиваем файл
+				var catalogFile = handler.TestGetCatalogFile(_source);
+
+				Assert.That(catalogFile, Is.Not.Null);
+				Assert.That(catalogFile.Source, Is.EqualTo(_source));
+				Assert.That(catalogFile.FileDate, Is.GreaterThan(DateTime.MinValue));
+				Assert.IsNotNullOrEmpty(catalogFile.LocalFileName);
+				Assert.That(File.Exists(catalogFile.LocalFileName), Is.True);
+
+				File.Delete(catalogFile.LocalFileName);
+
+				//производим вторую закачку и файл не качается, т.к. не обновлен
+				_source.FtpFileDate = catalogFile.FileDate;
+				var newCatalogFile = handler.TestGetCatalogFile(_source);
+
+				Assert.That(newCatalogFile, Is.Null, "Файл не должен быть закачен");
+			}
+		}
+
 	}
 }
--- src/PriceProcessor/Downloader/RostaCertificateCatalogHandler.cs
+++ src/PriceProcessor/Downloader/RostaCertificateCatalogHandler.cs
@@ -1,5 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using Castle.ActiveRecord;
+using Common.Tools;
+using Inforoom.Downloader.Ftp;
+using Inforoom.PriceProcessor.Waybills.CertificateSources;
+using Inforoom.PriceProcessor.Waybills.Models;
+
 namespace Inforoom.PriceProcessor.Downloader
 {
+	public class CertificateCatalogFile
+	{
+		public CertificateSource Source { get; set; }
+		public DateTime FileDate { get; set; }
+		public string LocalFileName { get; set; }
+	}
+
 	public class RostaCertificateCatalogHandler : AbstractHandler
 	{
 		public RostaCertificateCatalogHandler()
@@ -9,20 +27,107 @@ namespace Inforoom.PriceProcessor.Downloader
 		
 		protected override void ProcessData()
 		{
-			//using (new SessionScope()) {
-			//    var tasks = CertificateTask.FindAll();
+			using (new SessionScope()) {
+			    var source = CertificateSource.Queryable.Where(s => s.SourceClassName == typeof(RostaCertificateSource).Name).FirstOrDefault();
 
-			//    if (tasks != null && tasks.Length > 0)
-			//        foreach (var certificateTask in tasks) {
-			//            try {
-			//                ProcessTask(certificateTask);
-			//            }
-			//            catch (Exception exception) {
-			//                _logger.WarnFormat("Ошибка при отбработки задачи для сертификата {0} : {1}", certificateTask, exception);
-			//            }
-					
-			//        }
-			//}
+				if (source != null) {
+					Cleanup();
+
+					var catalogFile = GetCatalogFile(source);
+
+					try {
+						if (catalogFile != null)
+							ImportCatalogFile(catalogFile);
+					}
+					finally {
+						if (File.Exists(catalogFile.LocalFileName))
+							File.Delete(catalogFile.LocalFileName);
+					}
+				}
+			}
+		}
+
+		protected virtual void ImportCatalogFile(CertificateCatalogFile catalogFile)
+		{
+			var catalogTable = Dbf.Load(catalogFile.LocalFileName);
+			var catalogList = new List<CertificateSourceCatalog>();
+			foreach (DataRow row in catalogTable.Rows) {
+				var catalog = new CertificateSourceCatalog {
+					CertificateSource = catalogFile.Source,
+					SerialNumber = row["BATCH_ID"].ToString(),
+					SupplierCode = row["CODE"].ToString(),
+					OriginFilePath = row["GB_FILES"].ToString()
+				};
+				catalogList.Add(catalog);
+			}
+
+			using (var transaction = new TransactionScope(OnDispose.Rollback)) {
+
+				var session = ActiveRecordMediator.GetSessionFactoryHolder().CreateSession(typeof (ActiveRecordBase));
+				try
+				{
+					session.CreateSQLQuery(@"
+	delete from
+		documents.CertificateSourceCatalogs
+	where
+		CertificateSourceId = :certificateSourceId
+		")
+						.SetParameter("certificateSourceId", catalogFile.Source.Id)
+						.ExecuteUpdate();
+				}
+				finally
+				{
+					ActiveRecordMediator.GetSessionFactoryHolder().ReleaseSession(session);
+				}
+
+				catalogList.ForEach(c => c.Create());
+
+				var sessionUpdate = ActiveRecordMediator.GetSessionFactoryHolder().CreateSession(typeof (ActiveRecordBase));
+				try
+				{
+					sessionUpdate.CreateSQLQuery(@"
+	update
+		documents.CertificateSources
+	set
+		FtpFileDate = :FtpFileDate
+	where
+		Id = :certificateSourceId
+		")
+						.SetParameter("certificateSourceId", catalogFile.Source.Id)
+						.SetParameter("FtpFileDate", catalogFile.FileDate)
+						.ExecuteUpdate();
+				}
+				finally
+				{
+					ActiveRecordMediator.GetSessionFactoryHolder().ReleaseSession(sessionUpdate);
+				}
+
+				transaction.VoteCommit();
+			}
+		}
+
+		protected virtual CertificateCatalogFile GetCatalogFile(CertificateSource source)
+		{
+			var downloader = new FtpDownloader();
+
+			var downloadFiles = downloader.GetFilesFromSource(
+				Settings.Default.RostaCertificateFtp,
+				21,
+				"LIST",
+				Settings.Default.RostaCertificateFtpUserName,
+				Settings.Default.RostaCertificateFtpPassword,
+				"SERT_LIST.DBF",
+				source.FtpFileDate.HasValue ? source.FtpFileDate.Value : DateTime.MinValue,
+				DownHandlerPath);
+
+			if (downloadFiles.Count > 0)
+				return new CertificateCatalogFile{
+					Source = source,
+					FileDate = downloadFiles[0].FileDate,
+					LocalFileName = downloadFiles[0].FileName
+				};
+
+			return null;
 		}
 	}
 }
