fee7c60d10eea9378f86cd1aab03c20b86d6af58
Morozov Sergey <morozov@analit.net>
2011-11-30 15:15:33 +0400

    Новшество #6827 Загрузка сертификатов по Роста
    написал тесты для RostaCertificateSource.

--- src/PriceProcessor.Test/Waybills/Sources/RostaCertificateSourceFixture.cs
+++ src/PriceProcessor.Test/Waybills/Sources/RostaCertificateSourceFixture.cs
@@ -1,10 +1,103 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Castle.ActiveRecord;
+using Inforoom.PriceProcessor.Models;
+using Inforoom.PriceProcessor.Waybills.CertificateSources;
+using Inforoom.PriceProcessor.Waybills.Models;
+using NUnit.Framework;
+using Test.Support.Suppliers;
 
 namespace PriceProcessor.Test.Waybills.Sources
 {
 	[TestFixture]
 	public class RostaCertificateSourceFixture
 	{
+		private CertificateSource _source;
+
+		[SetUp]
+		public void SetUp()
+		{
+			var supplier = TestSupplier.Create();
+			var realSupplier = Supplier.Find(supplier.Id);
+
+			using (new TransactionScope()) {
+				var certificateSources = CertificateSource.Queryable.Where(s => s.SourceClassName == typeof(RostaCertificateSource).Name).ToList();
+				certificateSources.ForEach(c => c.Delete());
+
+				_source = new CertificateSource{
+					SourceClassName = typeof(RostaCertificateSource).Name
+				};
+				_source.Suppliers = new List<Supplier>();
+				_source.Suppliers.Add(realSupplier);
+				_source.Create();
+			}
+		}
+
+		[Test]
+		public void CertificateExists()
+		{
+			var rostaSource = new RostaCertificateSource();
+
+			var line = new DocumentLine {
+				Code = "22651",
+				SerialNumber = "835495"
+			};
+
+			Assert.That(rostaSource.CertificateExists(line), Is.False);
+
+			using (new TransactionScope(OnDispose.Commit)) {
+				var catalog = new CertificateSourceCatalog {
+					CertificateSource = _source,
+					SerialNumber = line.SerialNumber,
+					SupplierCode = line.Code,
+					OriginFilePath = Path.GetRandomFileName()
+				};
+				catalog.Create();
+			}
+
+			Assert.That(rostaSource.CertificateExists(line), Is.True);
+		}
+
+		[Test, Ignore("Для ручной проверки что бы не долбить сервер Rosta")]
+		public void TestGetFiles()
+		{
+			var rostaSource = new RostaCertificateSource();
+			var task = new CertificateTask();
+			task.CertificateSource = _source;
+			task.DocumentLine = new DocumentLine {
+			    Code = "000002",
+			    SerialNumber = "C392764"
+			};
+
+			using (new TransactionScope(OnDispose.Commit)) {
+				var catalog = new CertificateSourceCatalog {
+					CertificateSource = _source,
+					SerialNumber = task.DocumentLine.SerialNumber,
+					SupplierCode = task.DocumentLine.Code,
+					OriginFilePath = "005/0052602p-0.gif"
+				};
+				catalog.Create();
+
+				catalog = new CertificateSourceCatalog {
+					CertificateSource = _source,
+					SerialNumber = task.DocumentLine.SerialNumber,
+					SupplierCode = task.DocumentLine.Code,
+					OriginFilePath = "005/0052602pd-0.gif"
+				};
+				catalog.Create();
+			}
+
+			var files = rostaSource.GetCertificateFiles(task);
+
+			Assert.That(files.Count, Is.EqualTo(2));
+			var file = files[0];
+			Assert.That(File.Exists(file.LocalFile), Is.True, "файл не существует {0}", file.LocalFile);
+			Assert.That(file.ExternalFileId, Is.EqualTo(@"005/0052602p-0.gif"));
+			Assert.That(file.OriginFilename, Is.EqualTo(@"0052602p-0.gif"));
+			Assert.That(file.Extension, Is.EqualTo(".GIF").IgnoreCase);
+		}
+
 		
 	}
 }
--- src/PriceProcessor/Waybills/CertificateSources/RostaCertificateSource.cs
+++ src/PriceProcessor/Waybills/CertificateSources/RostaCertificateSource.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Inforoom.Downloader.Ftp;
 using Inforoom.PriceProcessor.Waybills.Models;
 
 namespace Inforoom.PriceProcessor.Waybills.CertificateSources
@@ -8,12 +11,85 @@ namespace Inforoom.PriceProcessor.Waybills.CertificateSources
 	{
 		public bool CertificateExists(DocumentLine line)
 		{
-			throw new NotImplementedException();
+			if (!String.IsNullOrWhiteSpace(line.Code)) {
+				var catalog = CertificateSourceCatalog.Queryable
+					.Where(
+						c =>
+						c.CertificateSource.SourceClassName == this.GetType().Name && c.SerialNumber == line.SerialNumber &&
+						c.SupplierCode == line.Code)
+					.FirstOrDefault();
+				return catalog != null;
+			}
+
+			return false;
 		}
 
 		public IList<CertificateFile> GetCertificateFiles(CertificateTask task)
 		{
-			throw new NotImplementedException();
+			var result = new List<CertificateFile>();
+
+			var catalogs = CertificateSourceCatalog.Queryable
+				.Where(
+					c =>
+					c.CertificateSource.Id == task.CertificateSource.Id
+					&& c.SerialNumber == task.DocumentLine.SerialNumber
+					&& c.SupplierCode == task.DocumentLine.Code)
+				.ToList();
+
+			if (catalogs.Count > 0) {
+				var tempDowloadDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+				if (!Directory.Exists(tempDowloadDir))
+					Directory.CreateDirectory(tempDowloadDir);
+
+				var downloader = new FtpDownloader();
+
+				foreach (var certificateSourceCatalog in catalogs) {
+					var dirName = ExtractFtpDir(certificateSourceCatalog.OriginFilePath);
+					var fileName = ExtractFileName(certificateSourceCatalog.OriginFilePath);
+
+					var downloadFiles = downloader.GetFilesFromSource(
+						Settings.Default.RostaCertificateFtp,
+						21,
+						dirName,
+						Settings.Default.RostaCertificateFtpUserName,
+						Settings.Default.RostaCertificateFtpPassword,
+						fileName,
+						DateTime.MinValue,
+						tempDowloadDir);
+
+					if (downloadFiles.Count > 0)
+						result.Add(new CertificateFile(
+							downloadFiles[0].FileName,
+							certificateSourceCatalog.OriginFilePath,
+							fileName,
+							task.CertificateSource));
+
+				}
+
+			}
+
+			return result;
 		}
+
+		private string ExtractFileName(string originFilePath)
+		{
+			var result = originFilePath;
+
+			if (result.IndexOf("/") > -1)
+				result = result.Substring(result.IndexOf("/") + 1);
+
+			return result;
+		}
+
+		private string ExtractFtpDir(string originFilePath)
+		{
+			var result = String.Empty;
+
+			if (originFilePath.IndexOf("/") > -1)
+				result = originFilePath.Substring(0, originFilePath.IndexOf("/"));
+
+			return result;
+		}
+
 	}
 }
