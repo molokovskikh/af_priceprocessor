ade75daad59b2608e90b6093ff431e13177d3373
Morozov Sergey <morozov@analit.net>
2011-12-30 11:35:33 +0400

    Новшество #7747 Настройка контактной группы отправителей для мини-почта
    добавил контактную группу для поставщиков плательщика 921.

---
+++ migrations/355_00_AddMiniMailsTo921.sql
@@ -0,0 +1,14 @@
+﻿insert into contacts.contact_groups
+(Name, Type, Public, ContactGroupOwnerId, Specialized)
+select
+'Список E-mail, с которых разрешена отправка писем клиентам АналитФармация' as Name,
+10 as Type,
+1 as Public,
+Suppliers.ContactGroupOwnerId,
+0 as Specialized
+from
+future.Suppliers
+left join contacts.contact_groups on contact_groups.ContactGroupOwnerId = Suppliers.ContactGroupOwnerId and contact_groups.Type = 10
+where
+Suppliers.Payer = 921
+and contact_groups.Id is null;
