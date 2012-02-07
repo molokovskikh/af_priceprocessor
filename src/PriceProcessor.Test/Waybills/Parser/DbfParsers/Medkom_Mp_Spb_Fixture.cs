85318a03aa6cef6926f6949bda85cfb9a44106c2
Золотарев Александр <a.zolotarev@analit.net>
2012-02-07 10:48:00 +0400

    Исправил #8203 Накладные для Медком-МП СПб

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/Medkom_Mp_Spb_Fixture.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Parser.DbfParsers;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	public class Medkom_Mp_Spb_Fixture
+	{
+		[Test]
+		public void Parse()
+		{
+			Assert.IsTrue(Medkom_Mp_Spb.CheckFileFormat(Dbf.Load(@"..\..\Data\Waybills\6792.dbf")));
+			var document = WaybillParser.Parse("6792.dbf");
+
+			Assert.That(document.Lines.Count, Is.EqualTo(28));
+			Assert.That(document.ProviderDocumentId, Is.EqualTo("СКЛ-006792"));
+			Assert.That(document.DocumentDate.Value.ToShortDateString(), Is.EqualTo("30.01.2012"));
+			
+			var invoice = document.Invoice;
+			Assert.That(invoice, Is.Not.Null);
+			Assert.That(invoice.InvoiceNumber, Is.EqualTo("СКЛ-006792"));
+			Assert.That(invoice.InvoiceDate.Value.ToShortDateString(), Is.EqualTo("30.01.2012"));
+			Assert.That(invoice.ConsigneeInfo, Is.EqualTo("ООО \"УК Здоровые Люди\",196105, Санкт-Петербург г, Московский пр-кт,  дом 143 лит.А 12Н"));
+
+			var line = document.Lines[0];
+			Assert.That(line.Code, Is.EqualTo("ММ-1016160"));
+			Assert.That(line.Product, Is.EqualTo("DUREX (lubr)  Play O 15ml"));
+			Assert.That(line.ProducerCostWithoutNDS, Is.EqualTo(423.73));
+			Assert.That(line.SupplierCost, Is.EqualTo(500));
+			Assert.That(line.Nds, Is.EqualTo(18));
+			Assert.That(line.EAN13, Is.EqualTo("0"));
+			Assert.That(line.Quantity, Is.EqualTo(2));
+			Assert.That(line.Amount, Is.EqualTo(1000));
+			Assert.That(line.NdsAmount, Is.EqualTo(152.54));
+			Assert.That(line.Producer, Is.Null);
+			Assert.That(line.Country, Is.EqualTo("ВЕЛИКОБРИТАНИЯ"));
+			Assert.That(line.BillOfEntryNumber, Is.EqualTo("10130070/210611/0013791/1"));
+			Assert.That(line.Period, Is.EqualTo("31.01.2015"));
+			Assert.That(line.SerialNumber, Is.Null);
+			Assert.That(line.VitallyImportant, Is.False);
+			Assert.That(line.Certificates, Is.EqualTo("РОСС GB.АЯ46.Д39193"));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/Medkom_Mp_Spb.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Models;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class Medkom_Mp_Spb : IDocumentParser
+	{
+		protected Encoding Encoding = Encoding.GetEncoding(866);
+
+		public Document Parse(string file, Document document)
+		{
+			var data = Dbf.Load(file, Encoding);
+
+			new DbfParser()
+				.DocumentHeader(h => h.ProviderDocumentId, "NDOC")
+				.DocumentHeader(h => h.DocumentDate, "DATEDOC")
+
+				.DocumentInvoice(i=> i.InvoiceNumber, "BILLNUM")
+				.DocumentInvoice(i=> i.InvoiceDate, "BILLDT")
+				.DocumentInvoice(i=> i.ConsigneeInfo, "PODR")
+
+				.Line(l => l.Code, "CODEPST")
+				.Line(l => l.Product, "NAME")
+				.Line(l => l.Producer, "FIRM")
+				.Line(l => l.Country, "CNTR")
+				.Line(l => l.SupplierCost, "PRICE")
+				.Line(l => l.ProducerCostWithoutNDS, "PRICEMAN")
+
+				.Line(l => l.Amount, "SUMSNDS")
+				.Line(l => l.NdsAmount, "SUMNDS")
+
+				.Line(l => l.Quantity, "QNT")
+
+				.Line(l => l.Period, "GDATE")
+				.Line(l => l.Certificates, "SERTIF")
+				.Line(l => l.SerialNumber, "SER")
+				.Line(l => l.EAN13, "EAN13")
+				.Line(l => l.BillOfEntryNumber, "GTD")
+
+				.Line(l => l.Nds, "NDS")
+				.Line(l => l.VitallyImportant, "ISLIFE")
+				.ToDocument(document, data);
+
+			return document;
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("BILLDT") &&
+				   data.Columns.Contains("BILLNUM") &&
+				   data.Columns.Contains("PODR") &&
+				   data.Columns.Contains("CODEPST") &&
+				   data.Columns.Contains("PRICEMAN") &&
+				   data.Columns.Contains("ISLIFE");
+		}
+	}
+}
