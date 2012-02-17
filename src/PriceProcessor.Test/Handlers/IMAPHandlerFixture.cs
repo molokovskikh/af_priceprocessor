eaf680f8f21aafa1ef3bd8d4e00cfbb1d15db1d3
Morozov Sergey <morozov@analit.net>
2012-02-17 14:09:06 +0400

    Ошибка #4880 Критические сообщения (отказы, накладные)
    первая версия IMAPHandler.

---
+++ src/PriceProcessor.Test/Handlers/IMAPHandlerFixture.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using Common.Tools;
+using Inforoom.PriceProcessor;
+using Inforoom.PriceProcessor.Downloader;
+using LumiSoft.Net.IMAP.Client;
+using LumiSoft.Net.Mime;
+using NUnit.Framework;
+using PriceProcessor.Test.TestHelpers;
+using Rhino.Mocks;
+
+namespace PriceProcessor.Test.Handlers
+{
+	[TestFixture]
+	public class IMAPHandlerFixture
+	{
+
+		interface ITestCall
+		{
+			 void ImplementationMethod();
+		}
+
+		class BaseClass : ITestCall
+		{
+			public int T = 0;
+
+			public virtual void ImplementationMethod()
+			{
+				T = 1;
+			}
+
+			public ITestCall GetInterface()
+			{
+				return this;
+			}
+		}
+
+		class ChildClass : BaseClass
+		{
+			public int C = 2;
+
+			public override void ImplementationMethod()
+			{
+				C = 3;
+			}
+		}
+
+		[Test(Description = "проверка того, какой метод будет вызываться, если метод интефейса переопределен потомком")]
+		public void InterfaceCallMethod()
+		{
+			var parent = new BaseClass();
+			Assert.That(parent.T, Is.EqualTo(0));
+			var testCall = parent.GetInterface();
+			testCall.ImplementationMethod();
+			Assert.That(parent.T, Is.EqualTo(1));
+
+			var child = new ChildClass();
+			Assert.That(child.T, Is.EqualTo(0));
+			Assert.That(child.C, Is.EqualTo(2));
+			testCall = child.GetInterface();
+			testCall.ImplementationMethod();
+			Assert.That(child.T, Is.EqualTo(0));
+			Assert.That(child.C, Is.EqualTo(3));
+		}
+
+		[Test(Description = "простая проверка работоспособности класс IMAPHandler")]
+		public void SimpleProcessImap()
+		{
+			ImapHelper.ClearImapFolder(Settings.Default.TestIMAPUser, Settings.Default.TestIMAPPass, Settings.Default.IMAPSourceFolder);
+			ImapHelper.StoreMessage(@"..\..\Data\Unparse.eml");
+
+			var imapReader = MockRepository.GenerateStub<IIMAPReader>();
+			imapReader.Stub(s => s.IMAPAuth(null))
+				.IgnoreArguments()
+				.Do(new Action<IMAP_Client>(client => {client.Authenticate(Settings.Default.TestIMAPUser, Settings.Default.TestIMAPPass);}) );
+
+			var handler = new IMAPHandler(imapReader);
+
+			handler.ProcessIMAPFolder();
+
+			imapReader.AssertWasCalled(r => r.IMAPAuth(Arg<IMAP_Client>.Is.Anything));
+			imapReader.AssertWasCalled(r => r.Ping());
+			imapReader.AssertWasCalled(r => r.ProcessMime(Arg<Mime>.Is.Anything));
+			imapReader.AssertWasNotCalled(r => r.ProcessBrokenMessage(Arg<IMAP_FetchItem>.Is.Anything, Arg<IMAP_FetchItem[]>.Is.Anything, Arg<Exception>.Is.Anything));
+
+			var existsMessages = ImapHelper.CheckImapFolder(Settings.Default.TestIMAPUser, Settings.Default.TestIMAPPass, Settings.Default.IMAPSourceFolder);
+			Assert.That(existsMessages.Count, Is.EqualTo(0), "Существуют письма в IMAP-папками с темами: {0}", existsMessages.Select(m => m.Envelope.Subject).Implode());
+		}
+		 
+	}
+}
---
+++ src/PriceProcessor/Downloader/IMAPHandler.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Linq;
+using LumiSoft.Net.IMAP;
+using LumiSoft.Net.IMAP.Client;
+using LumiSoft.Net.Mime;
+
+namespace Inforoom.PriceProcessor.Downloader
+{
+	public interface IIMAPReader
+	{
+		void IMAPAuth(IMAP_Client client);
+		void ProcessMime(Mime mime);
+		void ProcessBrokenMessage(IMAP_FetchItem item, IMAP_FetchItem[] oneItem, Exception ex);
+		void Ping();
+	}
+
+	public class IMAPHandler
+	{
+		public IIMAPReader ImapReader { get; private set; }
+
+		//Текущее обрабатываемое письмо
+		public Mime Message { get; private set; }
+
+		//UID текущего обрабатываемого письма
+		public int CurrentUID { get; private set; }
+
+		private IMAP_Client _imapClient;
+
+		public IMAPHandler(IIMAPReader imapReader)
+		{
+			if (imapReader == null)
+				throw new ArgumentNullException("imapReader");
+			ImapReader = imapReader;
+		}
+
+		public void ConnectToIMAP()
+		{
+			_imapClient.Connect(Settings.Default.IMAPHost, 143);
+			ImapReader.IMAPAuth(_imapClient);
+			_imapClient.SelectFolder(Settings.Default.IMAPSourceFolder);
+		}
+
+		public IMAP_FetchItem[] FetchUIDs()
+		{
+			var sequenceUids = new IMAP_SequenceSet();
+			sequenceUids.Parse("1:*", long.MaxValue);
+			return _imapClient.FetchMessages(sequenceUids, IMAP_FetchItem_Flags.UID, false, false);
+		}
+
+		public IMAP_FetchItem[] FetchMessages(int uid)
+		{
+			var sequenceMessages = new IMAP_SequenceSet();
+			sequenceMessages.Parse(uid.ToString(), long.MaxValue);
+			return _imapClient.FetchMessages(sequenceMessages, IMAP_FetchItem_Flags.Message, false, true);
+		}
+
+		public void ProcessIMAPFolder()
+		{
+			using (var imapClient = new IMAP_Client())
+			{
+				_imapClient = imapClient;
+				ConnectToIMAP();
+
+				try
+				{
+					IMAP_FetchItem[] items;
+					do
+					{
+						ImapReader.Ping();
+
+						items = FetchUIDs();
+
+						ImapReader.Ping();
+
+						if (items == null || items.Length == 0)
+							continue;
+
+						foreach (var item in items)
+						{
+							IMAP_FetchItem[] OneItem = null;
+							try
+							{
+								OneItem = FetchMessages(item.UID);
+
+								Message = Mime.Parse(OneItem[0].MessageData);
+
+								CurrentUID = item.UID;
+
+								ImapReader.Ping();
+								ImapReader.ProcessMime(Message);
+							}
+							catch (Exception ex)
+							{
+								Message = null;
+								ImapReader.ProcessBrokenMessage(item, OneItem, ex);
+								
+								//_logger.Error("Ошибка при обработке письма", ex);
+								//_message = null;
+								//SendBrokenMessage(item, OneItem, ex);
+							}
+						}
+
+						//Производим удаление писем
+						var sequence = new IMAP_SequenceSet();
+
+						sequence.Parse(String.Join(",", items.Select(i => i.UID.ToString()).ToArray()), long.MaxValue);
+						imapClient.DeleteMessages(sequence, true);
+					}
+					while (items != null && items.Length > 0);
+				}
+				finally
+				{
+					Message = null;
+				}
+			}
+		}
+ 
+	}
+}
