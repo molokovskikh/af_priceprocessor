0db881c35038cf624e1a7771d51eca545350855e
Morozov Sergey <morozov@analit.net>
2012-02-20 11:24:16 +0400

    Исправил #4880 Критические сообщения (отказы, накладные)
    покрыл тестом новую функциональность.

--- src/PriceProcessor.Test/Handlers/IMAPHandlerFixture.cs
+++ src/PriceProcessor.Test/Handlers/IMAPHandlerFixture.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Common.Tools;
 using Inforoom.PriceProcessor;
 using Inforoom.PriceProcessor.Downloader;
+using LumiSoft.Net.IMAP;
 using LumiSoft.Net.IMAP.Client;
 using LumiSoft.Net.Mime;
 using NUnit.Framework;
@@ -110,10 +111,105 @@ namespace PriceProcessor.Test.Handlers
 			imapReader.AssertWasCalled(r => r.IMAPAuth(Arg<IMAP_Client>.Is.Anything));
 			imapReader.AssertWasCalled(r => r.PingReader());
 			imapReader.AssertWasCalled(r => r.ProcessMime(Arg<Mime>.Is.Anything));
-			imapReader.AssertWasCalled(r => r.ProcessBrokenMessage(Arg<IMAP_FetchItem>.Is.Anything, Arg<IMAP_FetchItem[]>.Is.Anything, Arg<Exception>.Is.Equal(exception)));
+			imapReader.AssertWasNotCalled(r => r.ProcessBrokenMessage(Arg<IMAP_FetchItem>.Is.Anything, Arg<IMAP_FetchItem[]>.Is.Anything, Arg<Exception>.Is.Equal(exception)));
 
 			var existsMessages = ImapHelper.CheckImapFolder(Settings.Default.TestIMAPUser, Settings.Default.TestIMAPPass, Settings.Default.IMAPSourceFolder);
-			Assert.That(existsMessages.Count, Is.EqualTo(0), "Существуют письма в IMAP-папками с темами: {0}", existsMessages.Select(m => m.Envelope.Subject).Implode());
+			Assert.That(existsMessages.Count, Is.EqualTo(1), "Письмо было удалено сразу же после возникнования ошибок");
 		}
+
+		public class UIDInfoForTesting : UIDInfo
+		{
+			 public UIDInfoForTesting() : base(1)
+			 {
+			 	
+			 }
+		}
+
+		[Test(Description = "проверка срабатывания таймаута при обработке писем")]
+		public void CheckUIDTimeout()
+		{
+			var createTime = DateTime.Now;
+			var info = MockRepository.GenerateMock<UIDInfoForTesting>();
+
+			var imapReader = MockRepository.GenerateStub<IIMAPReader>();
+			var handler = new IMAPHandler(imapReader);
+
+			info.Stub(i => i.CreateTime).Return(createTime);
+
+			Assert.That(handler.UIDTimeout(info), Is.False);
+
+			createTime = createTime.AddMinutes(-(Settings.Default.UIDProcessTimeout+1));
+
+			info.BackToRecord(BackToRecordOptions.PropertyBehavior);
+			SetupResult.For(info.CreateTime).Return(createTime);
+			info.Replay();
+
+			Assert.That(handler.UIDTimeout(info), Is.True);
+		}
+
+		[Test(Description = "при возникновении ошибок письма должны обрабатываться несколько раз пока не возникнет таймаут")]
+		public void ProcessMessageWithTimeoutOnError()
+		{
+			ImapHelper.ClearImapFolder(Settings.Default.TestIMAPUser, Settings.Default.TestIMAPPass, Settings.Default.IMAPSourceFolder);
+			ImapHelper.StoreMessage(@"..\..\Data\Unparse.eml");
+
+			var imapReader = MockRepository.GenerateStub<IIMAPReader>();
+			imapReader.Stub(s => s.IMAPAuth(null))
+				.IgnoreArguments()
+				.Do(new Action<IMAP_Client>(client => {client.Authenticate(Settings.Default.TestIMAPUser, Settings.Default.TestIMAPPass);}) );
+
+			var exception = new Exception("ошибка при разборе письма в reader'е");
+
+			imapReader.Stub(s => s.ProcessMime(null))
+				.IgnoreArguments()
+				.Do(new Action<Mime>(mime => {throw exception;}) );
+
+			var handler = new IMAPHandler(imapReader);
+
+			//Обрабатываем письмо первый раз: оно должно попасть в ErrorInfos
+			Assert.That(handler.ErrorInfos.Count, Is.EqualTo(0));
+
+			handler.ProcessIMAPFolder();
+
+			imapReader.AssertWasCalled(r => r.IMAPAuth(Arg<IMAP_Client>.Is.Anything));
+			imapReader.AssertWasCalled(r => r.PingReader());
+			imapReader.AssertWasCalled(r => r.ProcessMime(Arg<Mime>.Is.Anything));
+			imapReader.AssertWasNotCalled(r => r.ProcessBrokenMessage(Arg<IMAP_FetchItem>.Is.Anything, Arg<IMAP_FetchItem[]>.Is.Anything, Arg<Exception>.Is.Equal(exception)));
+
+			var existsMessages = ImapHelper.CheckImapFolder(Settings.Default.TestIMAPUser, Settings.Default.TestIMAPPass, Settings.Default.IMAPSourceFolder);
+			Assert.That(existsMessages.Count, Is.EqualTo(1), "Письмо было удалено");
+
+			Assert.That(handler.ErrorInfos.Count, Is.EqualTo(1));
+
+			//Обрабатываем письмо второй раз: оно не должно быть обработано
+			handler.ProcessIMAPFolder();
+
+			imapReader.AssertWasCalled(r => r.ProcessMime(Arg<Mime>.Is.Anything), options => options.Repeat.Times(2));
+			imapReader.AssertWasNotCalled(r => r.ProcessBrokenMessage(Arg<IMAP_FetchItem>.Is.Anything, Arg<IMAP_FetchItem[]>.Is.Anything, Arg<Exception>.Is.Equal(exception)));
+
+			existsMessages = ImapHelper.CheckImapFolder(Settings.Default.TestIMAPUser, Settings.Default.TestIMAPPass, Settings.Default.IMAPSourceFolder);
+			Assert.That(existsMessages.Count, Is.EqualTo(1), "Письмо было удалено");
+
+			Assert.That(handler.ErrorInfos.Count, Is.EqualTo(1));
+
+			//Подменяем информацию о письме, чтобы сработал таймаут и в третий раз обрабатываем письмо: оно должно обработаться
+			var realInfo = handler.ErrorInfos[0];
+			var info = MockRepository.GenerateMock<UIDInfoForTesting>();
+			info.Stub(i => i.UID).Return(realInfo.UID);
+			info.Stub(i => i.CreateTime).Return(DateTime.Now.AddMinutes(-(Settings.Default.UIDProcessTimeout+1)));
+			handler.ErrorInfos.Clear();
+			handler.ErrorInfos.Add(info);
+
+			handler.ProcessIMAPFolder();
+
+			imapReader.AssertWasCalled(r => r.ProcessMime(Arg<Mime>.Is.Anything), options => options.Repeat.Times(3));
+			imapReader.AssertWasCalled(r => r.ProcessBrokenMessage(Arg<IMAP_FetchItem>.Is.Anything, Arg<IMAP_FetchItem[]>.Is.Anything, Arg<Exception>.Is.Equal(exception)));
+
+			existsMessages = ImapHelper.CheckImapFolder(Settings.Default.TestIMAPUser, Settings.Default.TestIMAPPass, Settings.Default.IMAPSourceFolder);
+			Assert.That(existsMessages.Count, Is.EqualTo(0), "Существуют письма в IMAP-папками с темами: {0}", existsMessages.Select(m => m.Envelope.Subject).Implode());
+
+			Assert.That(handler.ErrorInfos.Count, Is.EqualTo(0));
+		}
+
 	}
 }
--- src/PriceProcessor/Downloader/IMAPHandler.cs
+++ src/PriceProcessor/Downloader/IMAPHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using LumiSoft.Net.IMAP;
 using LumiSoft.Net.IMAP.Client;
@@ -14,9 +15,24 @@ namespace Inforoom.PriceProcessor.Downloader
 		void PingReader();
 	}
 
+	public class UIDInfo
+	{
+		public virtual int UID { get; private set; }
+
+		public virtual DateTime CreateTime { get; private set; }
+
+		public UIDInfo(int uid)
+		{
+			UID = uid;
+			CreateTime = DateTime.Now;
+		}
+	}
+
 	public class IMAPHandler
 	{
 		public IIMAPReader ImapReader { get; private set; }
+
+		public List<UIDInfo> ErrorInfos { get; set; }
 
 		//Текущее обрабатываемое письмо
 		public Mime Message { get; private set; }
@@ -31,6 +47,7 @@ namespace Inforoom.PriceProcessor.Downloader
 			if (imapReader == null)
 				throw new ArgumentNullException("imapReader");
 			ImapReader = imapReader;
+			ErrorInfos = new List<UIDInfo>();
 		}
 
 		public void ConnectToIMAP()
@@ -54,6 +71,11 @@ namespace Inforoom.PriceProcessor.Downloader
 			return _imapClient.FetchMessages(sequenceMessages, IMAP_FetchItem_Flags.Message, false, true);
 		}
 
+		public bool UIDTimeout(UIDInfo info)
+		{
+			return DateTime.Now.Subtract(info.CreateTime).TotalMinutes > Settings.Default.UIDProcessTimeout;
+		}
+
 		public void ProcessIMAPFolder()
 		{
 			using (var imapClient = new IMAP_Client())
@@ -64,8 +86,11 @@ namespace Inforoom.PriceProcessor.Downloader
 				try
 				{
 					IMAP_FetchItem[] items;
+					var toDelete = new List<IMAP_FetchItem>();
 					do
 					{
+						toDelete.Clear();
+
 						ImapReader.PingReader();
 
 						items = FetchUIDs();
@@ -88,21 +113,29 @@ namespace Inforoom.PriceProcessor.Downloader
 
 								ImapReader.PingReader();
 								ImapReader.ProcessMime(Message);
+								toDelete.Add(item);
 							}
 							catch (Exception ex)
 							{
 								Message = null;
-								ImapReader.ProcessBrokenMessage(item, OneItem, ex);
+								var errorInfo = GetErrorInfo(item.UID);
+								if (UIDTimeout(errorInfo)) {
+									ErrorInfos.Remove(errorInfo);
+									toDelete.Add(item);
+									ImapReader.ProcessBrokenMessage(item, OneItem, ex);
+								}
 							}
 						}
 
 						//Производим удаление писем
-						var sequence = new IMAP_SequenceSet();
+						if (toDelete.Count > 0) {
+							var sequence = new IMAP_SequenceSet();
 
-						sequence.Parse(String.Join(",", items.Select(i => i.UID.ToString()).ToArray()), long.MaxValue);
-						imapClient.DeleteMessages(sequence, true);
+							sequence.Parse(String.Join(",", toDelete.Select(i => i.UID.ToString()).ToArray()), long.MaxValue);
+							imapClient.DeleteMessages(sequence, true);
+						}
 					}
-					while (items != null && items.Length > 0);
+					while (items != null && items.Length > 0 && toDelete.Count > 0);
 				}
 				finally
 				{
@@ -110,6 +143,15 @@ namespace Inforoom.PriceProcessor.Downloader
 				}
 			}
 		}
- 
+
+		private UIDInfo GetErrorInfo(int uid)
+		{
+			var info = ErrorInfos.FirstOrDefault(i => i.UID == uid);
+			if (info == null) {
+				info = new UIDInfo(uid);
+				ErrorInfos.Add(info);
+			}
+			return info;
+		}
 	}
 }
