dceaa2a78db7a93bd3d29c46737c78e5e7f3c747
Золотарев Александр <a.zolotarev@analit.net>
2012-02-21 13:12:24 +0400

    ВЫполнил #8547 Формат для Вазакор, код 144

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/Vazakor_144_Fixture.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Parser.DbfParsers;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	public class Vazakor_144_Fixture
+	{
+		[Test]
+		public void Parse()
+		{
+			Assert.IsTrue(Vazakor_144_Parser.CheckFileFormat(Dbf.Load(@"..\..\Data\Waybills\00387_1.DBF")));
+			var document = WaybillParser.Parse("00387_1.DBF");
+
+			Assert.That(document.Lines.Count, Is.EqualTo(1));
+			Assert.That(document.ProviderDocumentId, Is.EqualTo("0000000387"));
+			Assert.That(document.DocumentDate.Value.ToShortDateString(), Is.EqualTo("20.02.2012"));
+			
+			var invoice = document.Invoice;
+			Assert.That(invoice, Is.Not.Null);
+			Assert.That(invoice.BuyerName, Is.EqualTo("Акватик ООО"));
+			Assert.That(invoice.SellerName, Is.EqualTo("ООО \"ВАЗАКОР\""));
+
+			var line = document.Lines[0];
+			Assert.That(line.Code, Is.EqualTo("132"));
+			Assert.That(line.Product, Is.EqualTo("Шприц 1 мл инсулиновый HELMJECT U-40/100"));
+			Assert.That(line.Unit, Is.EqualTo("шт"));
+			Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(2.94));
+			Assert.That(line.SupplierCost, Is.EqualTo(3.23));
+			Assert.That(line.Nds, Is.EqualTo(10));
+			Assert.That(line.Quantity, Is.EqualTo(200));
+			Assert.That(line.Amount, Is.EqualTo(646.8));
+			Assert.That(line.NdsAmount, Is.EqualTo(58.8));
+			Assert.That(line.Producer, Is.EqualTo("HELM Medical GmbH"));
+			Assert.That(line.Certificates, Is.EqualTo("РОСС.DE.ИМ08.Д00165"));
+			Assert.That(line.CertificatesDate, Is.EqualTo("09.12.2014"));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/Vazakor_144_Parser.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Models;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class Vazakor_144_Parser : IDocumentParser
+	{
+		protected Encoding Encoding = Encoding.GetEncoding(866);
+
+		public Document Parse(string file, Document document)
+		{
+			var data = Dbf.Load(file, Encoding);
+
+			document.Lines = data.Rows.Cast<DataRow>().Select(r => {
+				document.ProviderDocumentId = Convert.ToString(r["NUM_DOC"], CultureInfo.InvariantCulture);
+				document.DocumentDate = DateTime.Parse(r["DATE_DOC"].ToString());
+
+				var invoice = document.SetInvoice();
+				invoice.BuyerName = r["USER"].ToString();
+				invoice.SellerName = r["PRODAVEC"].ToString();
+
+				var line = document.NewLine();
+				line.Code = r["CODE_TOVAR"].ToString();
+				line.Product = r["NAME_TOVAR"].ToString();
+				line.Producer = r["PROIZ"].ToString();
+				line.SupplierCostWithoutNDS = Convert.ToDecimal(r["CENABEZNDS"], CultureInfo.InvariantCulture);
+				line.SupplierCost = Convert.ToDecimal(r["PRICE"], CultureInfo.InvariantCulture);
+				line.Amount = Convert.ToDecimal(r["SUMMA"], CultureInfo.InvariantCulture);
+				line.NdsAmount = Convert.ToDecimal(r["SUMMA_NDS"], CultureInfo.InvariantCulture);
+				line.Quantity = Convert.ToUInt32(r["VOLUME"], CultureInfo.InvariantCulture);
+				line.Unit = r["EDINIZM"].ToString();
+				line.Certificates = r["DOCUMENT"].ToString();
+				line.CertificatesDate = DateTime.Parse(r["SROKSERT"].ToString()).ToShortDateString();
+				line.Nds = Convert.ToUInt32(r["PCT_NDS"].ToString().Replace("%", string.Empty), CultureInfo.InvariantCulture);
+				return line;
+			}).ToList();
+
+			return document;
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("CODE_TOVAR") &&
+				data.Columns.Contains("LINKSERT") &&
+				data.Columns.Contains("PCT_NDS") &&
+				data.Columns.Contains("EDINIZM") &&
+				data.Columns.Contains("PRODAVEC") &&
+				data.Columns.Contains("NUM_DOC");
+		}
+	}
+}
