7871d5f17cacd3220daa840df10a268094a7ae25
Золотарев Александр <a.zolotarev@analit.net>
2012-02-29 15:12:56 +0400

    Выполнил #8535 Накладная для постащика Ленфарм

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/LenFarmFixture.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Parser.DbfParsers;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	public class LenFarmFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			Assert.IsTrue(LenFarmParser.CheckFileFormat(Dbf.Load(@"..\..\Data\Waybills\00012685.dbf")));
+			var document = WaybillParser.Parse("00012685.dbf");
+
+			Assert.That(document.Lines.Count, Is.EqualTo(3));
+			Assert.That(document.ProviderDocumentId, Is.EqualTo("М000012685"));
+			Assert.That(document.DocumentDate.Value.ToShortDateString(), Is.EqualTo("14.02.2012"));
+			
+			var invoice = document.Invoice;
+			Assert.That(invoice, Is.Not.Null);
+			Assert.That(invoice.BuyerName, Is.EqualTo("ЦРА №23 ОАО1 этаж"));
+			
+			var line = document.Lines[0];
+			Assert.That(line.Code, Is.EqualTo("6342"));
+			Assert.That(line.Product, Is.EqualTo("Альбадент (грейпфрут) освеж/полости рта 10 мл"));
+
+			Assert.That(line.Producer, Is.EqualTo("Фирма Вита ООО"));
+			Assert.IsNull(line.Country);
+
+			Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(22.46));
+			Assert.That(line.SupplierCost, Is.EqualTo(26.5));
+			Assert.That(line.RegistryCost, Is.EqualTo(0));
+
+			Assert.That(line.Nds, Is.EqualTo(18));
+			Assert.That(line.Quantity, Is.EqualTo(2));
+			Assert.That(line.Amount, Is.EqualTo(53));
+
+			Assert.That(line.Period, Is.EqualTo("01.06.2013"));
+			Assert.That(line.SerialNumber, Is.EqualTo("0878/011011"));
+			Assert.That(line.Certificates, Is.EqualTo("РОСС RU.АИ35.Д14019"));
+			Assert.That(line.CertificatesDate, Is.EqualTo("01.12.2013"));
+			Assert.IsNull(line.BillOfEntryNumber);
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/LenFarmParser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Models;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class LenFarmParser : IDocumentParser
+	{
+		protected Encoding Encoding = Encoding.GetEncoding(866);
+
+		public Document Parse(string file, Document document)
+		{
+			var data = Dbf.Load(file, Encoding);
+
+			new DbfParser()
+				.DocumentHeader(h => h.ProviderDocumentId, "NOMER")
+				.DocumentHeader(h => h.DocumentDate, "DATE")
+
+				.DocumentInvoice(i=> i.InvoiceNumber, "NDOCREG")
+				.DocumentInvoice(i=> i.BuyerName, "KLI")
+
+				.Line(l => l.Code, "KOD")
+				.Line(l => l.Product, "NM")
+
+				.Line(l => l.Producer, "PROIZV")
+				.Line(l => l.Country, "COUNTRY")
+
+				.Line(l => l.SupplierCostWithoutNDS, "PRICMNDS")
+				.Line(l => l.SupplierCost, "PRICWNDS")
+
+				.Line(l => l.RegistryCost, "REESTR")
+				.Line(l => l.Nds, "NDS")
+
+				.Line(l => l.Amount, "SUMMA")
+
+				.Line(l => l.Quantity, "KOLVO")
+
+				.Line(l => l.Period, "SROKGODN")
+				.Line(l => l.Certificates, "SERTIF")
+				.Line(l => l.CertificatesDate, "SROKSERT")
+				.Line(l => l.SerialNumber, "SERIA")
+				.Line(l => l.BillOfEntryNumber, "GTD")
+
+				.ToDocument(document, data);
+
+			return document;
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("PRICMNDS") &&
+				   data.Columns.Contains("SROKGODN") &&
+				   data.Columns.Contains("REESTR") &&
+				   data.Columns.Contains("NM") &&
+				   data.Columns.Contains("SERTIF") &&
+				   data.Columns.Contains("KLI");
+		}
+	}
+}
