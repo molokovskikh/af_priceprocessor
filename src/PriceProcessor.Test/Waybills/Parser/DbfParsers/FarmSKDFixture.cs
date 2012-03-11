80aa611f149a2691e294c78c85d413fad78d8ed1
Золотарев Александр <a.zolotarev@analit.net>
2012-03-11 11:04:41 +0400

    Выполнил #8805 накладные для Фарс СКД

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/FarmSKDFixture.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Parser.DbfParsers;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	public class FarmSKDFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			Assert.IsTrue(FarmSKDParser.CheckFileFormat(Dbf.Load(@"..\..\Data\Waybills\30529180.DBF")));
+			var document = WaybillParser.Parse("30529180.DBF");
+
+			Assert.That(document.ProviderDocumentId, Is.EqualTo("29180"));
+			Assert.That(document.DocumentDate.Value.ToShortDateString(), Is.EqualTo("05.03.2012"));
+
+			var line = document.Lines[0];
+			Assert.That(line.Code, Is.EqualTo("2012-0011610"));
+			Assert.That(line.Product, Is.EqualTo("Аллохол табл.п.о. N 50 (48)"));
+			Assert.That(line.Country, Is.EqualTo("БЕЛАРУСЬ"));
+			Assert.That(line.ProducerCostWithoutNDS, Is.EqualTo(36.27));
+			Assert.That(line.ProducerCost, Is.EqualTo(39.9));
+			Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(42.93));
+			Assert.That(line.SupplierCost, Is.EqualTo(47.22));
+			Assert.That(line.RegistryCost, Is.EqualTo(0));
+			Assert.That(line.Nds, Is.EqualTo(10));
+			Assert.That(line.Quantity, Is.EqualTo(10));
+			Assert.That(line.Amount, Is.EqualTo(472.23));
+			Assert.That(line.NdsAmount, Is.EqualTo(42.93));
+			Assert.That(line.Producer, Is.EqualTo("Белмедпрепараты"));
+			Assert.That(line.Period, Is.EqualTo("01.02.2016"));
+			Assert.That(line.SerialNumber, Is.EqualTo("030112"));
+			Assert.That(line.Unit, Is.EqualTo("уп."));
+			Assert.That(line.Certificates, Is.EqualTo("РОСС BY.ФМ05.Д97735"));
+			Assert.That(line.CertificatesDate, Is.EqualTo("06.02.2012"));
+			Assert.That(line.EAN13, Is.EqualTo("4810133003801"));
+			Assert.IsNull(line.BillOfEntryNumber);
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/FarmSKDParser.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Models;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class FarmSKDParser : IDocumentParser
+	{
+		protected Encoding Encoding = Encoding.GetEncoding(866);
+
+		public Document Parse(string file, Document document)
+		{
+			var data = Dbf.Load(file, Encoding);
+
+			new DbfParser()
+				.DocumentHeader(h => h.ProviderDocumentId, "NomNakl")
+				.DocumentHeader(h => h.DocumentDate, "DateNakl")
+
+				.Line(l => l.Code, "Kod")
+				.Line(l => l.Product, "Naim")
+
+				.Line(l => l.Producer, "Proizv")
+				.Line(l => l.Country, "Strana")
+
+				.Line(l => l.SupplierCostWithoutNDS, "ZenaBezNDS")
+				.Line(l => l.SupplierCost, "ZenaSNDS")
+
+				.Line(l => l.ProducerCostWithoutNDS, "ZenaProizv")
+				.Line(l => l.ProducerCost, "ZenaPrSNDS")
+
+				.Line(l => l.RegistryCost, "ZenaReestr")
+				.Line(l => l.Nds, "NDS")
+
+				.Line(l => l.Amount, "SumSNDS")
+				.Line(l => l.NdsAmount, "NDSSUM")
+
+				.Line(l => l.Quantity, "Kol")
+				.Line(l => l.Unit, "EdIzm")
+
+				.Line(l => l.Period, "SrokGodn")
+				.Line(l => l.Certificates, "ImSS")
+				.Line(l => l.CertificatesDate, "DateImSS")
+				.Line(l => l.SerialNumber, "Seria")
+				.Line(l => l.BillOfEntryNumber, "TamozhDek")
+
+				.Line(l => l.EAN13, "EAN8_13")
+				.Line(l => l.VitallyImportant, "ISLIFE")
+
+				.ToDocument(document, data);
+
+			return document;
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("NomNakl") &&
+				   data.Columns.Contains("DateNakl") &&
+				   data.Columns.Contains("EdIzm") &&
+				   data.Columns.Contains("GodenImSS") &&
+				   data.Columns.Contains("ZenaPrSNDS") &&
+				   data.Columns.Contains("EAN8_13");
+		}
+	}
+}
