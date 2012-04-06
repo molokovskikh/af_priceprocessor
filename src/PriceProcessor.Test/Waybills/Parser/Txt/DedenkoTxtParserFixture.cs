55975dca62c5e7e78db30990eb8f7f5b7a17017d
Золотарев Александр <a.zolotarev@analit.net>
2012-04-06 16:17:15 +0400

    Выполнил #9262 накладные для поставщика Деденко В.В.

---
+++ src/PriceProcessor.Test/Waybills/Parser/Txt/DedenkoTxtParserFixture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Inforoom.PriceProcessor.Waybills.Parser.TxtParsers;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.Txt
+{
+	[TestFixture]
+	public class DedenkoTxtParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			Assert.IsTrue(DedenkoTxtParser.CheckFileFormat(@"..\..\Data\Waybills\ДДЕД0000396.txt"));
+			var doc = WaybillParser.Parse("ДДЕД0000396.txt");
+			Assert.That(doc.ProviderDocumentId, Is.EqualTo("ДДЕД0000396"));
+			Assert.That(doc.DocumentDate, Is.EqualTo(new DateTime(2012, 04, 03)));
+			var invoice = doc.Invoice;
+			Assert.That(invoice.ShipperInfo, Is.EqualTo("ИП Деденко Виктория Владимировна"));
+			Assert.That(invoice.ConsigneeInfo, Is.EqualTo("Петросян Л.В. ИП"));
+			Assert.That(invoice.Amount, Is.EqualTo(603.28m));
+			var line = doc.Lines[0];
+			Assert.That(line.Code, Is.EqualTo("4607174430686"));
+			Assert.That(line.Product, Is.EqualTo("\"NS\" крем д/лица дневной д/сухой кожи 50"));
+			Assert.That(line.Producer, Is.EqualTo("Натура сиберика"));
+			Assert.That(line.Country, Is.EqualTo("РОССИЯ"));
+			Assert.That(line.Quantity, Is.EqualTo(1));
+			Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(112.41));
+			Assert.That(line.Period, Is.EqualTo("01.2015"));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/TxtParsers/DedenkoTxtParser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.TxtParsers
+{
+	public class DedenkoTxtParser : BaseIndexingParser
+	{
+		protected override void SetIndexes()
+		{
+			InvoiceAmountIndex = 2;
+			ShipperInfoIndex = 3;
+			ConsigneeInfoIndex = 4;
+
+			base.SetIndexes();
+
+			ProducerCostWithoutNdsIndex = -1;
+			SupplierCostWithoutNdsIndex = 6;
+			SupplierCostIndex = -1;
+			NdsIndex = -1;
+			SupplierPriceMarkupIndex = -1;
+			SerialNumberIndex = -1;
+			CertificatesIndex = -1;
+			RegistryCostIndex = -1;
+			VitallyImportantIndex = -1;
+		}
+
+		public static bool CheckFileFormat(string file)
+		{
+			using (var reader = new StreamReader(file, Encoding.GetEncoding(1251)))
+			{
+				var headerCaption = reader.ReadLine();
+				if (!headerCaption.ToLower().Equals("[header]"))
+					return false;
+				var header = reader.ReadLine().Split(';');
+				if ((header.Length != 7))
+					return false;
+				var bodyCaption = reader.ReadLine();
+				if (!bodyCaption.ToLower().Equals("[body]"))
+					return false;
+				var body = reader.ReadLine().Split(';');
+				if (GetDecimal(body[8]) != 0)
+					return false;
+				if (GetDecimal(body[5]) != null)
+					return false;
+				if (GetDecimal(body[6]) == null)
+					return false;
+				if (GetDateTime(body[10]) == null)
+					return false;
+			}
+			return true;
+		}
+	}
+}
