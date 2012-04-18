c3ed3205a9c46a9b435a2fc7ef18444b0bee7f83
Золотарев Александр <a.zolotarev@analit.net>
2012-04-18 12:24:05 +0400

    Выполнил #9313 Формат накладных для поставщика Медтехкомплект

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/MedtehKomplektParserFixture.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Parser.DbfParsers;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	public class MedtehKomplektParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			Assert.IsTrue(MedtehKomplektParser.CheckFileFormat(Dbf.Load(@"..\..\Data\Waybills\n11169.dbf")));
+			var document = WaybillParser.Parse("n11169.dbf");
+
+			Assert.That(document.Lines.Count, Is.EqualTo(20));
+			Assert.That(document.ProviderDocumentId, Is.EqualTo("Е11169"));
+			Assert.That(document.DocumentDate.Value.ToShortDateString(), Is.EqualTo("16.04.2012"));
+			
+			var invoice = document.Invoice;
+			Assert.That(invoice, Is.Not.Null);
+			Assert.That(invoice.BuyerName, Is.EqualTo("Фарма-555 ООО"));
+			Assert.That(invoice.Amount, Is.EqualTo(1298.95));
+
+			var line = document.Lines[0];
+			Assert.That(line.Code, Is.EqualTo("1906"));
+			Assert.That(line.Product, Is.EqualTo("АЛЛОХОЛ табл. п/о №50"));
+			Assert.That(line.ProducerCostWithoutNDS, Is.EqualTo(40.61));
+			Assert.That(line.ProducerCost, Is.EqualTo(44.67));
+			Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(42.64));
+			Assert.That(line.SupplierCost, Is.EqualTo(46.9));
+			Assert.That(line.RegistryCost, Is.EqualTo(0));
+			Assert.That(line.Nds, Is.EqualTo(10));
+			Assert.That(line.Quantity, Is.EqualTo(3));
+			Assert.That(line.Amount, Is.EqualTo(127.91));
+			Assert.That(line.NdsAmount, Is.EqualTo(12.79));
+			Assert.That(line.Producer, Is.EqualTo("Белмедпрепараты РУП Республика Беларусь"));
+			Assert.That(line.Country, Is.EqualTo("Беларусь"));
+			Assert.That(line.Period, Is.EqualTo("01.01.2016"));
+			Assert.That(line.SerialNumber, Is.EqualTo("651211"));
+			Assert.That(line.Certificates, Is.EqualTo("РОСС BY.ФМ05.Д79221"));
+			Assert.That(line.CertificatesDate, Is.EqualTo("27.12.2011"));
+			Assert.That(line.EAN13, Is.EqualTo("4810133003801"));
+			Assert.That(line.VitallyImportant, Is.False);
+			Assert.IsNull(line.BillOfEntryNumber);
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/MedtehKomplektParser.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Models;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class MedtehKomplektParser : IDocumentParser
+	{
+		protected Encoding Encoding = Encoding.GetEncoding(866);
+
+		public Document Parse(string file, Document document)
+		{
+			var data = Dbf.Load(file, Encoding);
+
+			new DbfParser()
+				.DocumentHeader(h => h.ProviderDocumentId, "NOMERDOK")
+				.DocumentHeader(h => h.DocumentDate, "DATADOK")
+
+				.DocumentInvoice(i=> i.BuyerName, "KLIENT")
+				.DocumentInvoice(i=> i.Amount, "SUMMDOK")
+
+				.Line(l => l.Code, "KODTOV")
+				.Line(l => l.Product, "TOV")
+
+				.Line(l => l.Producer, "PROIZV")
+				.Line(l => l.Country, "COUNTRY")
+
+				.Line(l => l.ProducerCostWithoutNDS, "CENAPROIZV")
+				.Line(l => l.ProducerCost, "CENAPRNDS")
+				.Line(l => l.SupplierCostWithoutNDS, "CENABZNDS")
+				.Line(l => l.SupplierCost, "CENASNDS")
+				.Line(l => l.RegistryCost, "CENAREESTR")
+
+				.Line(l => l.Amount, "SUMM")
+				.Line(l => l.NdsAmount, "SUMMNDS")
+
+				.Line(l => l.Quantity, "KOL")
+
+				.Line(l => l.Period, "USERBYDATE")
+				.Line(l => l.Certificates, "SERT_NUM")
+				.Line(l => l.CertificatesDate, "SERT_IDATE")
+				.Line(l => l.SerialNumber, "SER")
+				.Line(l => l.BillOfEntryNumber, "GTD")
+				.Line(l => l.VitallyImportant, "GNVLS")
+
+				.Line(l => l.EAN13, "BARCODE")
+				.Line(l => l.Nds, "STNDS")
+				.ToDocument(document, data);
+
+			return document;
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("BARCODE") &&
+				   data.Columns.Contains("CENAPROIZV") &&
+				   data.Columns.Contains("SERT_IDATE") &&
+				   data.Columns.Contains("USERBYDATE") &&
+				   data.Columns.Contains("KLIENT") &&
+				   data.Columns.Contains("SUMMDOK");
+		}
+	}
+}
