7896cc6601e3cc7dd80cf31af34b8a09adb6d85a
Золотарев Александр <a.zolotarev@analit.net>
2012-04-27 17:20:06 +0400

    Выполнил требование #9577 Фрмат накладной бля Быков В.И.

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/BikovCheboksary12649ParserFixture.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common.Tools;
+using NUnit.Framework;
+using Inforoom.PriceProcessor.Waybills.Parser.DbfParsers;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	public class BikovCheboksary12649ParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			Assert.IsTrue(BikovCheboksary12649Parser.CheckFileFormat(Dbf.Load(@"..\..\Data\Waybills\Б0001213.DBF")));
+			var document = WaybillParser.Parse("Б0001213.DBF");
+
+			var invoice = document.Invoice;
+			Assert.That(invoice, Is.Not.Null);
+			Assert.That(invoice.InvoiceNumber, Is.EqualTo("Б0001213"));
+			Assert.That(invoice.InvoiceDate.Value.ToShortDateString(), Is.EqualTo("24.04.2012"));
+			Assert.That(invoice.BuyerName, Is.EqualTo("ИП Шахвердиева Валентина Александровна"));
+
+			var line = document.Lines[0];
+			Assert.That(line.Code, Is.EqualTo("3244"));
+			Assert.That(line.Product, Is.EqualTo("911 Непотин гель д/ног 100 мл антиперспирант"));
+			Assert.That(line.ProducerCost, Is.EqualTo(45.35));
+			Assert.That(line.SupplierCost, Is.EqualTo(49.88));
+			Assert.That(line.Nds, Is.EqualTo(18));
+			Assert.That(line.Quantity, Is.EqualTo(1));
+			Assert.That(line.NdsAmount, Is.EqualTo(0));
+			Assert.That(line.Producer, Is.EqualTo("Твинс Тэк Россия"));
+			Assert.That(line.Period, Is.EqualTo("01.08.2013"));
+			Assert.That(line.SerialNumber, Is.EqualTo("0212"));
+			Assert.That(line.Certificates, Is.EqualTo("РОСС RU.АИ86.Д00149"));
+			Assert.That(line.CertificatesDate, Is.EqualTo("16.04.2011"));
+			Assert.IsNull(line.BillOfEntryNumber);
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/BikovCheboksary12649Parser.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Models;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class BikovCheboksary12649Parser : BaseDbfParser
+	{
+		protected Encoding Encoding = Encoding.GetEncoding(866);
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("NOM_SHET") &&
+				   data.Columns.Contains("DATA_SHET") &&
+				   data.Columns.Contains("CENA_ZAV") &&
+				   data.Columns.Contains("CENA_OTP") &&
+				   data.Columns.Contains("NOM_SERT") &&
+				   data.Columns.Contains("DATA_SERT");
+		}
+
+		public override DbfParser GetParser()
+		{
+			return new DbfParser()
+				.DocumentInvoice(i=> i.InvoiceNumber, "NOM_SHET")
+				.DocumentInvoice(i=> i.InvoiceDate, "DATA_SHET")
+				.DocumentInvoice(i=> i.BuyerName, "APTEKA")
+
+				.Line(l => l.Code, "CODE")
+				.Line(l => l.Product, "NAIM")
+
+				.Line(l => l.Producer, "ZAVOD")
+
+				.Line(l => l.ProducerCost, "CENA_ZAV")
+				.Line(l => l.SupplierCost, "CENA_OTP")
+
+				.Line(l => l.NdsAmount, "SUMMA_NDS")
+
+				.Line(l => l.Quantity, "KOL")
+
+				.Line(l => l.Period, "SROK")
+				.Line(l => l.Certificates, "NOM_SERT")
+				.Line(l => l.CertificatesDate, "DATA_SERT")
+				.Line(l => l.SerialNumber, "SERIA")
+				.Line(l => l.BillOfEntryNumber, "GTD")
+
+				.Line(l => l.Nds, "NDS");
+		}
+
+		public override void PostParsing(Document doc)
+		{
+			foreach (var documentLine in doc.Lines) {
+				if (!string.IsNullOrEmpty(documentLine.Period))
+					documentLine.Period = DateTime.ParseExact(documentLine.Period, "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None).ToShortDateString();
+				if (!string.IsNullOrEmpty(documentLine.CertificatesDate))
+					documentLine.CertificatesDate = DateTime.ParseExact(documentLine.CertificatesDate, "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None).ToShortDateString();
+			}
+		}
+	}
+}
