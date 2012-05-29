532ab07f0e41d2515979d12e7e87c527fd8599ca
Лысачев Михаил <m.lysachev@analit.net>
2012-05-29 10:01:53 +0400

    Новшество #10103. Накладные для Поставщика Пустоляков А. А., Код 7759. Реализовано в PustolyakovAA7759Parser.

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/PustolyakovAA7759ParserFixture.cs
@@ -0,0 +1,48 @@
+﻿using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Parser.DbfParsers;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	public class PustolyakovAA7759ParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var document = WaybillParser.Parse(@"..\..\Data\Waybills\00015222.dbf");
+
+			Assert.That(document.Lines.Count, Is.EqualTo(96));
+
+			Assert.That(document.ProviderDocumentId, Is.EqualTo("А0000015222"));
+			Assert.That(document.DocumentDate.Value.ToShortDateString(), Is.EqualTo("14.03.2012"));
+
+			var invoice = document.Invoice;
+			Assert.That(invoice, Is.Not.Null);
+			Assert.That(invoice.BuyerAddress, Is.EqualTo("425120, Марий Эл Респ, Моркинский р-н, Морки пгт, Мира ул, дом № 20, корпус а"));
+
+			var line = document.Lines[0];
+
+			Assert.That(line.Code, Is.EqualTo("Н0000015548"));
+			Assert.That(line.Product, Is.EqualTo("27 Oral-B Classic 40 зубн. щетка medium"));
+			Assert.That(line.Nds, Is.EqualTo(18));
+			Assert.That(line.SerialNumber, Is.EqualTo(null));
+			Assert.That(line.SupplierCost, Is.EqualTo(17.70));
+			Assert.That(line.Amount, Is.EqualTo(17.70));
+			Assert.That(line.Quantity, Is.EqualTo(1));
+			Assert.That(line.EAN13, Is.EqualTo("3014260275921"));
+			Assert.That(line.BillOfEntryNumber, Is.EqualTo(null));
+			Assert.That(line.Country, Is.EqualTo("ИНДИЯ"));
+			Assert.That(line.Producer, Is.EqualTo("Procter & Gamble"));
+			Assert.That(line.Certificates, Is.EqualTo("РОСC IE.АИ35.В06070"));
+			Assert.That(line.CertificatesDate, Is.EqualTo(null));
+		}
+
+
+		[Test]
+		public void Check_file_Format()
+		{
+			Assert.IsTrue(PustolyakovAA7759Parser.CheckFileFormat(Dbf.Load(@"..\..\Data\Waybills\00015222.dbf")));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/PustolyakovAA7759Parser.cs
@@ -0,0 +1,63 @@
+﻿using System.Data;
+
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class PustolyakovAA7759Parser : BaseDbfParser
+	{
+		public override DbfParser GetParser()
+		{
+			/*
+			Nom_dok ("s",11,0) - Номер накладной 
+			Data_dok ("d",8 ,0) - Дата накладной
+			Kod_TT ("s",9 ,0) - Комер Точки(Из нашего справочника ТТ, можем использовать ваши коды) -необходимо согласовать
+			Adress_TT ("s",100 ,0); - Адрес Точки
+			Kod_tov ("s",11,0); - Код товара(в нашей базе)
+			Tovar ("s",80,0); - Название товара
+			Stavka_NDS ("n",9, 0); - Ставка НДС (%)
+			SERYA ("s",20,0); - Серия — зачастую не заполняется
+			CenaSNDS ("n",9, 2); - Цена товара с НДС
+			Summa ("n",9, 2); - Сумма товара с НДС
+			Kol ("n",9, 2); - Количество (шт.)
+			ShtrihKod ("s",13,0); - Штрихкод производителя
+			Nomer_GTD ("s",30,0); - ГТД
+			Strana ("s",15,0); - Название страны фирмы-производителя
+			Proizv ("s",40,0); - Название фирмы-производителя
+			Nomer_Sert ("s",20,0); - № сертификата
+			Data_Sert ("d",8 ,0); - Дата сертификата
+			Srok_Sert ("d",8 ,0); - Срок действия сертификата
+			Kem_vidan ("s",80,0); - Кем выдан сертификат
+			*/
+			return new DbfParser()
+				.DocumentHeader(d => d.DocumentDate, "DATA_DOK")
+				.DocumentHeader(d => d.ProviderDocumentId, "NOM_DOK")
+
+				.DocumentInvoice(i => i.BuyerAddress, "ADRESS_TT")
+
+				.Line(l => l.Code, "KOD_TOV")
+				.Line(l => l.Product, "TOVAR")
+				.Line(l => l.Nds, "STAVKA_NDS")
+				.Line(l => l.SerialNumber, "SERYA")
+				.Line(l => l.SupplierCost, "CENASNDS")
+				.Line(l => l.Amount, "SUMMA")
+				.Line(l => l.Quantity, "KOL")
+				.Line(l => l.EAN13, "SHTRIHKOD")
+				.Line(l => l.BillOfEntryNumber, "NOMER_GTD")
+				.Line(l => l.Country, "STRANA")
+				.Line(l => l.Producer, "PROIZV")
+				.Line(l => l.Certificates, "NOMER_SERT")
+				.Line(l => l.CertificatesDate, "DATA_SERT");
+
+		}
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("KOD_TOV")
+				&& data.Columns.Contains("ADRESS_TT")
+				&& data.Columns.Contains("Kod_TT")
+				&& data.Columns.Contains("SERYA")
+				&& data.Columns.Contains("SHTRIHKOD")
+				&& data.Columns.Contains("KEM_VIDAN");
+		}
+
+	}
+}
