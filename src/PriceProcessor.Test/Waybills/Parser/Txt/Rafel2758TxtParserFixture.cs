bf20e9748283a70d933ea928395ccb4de6738f3a
Лысачев Михаил <m.lysachev@analit.net>
2012-05-29 14:41:09 +0400

    Ошибка #10123. Формат накладной Рафэл, Код 2758. Реализовано в Rafel2758TxtParser.

---
+++ src/PriceProcessor.Test/Waybills/Parser/Txt/Rafel2758TxtParserFixture.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.Txt
+{
+	[TestFixture]
+	public class Rafel2758TxtParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var doc = WaybillParser.Parse(@"02387.txt");
+			Assert.That(doc.Lines.Count, Is.EqualTo(17));
+			Assert.That(doc.ProviderDocumentId, Is.EqualTo("ПР-ЧЛН02387"));
+			Assert.That(doc.DocumentDate, Is.EqualTo(Convert.ToDateTime("21.05.2012")));
+			Assert.That(doc.Invoice.BuyerName, Is.EqualTo("ООО РАФЭЛ"));
+
+			Assert.That(doc.Lines[0].Code, Is.EqualTo("52185"));
+			Assert.That(doc.Lines[0].Product, Is.EqualTo("Ацетилсалициловая кислота 0,5гN10"));
+			Assert.That(doc.Lines[0].Producer, Is.EqualTo("РУП \"Борисовский завод медпрепаратов\", Беларусь"));
+			Assert.That(doc.Lines[0].Country, Is.EqualTo("Беларусь"));
+			Assert.That(doc.Lines[0].Quantity, Is.EqualTo(20));
+			Assert.That(doc.Lines[0].SupplierCostWithoutNDS, Is.EqualTo(1.75));
+			Assert.That(doc.Lines[0].Nds, Is.EqualTo(10));
+			Assert.That(doc.Lines[0].NdsAmount, Is.EqualTo(3.49));
+			Assert.That(doc.Lines[0].SerialNumber, Is.EqualTo("470212"));
+			Assert.That(doc.Lines[0].Period, Is.EqualTo("01.03.2016"));
+			Assert.That(doc.Lines[0].Certificates, Is.EqualTo("РОСС ВY.ФМ05.Д09920"));
+			Assert.That(doc.Lines[0].ProducerCostWithoutNDS, Is.EqualTo(1.52));
+			Assert.That(doc.Lines[0].RegistryCost, Is.EqualTo(5));
+			Assert.That(doc.Lines[0].VitallyImportant, Is.EqualTo(true));
+		}
+
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/TxtParsers/Rafel2758TxtParser.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Inforoom.PriceProcessor.Waybills.Parser.TxtParsers;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.TxtParsers
+{
+	public class Rafel2758TxtParser : BaseIndexingParser
+	{
+		/*
+			[Header]
+			0 <НомерДокумента>;+
+			1 <ДатаДокумента>;+
+			2 <Пусто>;
+			3 <Фирма>;+
+			4 <Пусто>;
+			[Body]
+			0 <ТоварНомер>;+
+			1 <ТоварНазвание>;+
+			2 <ПроизводительНазвание>;+
+			3 <ПроизводительСтрана>;+
+			4 <Количество>;+
+			5 <Пусто>;
+			6 <ЦенаПоставщикаБезНДС>;+
+			7 <НДС%>;+
+			8 <СуммаНДССтроки>;+
+			9 <СерияНомер>;+
+			10 <ГоденДо>;+
+			11 <Пусто>;
+			12 <Пусто>;
+			13 <Пусто>;
+			14 <Пусто>;
+			15 <Пусто>;
+			16 <Пусто>;
+			17 <Пусто>;
+			18 <Пусто>;
+			19 <Пусто>;
+			20 <Пусто>;
+			21 <Пусто>;
+			22 <№Разрешения>;+
+			23 <ЦЕНАПРОИЗВОДИТЕЛЯСНДС>;
+			24 <ЦЕНАПРОИЗВОДИТЕЛЯБЕЗНДС>;+
+			25 <ЦенаГосРеестра>;+
+			26 <ЛекЖВ>;+
+		*/
+		protected override void SetIndexes()
+		{
+			ProviderDocumentIdIndex = 0;
+			DocumentDateIndex = 1;
+			BuyerNameIndex = 3;
+			CodeIndex = 0;
+			ProductIndex = 1;
+			ProducerIndex = 2;
+			CountryIndex = 3;
+			QuantityIndex = 4;
+			SupplierCostWithoutNdsIndex = 6;
+			NdsIndex = 7;
+			NdsAmountIndex = 8;
+			SerialNumberIndex = 9;
+			PeriodIndex = 10;
+			CertificatesIndex = 22;
+			ProducerCostWithoutNdsIndex = 24;
+			RegistryCostIndex = 25;
+			VitallyImportantIndex = 26;
+		}
+
+		public static bool CheckFileFormat(string file)
+		{
+			using (var reader = new StreamReader(file, Encoding.GetEncoding(1251)))
+			{
+				var headerCaption = reader.ReadLine();
+				if (!headerCaption.ToLower().Equals("[header]"))
+					return false;
+				var header = reader.ReadLine().Split(';');
+				if (header.Length != 6)
+					return false;
+				var bodyCaption = reader.ReadLine();
+				if (!bodyCaption.ToLower().Equals("[body]"))
+					return false;
+				var body = reader.ReadLine().Split(';');
+				if (body.Length != 28)
+					return false;
+			}
+			return true;
+		}
+	}
+}
