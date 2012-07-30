40cb5e25ba9bd26aba051312f274faf4a3175ae2
Квасов Роман <r.kvasov@analit.net>
2012-07-30 16:13:23 +0400

    исправил установку парсера накладной

--- src/PriceProcessor/Waybills/Models/Document.cs
+++ src/PriceProcessor/Waybills/Models/Document.cs
@@ -24,6 +24,7 @@ namespace Inforoom.PriceProcessor.Waybills.Models
 
 		public Document(DocumentReceiveLog log, string parser = null)
 		{
+			Parser = parser;
 			Log = log;
 			WriteTime = DateTime.Now;
 			FirmCode = Convert.ToUInt32(log.Supplier.Id);
