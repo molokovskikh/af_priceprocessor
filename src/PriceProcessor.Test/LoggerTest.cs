2032dce735ce4a4cafdfe44d8ae0694a21867f77
Александр Шилин <a.shilin@analit.net>
2012-08-13 10:46:08 +0400

    Ошибка #11189. Логирование некритических ошибок. Добавлено логирование в базу и фильтры для e-mail логирования.

--- src/PriceProcessor.Test/LoggerTest.cs
+++ src/PriceProcessor.Test/LoggerTest.cs
@@ -2,21 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Common.Tools;
+using Inforoom.PriceProcessor.Models;
+using MySql.Data.MySqlClient;
 using NUnit.Framework;
+using Test.Support;
 using log4net;
 using log4net.Config;
 
 namespace PriceProcessor.Test
 {
 	[TestFixture]
-	public class LoggerTest
+	public class LoggerTest : IntegrationFixture
 	{
 		[Test]
 		public void DBLoggerTest()
 		{
 			ILog logger = LogManager.GetLogger(GetType());
 			XmlConfigurator.Configure();
-			logger.Error("Alarm!!!");
+			logger.Error("Не удалось разобрать накладную",new Exception("Ошибка!"));
+			logger.Error("Не удалось разобрать накладную", new DbfException(String.Format("Не могу преобразовать значение '{0}' к числу, строка {1} столбец {2}",
+					1, 2, 3)));
+			logger.Error("Не удалось разобрать накладную",new IndexOutOfRangeException("Индекс находился вне границ массива"));
+			Assert.That(session.QueryOver<Log>().Where(l => l.Source == "FilterPriceProcessor" &&
+				l.Message == "Не удалось разобрать накладную").List().Count, Is.EqualTo(1));
+			Assert.That(session.QueryOver<Log>().Where(l => l.Source == "PriceProcessor" &&
+				l.Message == "Не удалось разобрать накладную").List().Count, Is.EqualTo(3));
 		}
 	}
 }
