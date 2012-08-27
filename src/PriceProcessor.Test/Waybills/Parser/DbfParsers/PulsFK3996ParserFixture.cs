36655d772fad9a9d7b5823fb5c55dc6670b35b5a
Александр Шилин <a.shilin@analit.net>
2012-08-27 11:39:53 +0400

    Новшество #11488. Формат накладной для Поставщика ПУЛЬС ФК, Код 3996. Добавлен формат PulsFK3996Parser

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/PulsFK3996ParserFixture.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	public class PulsFK3996ParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var doc = WaybillParser.Parse("00627149.dbf");
+			Assert.That(doc.ProviderDocumentId, Is.EqualTo("00627149"));
+			Assert.That(doc.DocumentDate.Value.ToShortDateString(), Is.EqualTo("13.08.2012"));
+			Assert.That(doc.Invoice.ShipperInfo, Is.EqualTo("ООО ФК ПУЛЬС"));
+			var line = doc.Lines[0];
+
+			Assert.That(line.Code, Is.EqualTo("05583"));
+			Assert.That(line.Product, Is.EqualTo("Ампициллина т/г табл. 250 мг х20"));
+			Assert.That(line.SerialNumber, Is.EqualTo("430612"));
+			Assert.That(line.Period, Is.EqualTo("01.07.2015"));
+			Assert.That(line.SupplierCost, Is.EqualTo(9.46));
+			Assert.That(line.Quantity, Is.EqualTo(10));
+			Assert.That(line.Certificates, Is.EqualTo("РОСС BY.ФМ05.Д87322"));
+			Assert.That(line.CertificateAuthority, Is.EqualTo("ООО \"ОЦС\" г. Екатеринбург"));
+			Assert.That(line.Nds, Is.EqualTo(10));
+			Assert.That(line.RegistryCost, Is.EqualTo(14.23));
+			Assert.That(line.Producer, Is.EqualTo("Белмедпрепараты"));
+			Assert.That(line.Country, Is.EqualTo("БЕЛАРУСЬ"));
+			Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(8.6));
+			Assert.That(line.ProducerCostWithoutNDS, Is.EqualTo(8.8));
+			Assert.That(line.BillOfEntryNumber, Is.Null);
+			Assert.That(line.VitallyImportant, Is.True);
+			Assert.That(line.ProducerCost, Is.EqualTo(9.68));
+			Assert.That(line.EAN13, Is.EqualTo("4810133000169"));
+			Assert.That(line.Amount, Is.EqualTo(94.6));
+			Assert.That(line.NdsAmount, Is.EqualTo(8.6));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/PulsFK3996Parser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class PulsFK3996Parser : BaseDbfParser
+	{
+		public override DbfParser GetParser()
+		{
+			return new DbfParser()
+				.DocumentHeader(h => h.ProviderDocumentId, "DocName")
+				.DocumentHeader(h => h.DocumentDate, "DateDoc")
+				.DocumentInvoice(i => i.ShipperInfo, "Vendor")
+				.Line(l => l.Code, "Code")
+				.Line(l => l.Product, "Good")
+				.Line(l => l.SerialNumber, "Serial")
+				.Line(l => l.Period, "DateB")
+				.Line(l => l.SupplierCost, "Price")
+				.Line(l => l.Quantity, "Quant")
+				.Line(l => l.Certificates, "Sert")
+				.Line(l => l.CertificateAuthority, "SertWho")
+				.Line(l => l.Nds, "NDS")
+				.Line(l => l.RegistryCost, "Reestr")
+				.Line(l => l.Producer, "Enterp")
+				.Line(l => l.Country, "Country")
+				.Line(l => l.SupplierCostWithoutNDS, "PriceWONDS")
+				.Line(l => l.ProducerCostWithoutNDS, "Priceent")
+				.Line(l => l.BillOfEntryNumber, "GTD")
+				.Line(l => l.VitallyImportant, "ZNVLS")
+				.Line(l => l.ProducerCost, "PRDWNDS")
+				.Line(l => l.EAN13, "ProdSBar")
+				.Line(l => l.Amount, "SUMSNDS")
+				.Line(l => l.NdsAmount, "SUMNDS");
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("Vendor")
+				&& data.Columns.Contains("Enterp")
+				&& data.Columns.Contains("Good")
+				&& data.Columns.Contains("PriceWONDS")
+				&& data.Columns.Contains("ZNVLS");
+		}
+	}
+}
