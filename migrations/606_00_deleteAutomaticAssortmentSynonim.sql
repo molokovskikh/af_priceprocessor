694bcde91fc5a1de4ebf7aed46fa1f213c520034
Золотарев Александр <a.zolotarev@analit.net>
2012-08-28 13:00:55 +0400

    Удаляю лишние, созданные записи

---
+++ migrations/606_00_deleteAutomaticAssortmentSynonim.sql
@@ -0,0 +1,20 @@
+DROP TEMPORARY TABLE IF EXISTS farm.AutomaticProducerSynonymsForDelete;
+
+CREATE TEMPORARY TABLE farm.AutomaticProducerSynonymsForDelete (
+SynonymId INT unsigned);
+
+INSERT
+INTO    farm.AutomaticProducerSynonymsForDelete
+
+SELECT
+aps.*
+FROM
+farm.SynonymFirmCr s
+join catalogs.assortment a on a.ProducerId = s.CodeFirmCr
+join farm.AutomaticProducerSynonyms aps on aps.ProducerSynonymId = s.SynonymFirmCrCode
+group by s.SynonymFirmCrCode
+having count(a.ProducerId) = 1;
+
+
+delete from farm.AutomaticProducerSynonyms
+where ProducerSynonymId in (select * from farm.AutomaticProducerSynonymsForDelete);
