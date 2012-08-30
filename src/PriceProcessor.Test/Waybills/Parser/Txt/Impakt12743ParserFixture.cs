4d964d742c3980346bf9eb792b9032dd6d5aea88
Александр Шилин <a.shilin@analit.net>
2012-08-30 11:56:17 +0400

    Ошибка #11561. Формат накладной для Поставщика Импакт, Код 12743. Добавлен формат Impakt12743Parser

---
+++ src/PriceProcessor.Test/Waybills/Parser/Txt/Impakt12743ParserFixture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Inforoom.PriceProcessor.Waybills.Parser.TxtParsers;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.Txt
+{
+	[TestFixture]
+	public class Impakt12743ParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			Assert.IsTrue(Impakt12743Parser.CheckFileFormat(@"..\..\Data\Waybills\СКИМП011737.txt"));
+			var doc = WaybillParser.Parse("СКИМП011737.txt");
+			Assert.That(doc.ProviderDocumentId, Is.EqualTo("СКИМП011737"));
+			Assert.That(doc.DocumentDate, Is.EqualTo(new DateTime(2012, 07, 18)));
+			var invoice = doc.Invoice;
+			Assert.That(invoice.Amount, Is.EqualTo(1334.77m));
+			var line = doc.Lines[0];
+			Assert.That(line.Code, Is.EqualTo("1067"));
+			Assert.That(line.Product, Is.EqualTo("Бинт Lastotel                4х400"));
+			Assert.That(line.Producer, Is.EqualTo("Пауль Хартман"));
+			Assert.That(line.Quantity, Is.EqualTo(1));
+			Assert.That(line.Unit, Is.EqualTo("шт"));
+			Assert.That(line.SupplierCost, Is.EqualTo(14.96));
+			Assert.That(line.Amount, Is.EqualTo(14.96));
+			Assert.That(line.Nds, Is.EqualTo(10));
+			Assert.That(line.NdsAmount, Is.EqualTo(1.36));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/TxtParsers/Impakt12743Parser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.TxtParsers
+{
+	public class Impakt12743Parser : BaseIndexingParser
+	{
+		protected override void SetIndexes()
+		{
+			InvoiceAmountIndex = 2;
+			base.SetIndexes();
+
+			QuantityIndex = 3;
+			UnitIndex = 4;
+			SupplierCostIndex = 5;
+			AmountIndex = 6;
+			NdsIndex = 8;
+			NdsAmountIndex = 9;
+
+
+			ProducerCostWithoutNdsIndex = -1;
+			SupplierPriceMarkupIndex = -1;
+			SerialNumberIndex = -1;
+			PeriodIndex = -1;
+			CertificatesIndex = -1;
+			RegistryCostIndex = -1;
+			VitallyImportantIndex = -1;
+			SupplierCostWithoutNdsIndex = -1;
+		}
+
+		public static bool CheckFileFormat(string file)
+		{
+			using (var reader = new StreamReader(file, Encoding.GetEncoding(1251))) {
+				var headerCaption = reader.ReadLine();
+				if (!headerCaption.ToLower().Equals("[header]"))
+					return false;
+				var header = reader.ReadLine().Split(';');
+				if ((header.Length != 3))
+					return false;
+				var bodyCaption = reader.ReadLine();
+				if (!bodyCaption.ToLower().Equals("[body]"))
+					return false;
+				var body = reader.ReadLine().Split(';');
+				if ((body.Length != 10))
+					return false;
+			}
+			return true;
+		}
+	}
+}
