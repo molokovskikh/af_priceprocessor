be602b8ecf692780950d943e03daecbc93476ba6
Александр Шилин <a.shilin@analit.net>
2012-09-12 10:28:59 +0400

    Новшество #11779. Формат накладных для поставщика Бестселлер Групп. Добавлен новый формат BestsellerGroupParser

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/BestsellerGroupParserFixture.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	public class BestsellerGroupParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var document = WaybillParser.Parse("R_3470.DBF");
+			Assert.That(document.Lines.Count, Is.EqualTo(46));
+			Assert.That(document.ProviderDocumentId, Is.EqualTo("3470"));
+			Assert.That(document.DocumentDate, Is.EqualTo(new DateTime(2012, 9, 11)));
+			var line = document.Lines[0];
+			Assert.That(line.Code, Is.EqualTo("Р12941"));
+			Assert.That(line.Product, Is.EqualTo("1010а Бутылочка пл. 240мл сил/соск быстр. поток/6+ /аква/12"));
+			Assert.That(line.SerialNumber, Is.Null);
+			Assert.That(line.Period, Is.Null);
+			Assert.That(line.SupplierCost, Is.EqualTo(81.81));
+			Assert.That(line.Quantity, Is.EqualTo(1));
+			Assert.That(line.Certificates, Is.Null);
+			Assert.That(line.CertificateAuthority, Is.Null);
+			Assert.That(line.CertificatesDate, Is.Null);
+			Assert.That(line.SupplierPriceMarkup, Is.EqualTo(0));
+			Assert.That(line.Nds, Is.EqualTo(18));
+			Assert.That(line.RegistryCost, Is.EqualTo(0));
+			Assert.That(line.Producer, Is.Null);
+			Assert.That(line.Country, Is.EqualTo("Китай"));
+			Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(69.3305));
+			Assert.That(line.ProducerCostWithoutNDS, Is.EqualTo(0));
+			Assert.That(line.NdsAmount, Is.EqualTo(12.4795));
+			Assert.That(line.Amount, Is.EqualTo(81.81));
+			Assert.That(line.VitallyImportant, Is.EqualTo(false));
+			Assert.That(document.Invoice.AmountWithoutNDS, Is.EqualTo(69.3305));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/BestsellerGroupParser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class BestsellerGroupParser : BaseDbfParser
+	{
+		public override DbfParser GetParser()
+		{
+			return new DbfParser()
+				.DocumentHeader(h => h.ProviderDocumentId, "DOCNO")
+				.DocumentHeader(h => h.DocumentDate, "DOCDAT")
+				.Line(l => l.Code, "CODE")
+				.Line(l => l.Product, "GOOD")
+				.Line(l => l.SerialNumber, "SERIAL")
+				.Line(l => l.Period, "DATEB")
+				.Line(l => l.SupplierCost, "PRICE")
+				.Line(l => l.Quantity, "QUANT")
+				.Line(l => l.Certificates, "SERT")
+				.Line(l => l.CertificatesDate, "DATES")
+				.Line(l => l.CertificateAuthority, "SERTWHO")
+				.Line(l => l.SupplierPriceMarkup, "MARGIN")
+				.Line(l => l.Nds, "NDS")
+				.Line(l => l.RegistryCost, "REESTR")
+				.Line(l => l.Producer, "ENTERP")
+				.Line(l => l.Country, "COUNTRY")
+				.Line(l => l.SupplierCostWithoutNDS, "PRICEWONDS")
+				.Line(l => l.ProducerCostWithoutNDS, "PRICEENT")
+				.DocumentInvoice(i => i.AmountWithoutNDS, "SUMM")
+				.Line(l => l.Amount, "SUMMWNDS")
+				.Line(l => l.NdsAmount, "SUMMNDS")
+				.Line(l => l.VitallyImportant, "PV");
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("DOCNO")
+				&& data.Columns.Contains("DOCDAT")
+				&& data.Columns.Contains("QUANT")
+				&& data.Columns.Contains("PRICEWONDS");
+		}
+	}
+}
