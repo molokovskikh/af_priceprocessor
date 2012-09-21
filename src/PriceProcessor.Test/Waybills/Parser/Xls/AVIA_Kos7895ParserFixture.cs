fd3270e37af838bcb2d365ab02beab5146484fe1
Александр Шилин <a.shilin@analit.net>
2012-09-21 11:14:26 +0400

    Новшество #11959. Формат накладной для Поставщика АВИА-Кос, Код 7895. Добавлен новый формат AVIA_Kos7895Parser

---
+++ src/PriceProcessor.Test/Waybills/Parser/Xls/AVIA_Kos7895ParserFixture.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.Xls
+{
+	[TestFixture]
+	public class AVIA_Kos7895ParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var document = WaybillParser.Parse("КА62689.xls");
+			Assert.That(document.ProviderDocumentId, Is.EqualTo("КА/62689"));
+			Assert.That(document.DocumentDate, Is.EqualTo(DateTime.Parse("14.09.2012")));
+
+			Assert.That(document.Lines.Count, Is.EqualTo(48));
+			var line = document.Lines[0];
+			Assert.That(line.Product, Is.EqualTo("Джонсон Салф б/отдушки 64шт"));
+			Assert.That(line.Code, Is.EqualTo("6296704/2/1"));
+			Assert.That(line.EAN13, Is.EqualTo("3574660505832"));
+			Assert.That(line.Unit, Is.EqualTo("шт."));
+			Assert.That(line.Producer, Is.Null);
+			Assert.That(line.RegistryCost, Is.Null);
+			Assert.That(line.SupplierCost, Is.EqualTo(85.97));
+			Assert.That(line.Amount, Is.EqualTo(85.97));
+			Assert.That(line.SupplierCostWithoutNDS, Is.Null);
+			Assert.That(line.Nds, Is.Null);
+			Assert.That(line.Quantity, Is.EqualTo(1));
+			Assert.That(line.ProducerCostWithoutNDS, Is.Null);
+			Assert.That(line.SerialNumber, Is.Null);
+			Assert.That(line.Certificates, Is.Null);
+			Assert.That(line.Country, Is.EqualTo("РОССИЯ"));
+			Assert.That(line.BillOfEntryNumber, Is.EqualTo("---"));
+			Assert.That(line.Period, Is.Null);
+			Assert.That(line.VitallyImportant, Is.Null);
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/XlsParsers/AVIA_Kos7895XlsParser.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExcelLibrary.BinaryFileFormat;
+using ExcelLibrary.SpreadSheet;
+using Inforoom.PriceProcessor.Waybills.Models;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.XlsParsers
+{
+	public class AVIA_Kos7895XlsParser : IDocumentParser
+	{
+		public Document Parse(string file, Document document)
+		{
+			StringDecoder.DefaultEncoding = Encoding.GetEncoding(1251);
+			var book = Workbook.Load(file);
+			var sheet = book.Worksheets[0];
+
+			string value = sheet.Cells[9, 3].StringValue;
+			int positionProviderDocumentId = value.IndexOf("№") + 1;
+			int positionDate = value.IndexOf("от");
+
+			document.ProviderDocumentId = value.Substring(positionProviderDocumentId, positionDate - positionProviderDocumentId).Trim();
+			document.DocumentDate = DateTime.Parse(value.Substring(positionDate + 2).Trim());
+
+
+			foreach (var row in sheet.Cells.Rows.Values.Skip(7)) {
+				if ((String.IsNullOrEmpty(row.GetCell(0).StringValue) || (row.GetCell(0).StringValue.Equals("#NULL!"))) &&
+					(String.IsNullOrEmpty(row.GetCell(4).StringValue) || (row.GetCell(4).StringValue.Equals("#NULL!"))) &&
+					(String.IsNullOrEmpty(row.GetCell(6).StringValue) || (row.GetCell(6).StringValue.Equals("#NULL!"))))
+					return document;
+
+				var line = document.NewLine();
+				line.Code = row.GetCell(2).StringValue;
+				line.EAN13 = row.GetCell(3).StringValue;
+				line.Product = row.GetCell(4).StringValue;
+				line.Unit = row.GetCell(5).StringValue;
+				line.Quantity = Convert.ToUInt32(row.GetCell(6).Value);
+				line.SupplierCost = Convert.ToDecimal(row.GetCell(7).StringValue);
+				line.Amount = Convert.ToDecimal(row.GetCell(8).StringValue);
+				line.Country = row.GetCell(9).StringValue;
+				line.BillOfEntryNumber = row.GetCell(10).StringValue;
+			}
+
+			return document;
+		}
+
+		public static bool CheckFileFormat(string file)
+		{
+			StringDecoder.DefaultEncoding = Encoding.GetEncoding(1251);
+			var workbook = Workbook.Load(file);
+			var sheet = workbook.Worksheets[0];
+			return (sheet.Cells[11, 0].StringValue.ToLower().Equals("№ п/п")) &&
+				(sheet.Cells[11, 4].StringValue.ToLower().Equals("наименование")) &&
+				(sheet.Cells[11, 3].StringValue.ToLower().Equals("штрих-код"));
+		}
+	}
+}
