da3071e8f7000c203d69509391b82f09a49db986
Александр Шилин <a.shilin@analit.net>
2012-09-25 16:37:28 +0400

    Revert "Добавил комментарий"
    This reverts commit c5d503e38e3c9dfff68348098799a33a9302fed7.

--- src/PriceProcessor.Test/Formalization/NewFormilizerFixture.cs
+++ src/PriceProcessor.Test/Formalization/NewFormilizerFixture.cs
@@ -115,7 +115,7 @@ namespace PriceProcessor.Test.Formalization
 			}
 		}
 
-		[Test, Description("Проверяем, что при формализации прайса мы не создаем автоматический синоним, созданный по ассортименту")]
+		[Test]
 		public void Complex_double_firmalize_no_automatic_synonim()
 		{
 			With.Connection(c => {
