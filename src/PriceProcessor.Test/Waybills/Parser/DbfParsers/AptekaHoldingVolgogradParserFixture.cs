023afea4e2b2a7d327f9c1d21f4c55210bd8f290
Александр Шилин <a.shilin@analit.net>
2012-10-03 17:48:29 +0400

    Новшество #12109. Формат для не разобранных накладных ООО «АПТЕКА-ХОЛДИНГ 1» филиал в г. Волгоград (50). Добавлен новый формат AptekaHoldingVolgogradParser

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/AptekaHoldingVolgogradParserFixture.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	class AptekaHoldingVolgogradParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var document = WaybillParser.Parse("3518_1090425.dbf");
+
+			Assert.That(document.ProviderDocumentId, Is.EqualTo("АХ1-1090425/0"));
+			Assert.That(document.DocumentDate, Is.EqualTo(new DateTime(2012, 9, 25)));
+
+			var invoice = document.Invoice;
+			Assert.That(invoice.RecipientAddress, Is.EqualTo("N13 г.Астрахань, ул.Чалабяна/Ногина/Свердлова, д.19/7/98, Лит.Б"));
+			Assert.That(invoice.InvoiceNumber, Is.Null);
+			Assert.That(invoice.InvoiceDate, Is.Null);
+			Assert.That(invoice.SellerAddress, Is.Null);
+			Assert.That(invoice.SellerINN, Is.Null);
+			Assert.That(invoice.SellerKPP, Is.Null);
+			Assert.That(invoice.SellerName, Is.Null);
+
+			var line = document.Lines[0];
+			Assert.That(line.SerialNumber, Is.EqualTo("21580411"));
+			Assert.That(line.Period, Is.EqualTo("01.04.2014"));
+			Assert.That(line.Certificates, Is.EqualTo("РОСС NL.ФМ05.Д02656"));
+			Assert.That(line.CertificatesDate, Is.Null);
+			Assert.That(line.ProducerCostWithoutNDS, Is.EqualTo(80.5));
+			Assert.That(line.RegistryCost, Is.EqualTo(0));
+			Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(80.7));
+			Assert.That(line.Nds, Is.EqualTo(10));
+			Assert.That(line.SupplierCost, Is.EqualTo(88.77));
+			Assert.That(line.Quantity, Is.EqualTo(3));
+			Assert.That(line.SupplierPriceMarkup, Is.EqualTo(0.25));
+			Assert.That(line.NdsAmount, Is.EqualTo(24.21));
+			Assert.That(line.Amount, Is.EqualTo(266.31));
+			Assert.That(line.CertificateAuthority, Is.Null);
+			Assert.That(line.VitallyImportant, Is.False);
+			Assert.That(line.EAN13, Is.Null);
+			Assert.That(line.Producer, Is.EqualTo("Natur Produkt"));
+			Assert.That(line.Product, Is.EqualTo("Анти-Ангин формула пастилки N24 Нидерланды"));
+			Assert.That(line.Code, Is.EqualTo("15337"));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/AptekaHoldingVolgogradParser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class AptekaHoldingVolgogradParser : BaseDbfParser
+	{
+		public override DbfParser GetParser()
+		{
+			return new DbfParser()
+				.DocumentHeader(h => h.ProviderDocumentId, "TTN")
+				.DocumentHeader(h => h.DocumentDate, "TTN_DATE")
+				.DocumentInvoice(i => i.RecipientAddress, "ANAME_AF")
+				.DocumentInvoice(i => i.InvoiceNumber, "I_NUM")
+				.DocumentInvoice(i => i.SellerAddress, "I_SEL_ADR")
+				.DocumentInvoice(i => i.SellerINN, "I_SEL_INN")
+				.DocumentInvoice(i => i.SellerKPP, "I_SEL_KPP")
+				.DocumentInvoice(i => i.SellerName, "I_SEL_NAME")
+				.Line(l => l.SerialNumber, "SERIA")
+				.Line(l => l.Period, "SGODN")
+				.Line(l => l.Certificates, "SERT")
+				.Line(l => l.CertificatesDate, "SERT_DATE")
+				.Line(l => l.ProducerCostWithoutNDS, "PRCENA_BND")
+				.Line(l => l.RegistryCost, "GR_CENA")
+				.Line(l => l.SupplierCostWithoutNDS, "PCENA_BNDS")
+				.Line(l => l.Nds, "NDS")
+				.Line(l => l.SupplierCost, "PCENA_NDS")
+				.Line(l => l.Quantity, "KOL_TOV")
+				.Line(l => l.SupplierPriceMarkup, "SP_MARKUP")
+				.Line(l => l.NdsAmount, "P_NDS_AMNT")
+				.Line(l => l.Amount, "P_AMNT")
+				.Line(l => l.CertificateAuthority, "SERT_AUTH")
+				.Line(l => l.VitallyImportant, "VT")
+				.Line(l => l.EAN13, "BAR_CODE")
+				.Line(l => l.Producer, "PRZV_POST")
+				.Line(l => l.Product, "NAME_POST")
+				.Line(l => l.Code, "SP_PRD_ID");
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("PRCENA_BND") &&
+				data.Columns.Contains("TTN") &&
+				data.Columns.Contains("TTN_DATE") &&
+				data.Columns.Contains("PRZV_POST");
+		}
+	}
+}
