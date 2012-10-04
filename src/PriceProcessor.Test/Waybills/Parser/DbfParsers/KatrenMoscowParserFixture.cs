981aef20d59c21a4204c74295689be8b7758249c
Александр Шилин <a.shilin@analit.net>
2012-10-04 18:12:53 +0400

    Новшество #12101. Формат для не разобранных накладных ЗАО Катрен (407). Добавлен новый формат KatrenMoscowParser

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/KatrenMoscowParserFixture.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser
+{
+	[TestFixture]
+	public class KatrenMoscowParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var doc = WaybillParser.Parse("15674587_Катрен(828475).DBF");
+			Assert.That(doc.ProviderDocumentId, Is.EqualTo("828475"));
+			Assert.That(doc.DocumentDate, Is.EqualTo(DateTime.Parse("23.09.2012")));
+
+			var line = doc.Lines[0];
+			Assert.That(line.Code, Is.EqualTo("33998734"));
+			Assert.That(line.Product, Is.EqualTo("АМПИЦИЛЛИН 0,25 N20 ТАБЛ"));
+			Assert.That(line.Quantity, Is.EqualTo(15));
+			Assert.That(line.SupplierCost, Is.EqualTo(17.16));
+			Assert.That(line.Nds, Is.EqualTo(10));
+			Assert.That(line.NdsAmount, Is.EqualTo(23.4));
+			Assert.That(line.Amount, Is.EqualTo(257.4));
+			Assert.That(line.SerialNumber, Is.EqualTo("050812"));
+			Assert.That(line.Period, Is.EqualTo("01.09.2014"));
+			Assert.That(line.Certificates, Is.EqualTo("РОСС RU.ФМ10.Д09517"));
+			Assert.That(line.Producer, Is.EqualTo("Барнаульский завод медпрепаратов,ООО"));
+			Assert.That(line.Country, Is.EqualTo("Россия"));
+			Assert.That(line.BillOfEntryNumber, Is.Null);
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/KatrenMoscowParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class KatrenMoscowParser : BaseDbfParser
+	{
+		public override DbfParser GetParser()
+		{
+			return new DbfParser()
+				.DocumentHeader(h => h.ProviderDocumentId, "DocNum")
+				.DocumentHeader(h => h.DocumentDate, "DocDate")
+				.Line(l => l.Code, "ItemId")
+				.Line(l => l.Product, "GoodName")
+				.Line(l => l.Quantity, "Amount")
+				.Line(l => l.SupplierCost, "Price_NDS")
+				.Line(l => l.Nds, "NDS")
+				.Line(l => l.NdsAmount, "SummNDS")
+				.Line(l => l.Amount, "Summ_NDS")
+				.Line(l => l.SerialNumber, "Series")
+				.Line(l => l.Period, "LifeTime")
+				.Line(l => l.Certificates, "Sertif")
+				.Line(l => l.Producer, "Vendor")
+				.Line(l => l.Country, "Country")
+				.Line(l => l.BillOfEntryNumber, "GTD");
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("ItemId")
+				&& data.Columns.Contains("SummNDS")
+				&& data.Columns.Contains("Summ_NDS");
+		}
+	}
+}
