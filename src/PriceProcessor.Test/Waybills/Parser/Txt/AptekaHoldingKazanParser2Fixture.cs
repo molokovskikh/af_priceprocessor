9f4ee5c66629e63dcef2255ebe5ceea1ddb3a9d2
Александр Шилин <a.shilin@analit.net>
2012-10-04 10:57:57 +0400

    Новшество #12095. Формат для не разобранных накладных Аптека-Холдинг 1 филиал ЗАО (8). Добавлен новый формат AptekaHoldingKazanParser2

---
+++ src/PriceProcessor.Test/Waybills/Parser/Txt/AptekaHoldingKazanParser2Fixture.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.Txt
+{
+	[TestFixture]
+	public class AptekaHoldingKazanParser2Fixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var doc = WaybillParser.Parse("051097.txt");
+
+			Assert.That(doc.Lines.Count, Is.EqualTo(27));
+			Assert.That(doc.ProviderDocumentId, Is.EqualTo("0000000001051097/0"));
+			Assert.That(doc.DocumentDate, Is.EqualTo(Convert.ToDateTime("25.09.2012")));
+			var line = doc.Lines[0];
+			Assert.That(line.Code, Is.EqualTo("31662"));
+			Assert.That(line.Product, Is.EqualTo("Авамис спрей назальный 27.5мкг/доза 120доз N1 Великобритания"));
+			Assert.That(line.Producer, Is.EqualTo("Glaxo Operations UK Limited"));
+			Assert.That(line.Country, Is.EqualTo("Великобритания"));
+			Assert.That(line.Quantity, Is.EqualTo(1));
+			Assert.That(line.ProducerCostWithoutNDS, Is.EqualTo(420.53));
+			Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(420.53));
+			Assert.That(line.Nds, Is.EqualTo(10));
+			Assert.That(line.NdsAmount, Is.EqualTo(42.05));
+			Assert.That(line.SerialNumber, Is.EqualTo("C577235"));
+			Assert.That(line.Period, Is.EqualTo("01.02.2015"));
+			Assert.That(line.BillOfEntryNumber, Is.EqualTo("10130130/130712/0014129/01"));
+			Assert.That(line.Certificates, Is.EqualTo("РОСС GB.ФМ01.Д75373"));
+			Assert.That(line.CertificatesDate, Is.EqualTo("12.07.2012"));
+			Assert.That(line.RegistryCost, Is.EqualTo(0));
+			Assert.That(line.EAN13, Is.EqualTo("4607008131321"));
+			Assert.That(line.VitallyImportant, Is.EqualTo(false));
+			Assert.That(line.Amount, Is.EqualTo(462.58));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/TxtParsers/AptekaHoldingKazanParser2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.TxtParsers
+{
+	public class AptekaHoldingKazanParser2 : BaseIndexingParser
+	{
+		protected override void SetIndexes()
+		{
+			CodeIndex = 0;
+			ProductIndex = 1;
+			ProducerIndex = 2;
+			CountryIndex = 3;
+			QuantityIndex = 4;
+			ProducerCostWithoutNdsIndex = 5;
+			SupplierCostWithoutNdsIndex = 6;
+			NdsIndex = 7;
+			NdsAmountIndex = 8;
+			SerialNumberIndex = 9;
+			PeriodIndex = 10;
+			BillOfEntryNumberIndex = 11;
+			CertificatesIndex = 12;
+			CertificatesDateIndex = 14;
+			RegistryCostIndex = 22;
+			EAN13Index = 26;
+			VitallyImportantIndex = 27;
+			AmountIndex = 28;
+		}
+
+		public static bool CheckFileFormat(string file)
+		{
+			using (var reader = new StreamReader(file, Encoding.GetEncoding(1251))) {
+				var headerCaption = reader.ReadLine();
+				if (!headerCaption.ToLower().Equals("[header]"))
+					return false;
+				var header = reader.ReadLine().Split(';');
+				if (header.Length != 7)
+					return false;
+				if (!header[3].ToLower().Equals("аптека-холдинг"))
+					return false;
+				var bodyCaption = reader.ReadLine();
+				if (!bodyCaption.ToLower().Equals("[body]"))
+					return false;
+				var body = reader.ReadLine().Split(';');
+				if (body.Length != 29)
+					return false;
+			}
+			return true;
+		}
+	}
+}
