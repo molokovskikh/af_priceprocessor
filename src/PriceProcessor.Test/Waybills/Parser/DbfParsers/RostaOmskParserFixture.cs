a13c0fa58f04008d800863ccd9ee0c8e1ef08ff0
Александр Шилин <a.shilin@analit.net>
2012-10-04 19:46:38 +0400

    Новшество #12115. Формат для не разобранных накладных ЗАО Роста г.Казань (53). Добавлен новый формат RostaOmskParser

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/RostaOmskParserFixture.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	public class RostaOmskParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var doc = WaybillParser.Parse("15790421_Роста(129339).DBF");
+			Assert.That(doc.ProviderDocumentId, Is.EqualTo("129339/3"));
+			Assert.That(doc.DocumentDate, Is.EqualTo(DateTime.Parse("28.09.2012")));
+
+			var line = doc.Lines[0];
+			Assert.That(line.Code, Is.EqualTo("80852"));
+			Assert.That(line.Product, Is.EqualTo("Новосепт апельсин паст х 24"));
+			Assert.That(line.Quantity, Is.EqualTo(3));
+			Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(48.01));
+			Assert.That(line.Nds, Is.EqualTo(18));
+			Assert.That(line.NdsAmount, Is.EqualTo(25.93));
+			Assert.That(line.Amount, Is.EqualTo(169.96));
+			Assert.That(line.SerialNumber, Is.EqualTo("25031211"));
+			Assert.That(line.Certificates, Is.EqualTo("РОСС NL ФМ05 Д89796"));
+			Assert.That(line.Producer, Is.EqualTo("Natur Produkt Europa B.V. - Нидерланды"));
+			Assert.That(line.ProducerCostWithoutNDS, Is.EqualTo(46.25));
+			Assert.That(line.Country, Is.EqualTo("Нидерланды"));
+			Assert.That(line.BillOfEntryNumber, Is.EqualTo("10404054/150612/0005121/1"));
+			Assert.That(line.EAN13, Is.EqualTo("4601372002973"));
+
+			Assert.That(doc.Invoice.ShipperInfo, Is.EqualTo("РОСТА"));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/RostaOmskParser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Models;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class RostaOmskParser : IDocumentParser
+	{
+		public Document Parse(string file, Document document)
+		{
+			var data = Dbf.Load(file, Encoding.GetEncoding(1251), false, false);
+			new DbfParser()
+				.DocumentHeader(h => h.ProviderDocumentId, "DOCNUM")
+				.DocumentHeader(h => h.DocumentDate, "DOCDATE")
+				.Line(l => l.Code, "ID")
+				.Line(l => l.Product, "NAME")
+				.Line(l => l.Quantity, "KOL")
+				.Line(l => l.SupplierCostWithoutNDS, "PRICENONDS")
+				.Line(l => l.Nds, "NDSPROC")
+				.Line(l => l.NdsAmount, "NDSSUM")
+				.Line(l => l.Amount, "SUMWNDS")
+				.Line(l => l.SerialNumber, "SER")
+				.Line(l => l.Period, "SROK")
+				.Line(l => l.Certificates, "SERT")
+				.Line(l => l.Producer, "PRO")
+				.Line(l => l.ProducerCostWithoutNDS, "PRONONDS")
+				.Line(l => l.Country, "COUNTRY")
+				.Line(l => l.BillOfEntryNumber, "GTD")
+				.Line(l => l.EAN13, "BARCODE")
+				.DocumentInvoice(i => i.ShipperInfo, "VENDOR")
+				.ToDocument(document, data);
+			return document;
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("PRICENONDS")
+				&& data.Columns.Contains("NDSPROC")
+				&& data.Columns.Contains("PRONONDS");
+		}
+	}
+}
