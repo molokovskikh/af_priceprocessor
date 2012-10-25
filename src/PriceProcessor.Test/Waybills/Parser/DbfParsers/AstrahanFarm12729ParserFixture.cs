56e3ec3b20d27e9022fb06da2c206a35d786c22c
Александр Шилин <a.shilin@analit.net>
2012-10-25 11:44:39 +0400

    Новшество #12685. Формат накладной для Поставщика Астрахань-Фарм, Код 12729. Добавлен новый формат AstrahanFarm12729Parser

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/AstrahanFarm12729ParserFixture.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	public class AstrahanFarm12729ParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var document = WaybillParser.Parse("360616.dbf");
+
+			Assert.That(document.ProviderDocumentId, Is.EqualTo("Рн-Ас00000360616"));
+			Assert.That(document.DocumentDate, Is.EqualTo(DateTime.Parse("23.10.2012")));
+
+			var line = document.Lines[0];
+			Assert.That(line.Product, Is.EqualTo("Ревит др №100"));
+			Assert.That(line.Code, Is.EqualTo("211552656"));
+			Assert.That(line.Producer, Is.EqualTo("Марбиофарм ОАО"));
+			Assert.That(line.Quantity, Is.EqualTo(10));
+			Assert.That(line.SerialNumber, Is.EqualTo("770912"));
+			Assert.That(line.Period, Is.EqualTo("01.10.2013"));
+			Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(9.15));
+			Assert.That(line.SupplierCost, Is.EqualTo(10.06));
+			Assert.That(line.Amount, Is.EqualTo(100.6));
+			Assert.That(line.Nds, Is.EqualTo(10));
+			Assert.That(line.NdsAmount, Is.EqualTo(9.15));
+			Assert.That(line.ProducerCostWithoutNDS, Is.EqualTo(8.36));
+			Assert.That(line.RegistryCost, Is.EqualTo(0));
+			Assert.That(line.VitallyImportant, Is.EqualTo(false));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/AstrahanFarm12729Parser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class AstrahanFarm12729Parser : BaseDbfParser
+	{
+		public override DbfParser GetParser()
+		{
+			return new DbfParser()
+				.DocumentHeader(h => h.ProviderDocumentId, "NAKL")
+				.DocumentHeader(h => h.DocumentDate, "DATENAKL")
+				.Line(l => l.Product, "TOVAR")
+				.Line(l => l.Code, "CODE")
+				.Line(l => l.Producer, "ZAVOD")
+				.Line(l => l.Quantity, "KOLVO")
+				.Line(l => l.SerialNumber, "SERIA")
+				.Line(l => l.Period, "GODEN_DO")
+				.Line(l => l.SupplierCostWithoutNDS, "CENA_B_N")
+				.Line(l => l.SupplierCost, "CENA_S_N")
+				.Line(l => l.Amount, "SUM_S_N")
+				.Line(l => l.Nds, "NDS")
+				.Line(l => l.NdsAmount, "SUM_NDS")
+				.Line(l => l.ProducerCostWithoutNDS, "CENA_IZG")
+				.Line(l => l.RegistryCost, "CENA_REE")
+				.Line(l => l.VitallyImportant, "JNV");
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("CENA_B_N") &&
+				data.Columns.Contains("JNV") &&
+				data.Columns.Contains("ZAVOD");
+		}
+	}
+}
