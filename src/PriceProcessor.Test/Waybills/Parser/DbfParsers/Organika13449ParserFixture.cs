2f73a5c64311ac218525aa3873d55d99408f75ad
Александр Шилин <a.shilin@analit.net>
2012-11-01 17:14:49 +0400

    Новшество #12799. Формат накладной для Поставщика Органика-Юг, Код 13449. Добавлен новый формат Organika13449Parser

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/Organika13449ParserFixture.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	public class Organika13449ParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var document = WaybillParser.Parse("А423789.dbf");
+			Assert.That(document.ProviderDocumentId, Is.EqualTo("А-423789"));
+			Assert.That(document.DocumentDate, Is.EqualTo(DateTime.Parse("31.10.2012")));
+			Assert.That(document.Invoice.Amount, Is.EqualTo(5363.18));
+			Assert.That(document.Invoice.RecipientName, Is.EqualTo("ИП Церенова Е.Н."));
+
+			var line = document.Lines[0];
+			Assert.That(line.Code, Is.EqualTo("590607"));
+			Assert.That(line.Product, Is.EqualTo("Амигренин (таб. 100 мг №2 )"));
+			Assert.That(line.Producer, Is.EqualTo("Верофарм ЗАО"));
+			Assert.That(line.Country, Is.EqualTo("Россия"));
+			Assert.That(line.Quantity, Is.EqualTo(2));
+			Assert.That(line.ProducerCostWithoutNDS, Is.EqualTo(210.74));
+			Assert.That(line.SupplierPriceMarkup, Is.EqualTo(1.19));
+			Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(213.25));
+			Assert.That(line.Amount, Is.EqualTo(469.14));
+			Assert.That(line.Nds, Is.EqualTo(10));
+			Assert.That(line.SerialNumber, Is.EqualTo("91011"));
+			Assert.That(line.Certificates, Is.EqualTo("ФГУ ЦЭиКМП РОСДРАВНАДЗОР №РОСС RU.ФМ01.Д46222 до 01.11.13"));
+			Assert.That(line.Period, Is.EqualTo("01.11.2013"));
+			Assert.That(line.BillOfEntryNumber, Is.Null);
+			Assert.That(line.EAN13, Is.EqualTo("4602930005214"));
+			Assert.That(line.VitallyImportant, Is.EqualTo(false));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/Organika13449Parser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class Organika13449Parser : BaseDbfParser
+	{
+		public override DbfParser GetParser()
+		{
+			return new DbfParser()
+				.DocumentHeader(h => h.ProviderDocumentId, "NUMDOC")
+				.DocumentHeader(h => h.DocumentDate, "DATEDOC")
+				.DocumentInvoice(i => i.Amount, "SUMDOC")
+				.DocumentInvoice(i => i.RecipientName, "APTEKA")
+				.Line(l => l.Code, "CODE")
+				.Line(l => l.Product, "NAME")
+				.Line(l => l.Producer, "MANUFACT")
+				.Line(l => l.Country, "COUNTRY")
+				.Line(l => l.Quantity, "COUNT")
+				.Line(l => l.ProducerCostWithoutNDS, "PRICE_MAN")
+				.Line(l => l.SupplierPriceMarkup, "MARGIN")
+				.Line(l => l.SupplierCostWithoutNDS, "PRICE")
+				.Line(l => l.Amount, "SUM")
+				.Line(l => l.Nds, "NDS")
+				.Line(l => l.SerialNumber, "SER")
+				.Line(l => l.Certificates, "CERT")
+				.Line(l => l.Period, "TERM")
+				.Line(l => l.BillOfEntryNumber, "GTD")
+				.Line(l => l.EAN13, "BARCODE")
+				.Line(l => l.VitallyImportant, "GNVLS");
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("PRICE_MAN")
+				&& data.Columns.Contains("TERM")
+				&& data.Columns.Contains("MANUFACT");
+		}
+	}
+}
