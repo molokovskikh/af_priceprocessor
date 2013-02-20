07d1e447c5487d6b4a969c81dff8d03fd96a125c
Золотарев Александр <a.zolotarev@analit.net>
2013-02-20 15:25:20 +0400

    Выполнил #15039 Формат накладной для поставщика СИА Интернешнл - Краснодар

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/SiaKrasnodarParserFixture.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	class SiaKrasnodarParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var doc = WaybillParser.Parse(@"..\..\Data\Waybills\3522002.DBF");
+
+			Assert.That(doc.ProviderDocumentId, Is.EqualTo("Р-3522002"));
+			Assert.That(doc.DocumentDate, Is.EqualTo(DateTime.Parse("18.02.2013")));
+
+			var l = doc.Lines[0];
+			Assert.That(l.Code, Is.EqualTo("76011"));
+			Assert.That(l.Product, Is.EqualTo("Аводарт 0.5мг Капс. Х30"));
+			Assert.That(l.Producer, Is.EqualTo("Каталент Франс Байнхайм С.А."));
+			Assert.That(l.Country, Is.EqualTo("ФРАНЦИЯ"));
+			Assert.That(l.ProducerCost, Is.EqualTo(1884.4900));
+			Assert.That(l.SupplierCost, Is.EqualTo(1884.49));
+			Assert.That(l.Amount, Is.EqualTo(1884.490));
+			Assert.That(l.NdsAmount, Is.EqualTo(188.4490));
+			Assert.That(l.Quantity, Is.EqualTo(1));
+			Assert.That(l.Period, Is.EqualTo("01.04.2016"));
+			Assert.That(l.Certificates, Is.EqualTo("РОСС FR.ФМ01.Д25841"));
+			Assert.That(l.SerialNumber, Is.EqualTo("062650C"));
+			Assert.That(l.BillOfEntryNumber, Is.EqualTo("10130130/011012/0019410/03"));
+			Assert.That(l.EAN13, Is.EqualTo("4607008130676"));
+			Assert.That(l.Nds, Is.EqualTo(10));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/SiaKrasnodarParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class SiaKrasnodarParser : BaseDbfParser
+	{
+		public override DbfParser GetParser()
+		{
+			return new DbfParser()
+				.DocumentHeader(d => d.ProviderDocumentId, "NDOC")
+				.DocumentHeader(d => d.DocumentDate, "DATEDOC")
+				.Line(l => l.Code, "CODEPST")
+				.Line(l => l.Product, "NAME")
+				.Line(l => l.Producer, "FIRM")
+				.Line(l => l.Country, "CNTR")
+				.Line(l => l.Quantity, "QNT")
+				.Line(l => l.SupplierCost, "PRICE2")
+				.Line(l => l.ProducerCost, "PRICE1")
+				.Line(l => l.Nds, "NDS")
+				.Line(l => l.Amount, "SUMPAY")
+				.Line(l => l.NdsAmount, "SUMNDS2")
+				.Line(l => l.EAN13, "EAN13")
+				.Line(l => l.BillOfEntryNumber, "NUMGTD")
+				.Line(l => l.SerialNumber, "SER")
+				.Line(l => l.Period, "GDATE")
+				.Line(l => l.Certificates, "SERTIF");
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("GDATE")
+				&& data.Columns.Contains("SUMNDS2")
+				&& data.Columns.Contains("PRICE1")
+				&& data.Columns.Contains("PRICE2");
+		}
+	}
+}
