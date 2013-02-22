f76b79d61f5b5243eef1493e1a030e84ed423978
Александр Шилин <a.shilin@analit.net>
2013-02-22 11:36:50 +0400

    Ошибка #13106. Ошибка при сопоставлении синонимов. Увеличил таймаут для передачи результата

--- src/RemotePriceProcessor/PriceProcessorWcfHelper.cs
+++ src/RemotePriceProcessor/PriceProcessorWcfHelper.cs
@@ -95,6 +95,8 @@ namespace RemotePriceProcessor
 			binding.TransferMode = TransferMode.Streamed;
 			binding.MaxReceivedMessageSize = Int32.MaxValue;
 			binding.MaxBufferSize = MaxBufferSize;
+			binding.SendTimeout += new TimeSpan(0, 1, 0);
+			binding.ReceiveTimeout += new TimeSpan(0, 1, 0);
 			return binding;
 		}
 
