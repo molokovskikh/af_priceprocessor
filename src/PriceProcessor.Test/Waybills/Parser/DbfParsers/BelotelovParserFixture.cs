250ac2199a0011fe91fb7c5b33867384b7ed7c57
Золотарев Александр <a.zolotarev@analit.net>
2013-03-13 13:09:09 +0400

    Выполнил #15351 Формат накладной для поставщика Белотелов

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/BelotelovParserFixture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	public class BelotelovParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var doc = WaybillParser.Parse("naknadn.dbf");
+			var line = doc.Lines[0];
+			Assert.That(line.Product, Is.EqualTo("Влажная туалетная бумага Mon Rulon 20шт"));
+			Assert.That(line.Code, Is.EqualTo("09205"));
+			Assert.That(line.Quantity, Is.EqualTo(1));
+			Assert.That(line.Country, Is.EqualTo("Россия"));
+			Assert.That(line.Producer, Is.EqualTo("Авангард ООО"));
+			Assert.That(line.Period, Is.EqualTo("03.02.2013"));
+			Assert.That(line.SupplierCost, Is.EqualTo(19.37));
+			Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(19.37));
+			Assert.That(line.Nds, Is.EqualTo(0));
+			Assert.That(line.Unit, Is.EqualTo("шт"));
+			Assert.That(line.EAN13, Is.EqualTo("4607091481235"));
+			Assert.That(line.OrderId, Is.EqualTo(39676595));
+			Assert.That(line.VitallyImportant, Is.EqualTo(false));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/BelotelovParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class BelotelovParser : BaseDbfParser
+	{
+		public override DbfParser GetParser()
+		{
+			return new DbfParser()
+				.Line(l => l.Code, "SP_PRD_ID")
+				.Line(l => l.Product, "NAME_POST")
+				.Line(l => l.Quantity, "KOL_TOV")
+				.Line(l => l.Period, "SGODN")
+				.Line(l => l.Country, "CNTR_POST")
+				.Line(l => l.Producer, "PRZV_POST")
+				.Line(l => l.Nds, "NDS")
+				.Line(l => l.SupplierCost, "PCENA_NDS")
+				.Line(l => l.SupplierCostWithoutNDS, "PCENA_BNDS")
+				.Line(l => l.Unit, "UNIT")
+				.Line(l => l.EAN13, "BAR_CODE")
+				.Line(l => l.NdsAmount, "NDS_SUM")
+				.Line(l => l.VitallyImportant, "GNVLS")
+				.Line(l => l.OrderId, "ORDERID");
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("SP_PRD_ID") &&
+				data.Columns.Contains("PCENA_BNDS") &&
+				data.Columns.Contains("PCENA_NDS") &&
+				data.Columns.Contains("ORDERID") &&
+				data.Columns.Contains("CNTR_POST") &&
+				data.Columns.Contains("NAME_POST") &&
+				data.Columns.Contains("BAR_CODE");
+		}
+	}
+}
