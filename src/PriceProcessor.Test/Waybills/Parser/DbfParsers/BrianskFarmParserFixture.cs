c8e62a4cb6199853952f676a54056f4f5ed1e16f
Золотарев Александр <a.zolotarev@analit.net>
2013-06-11 12:15:45 +0400

    Выполнил #17741 Формат накладной для Брянскфарм

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/BrianskFarmParserFixture.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Parser.DbfParsers;
+using NUnit.Framework;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	public class BrianskFarmParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			Assert.IsTrue(BrianskFarmParser.CheckFileFormat(Dbf.Load(@"..\..\Data\Waybills\77277071.DBF")));
+			var doc = WaybillParser.Parse("77277071.DBF");
+			var line = doc.Lines[0];
+			var invoice = doc.Invoice;
+			Assert.That(doc.ProviderDocumentId, Is.EqualTo("6_77277071"));
+			Assert.That(doc.DocumentDate.Value.ToShortDateString(), Is.EqualTo("07.06.2013"));
+			Assert.That(line.Code, Is.EqualTo("4160004270"));
+			Assert.That(line.Product, Is.EqualTo("Диклофенак гель 5% 30г туба (инд уп)"));
+			Assert.That(line.Producer, Is.EqualTo("Синтез"));
+			Assert.That(doc.Lines[1].BillOfEntryNumber, Is.EqualTo("10113080/250213/0003055/1"));
+			Assert.That(line.SerialNumber, Is.EqualTo("220313"));
+			Assert.That(line.Period, Is.EqualTo("01.04.2015"));
+			Assert.That(line.Quantity, Is.EqualTo(4));
+			Assert.That(line.SupplierCost, Is.EqualTo(31.35));
+			Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(28.5));
+			Assert.That(line.NdsAmount, Is.EqualTo(11.4));
+			Assert.That(line.Amount, Is.EqualTo(125.4));
+			Assert.That(line.ProducerCostWithoutNDS, Is.EqualTo(25.91));
+			Assert.That(line.RegistryCost, Is.EqualTo(0));
+			Assert.That(line.Certificates, Is.EqualTo("РОСС RU.ФМ05.Д50228"));
+			Assert.That(line.CertificatesDate, Is.EqualTo(null));
+			Assert.That(line.CertificateAuthority, Is.EqualTo("ООО Окружной ЦКК , г.Москва"));
+			Assert.That(line.SupplierPriceMarkup, Is.EqualTo(10));
+			Assert.That(doc.Lines[1].BillOfEntryNumber, Is.EqualTo("10113080/250213/0003055/1"));
+			Assert.That(line.Unit, Is.EqualTo("шт."));
+			Assert.That(line.EAN13, Is.EqualTo("4602565013509"));
+			Assert.That(invoice.InvoiceNumber, Is.EqualTo("6_77277071"));
+			Assert.That(invoice.InvoiceDate.Value.ToShortDateString(), Is.EqualTo("07.06.2013"));
+			Assert.That(invoice.SellerName, Is.EqualTo("ООО \" Брянскфарм\""));
+			Assert.That(invoice.SellerAddress, Is.EqualTo("242620, Брянская обл, Дятьковский р-н, п. Любохна, ул. Сидорова, д,2"));
+			Assert.That(invoice.SellerINN, Is.EqualTo("3202009890"));
+			Assert.That(invoice.SellerKPP, Is.EqualTo("324501001"));
+			Assert.That(invoice.BuyerName, Is.EqualTo("ИП Буренок С, Я,"));
+			Assert.That(invoice.BuyerAddress, Is.EqualTo("г.Брянск, ул.Авиационная, 5-а"));
+			Assert.That(invoice.BuyerINN, Is.EqualTo("320700042889"));
+			Assert.That(invoice.BuyerKPP, Is.EqualTo(null));
+			Assert.That(invoice.AmountWithoutNDS10, Is.EqualTo(2425.1));
+			Assert.That(invoice.NDSAmount10, Is.EqualTo(242.5));
+			Assert.That(invoice.Amount10, Is.EqualTo(2667.6));
+			Assert.That(invoice.AmountWithoutNDS0, Is.EqualTo(0));
+			Assert.That(invoice.AmountWithoutNDS18, Is.EqualTo(0));
+			Assert.That(invoice.NDSAmount18, Is.EqualTo(0));
+			Assert.That(invoice.Amount18, Is.EqualTo(0));
+			Assert.That(invoice.AmountWithoutNDS, Is.EqualTo(2425.1));
+			Assert.That(invoice.NDSAmount, Is.EqualTo(242.5));
+			Assert.That(invoice.Amount, Is.EqualTo(2667.6));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/BrianskFarmParser.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class BrianskFarmParser : BaseDbfParser
+	{
+		public override DbfParser GetParser()
+		{
+			return new DbfParser()
+				.DocumentHeader(d => d.ProviderDocumentId, "ttn")
+				.DocumentHeader(d => d.DocumentDate, "ttn_date")
+				.DocumentInvoice(i => i.InvoiceNumber, "i_num")
+				.DocumentInvoice(i => i.InvoiceDate, "i_date")
+				.DocumentInvoice(i => i.SellerName, "i_sel_name")
+				.DocumentInvoice(i => i.SellerAddress, "i_sel_adr")
+				.DocumentInvoice(i => i.SellerINN, "i_sel_inn")
+				.DocumentInvoice(i => i.SellerKPP, "i_sel_kpp")
+				.DocumentInvoice(i => i.BuyerName, "i_bu_name")
+				.DocumentInvoice(i => i.BuyerAddress, "i_bu_adr")
+				.DocumentInvoice(i => i.BuyerINN, "i_bu_inn")
+				.DocumentInvoice(i => i.BuyerKPP, "i_bu_kpp")
+				.DocumentInvoice(i => i.AmountWithoutNDS0, "amnt_e_0")
+				.DocumentInvoice(i => i.AmountWithoutNDS10, "amnt_e_10")
+				.DocumentInvoice(i => i.AmountWithoutNDS18, "amnt_e_18")
+				.DocumentInvoice(i => i.Amount18, "amnt_w_18")
+				.DocumentInvoice(i => i.Amount10, "amnt_w_10")
+				.DocumentInvoice(i => i.NDSAmount, "amnt_n_all")
+				.DocumentInvoice(i => i.NDSAmount10, "amnt_n_10")
+				.DocumentInvoice(i => i.NDSAmount18, "amnt_n_18")
+				.DocumentInvoice(i => i.AmountWithoutNDS, "amnt_e_all")
+				.DocumentInvoice(i => i.Amount, "amnt")
+				.Line(l => l.Product, "name_post")
+				.Line(l => l.Producer, "przv_post")
+				.Line(l => l.Quantity, "kol_tov")
+				.Line(l => l.Period, "sgodn")
+				.Line(l => l.SupplierCost, "pcena_nds")
+				.Line(l => l.SupplierCostWithoutNDS, "pcena_bnds")
+				.Line(l => l.ProducerCostWithoutNDS, "PRCENABNDS")
+				.Line(l => l.RegistryCost, "gr_cena")
+				.Line(l => l.SerialNumber, "seria")
+				.Line(l => l.Certificates, "sert")
+				.Line(l => l.CertificatesDate, "sert_date")
+				.Line(l => l.CertificateAuthority, "sert_auth")
+				.Line(l => l.Code, "sp_prd_id")
+				.Line(l => l.Nds, "nds")
+				.Line(l => l.SupplierPriceMarkup, "sp_markup")
+				.Line(l => l.VitallyImportant, "vt")
+				.Line(l => l.NdsAmount, "p_nds_amnt")
+				.Line(l => l.Amount, "p_amnt")
+				.Line(l => l.Unit, "unit")
+				.Line(l => l.BillOfEntryNumber, "bll_ntr_id")
+				.Line(l => l.EAN13, "bar_code");
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("pcena_bnds")
+				&& data.Columns.Contains("bll_ntr_id")
+				&& data.Columns.Contains("p_nds_amnt")
+				&& data.Columns.Contains("amnt_e_all")
+				&& data.Columns.Contains("sp_markup");
+		}
+	}
+}
