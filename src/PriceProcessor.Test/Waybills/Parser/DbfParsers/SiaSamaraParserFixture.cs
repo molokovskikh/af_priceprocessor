53eedf4a2ac3f90dbb30f7b14ef61c0c159bb79b
Анастасия Червинская <kitten112007@mail.ru>
2013-07-23 12:41:18 +0400

    #18599 Формат накладной для Поставщика СИА-Самара, Код 3304

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/SiaSamaraParserFixture.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using PriceProcessor.Test.TestHelpers;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	class SiaSamaraParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var doc = WaybillParser.Parse("Р-1017256.DBF");
+
+			Assert.That(doc.ProviderDocumentId, Is.EqualTo("Р-1017256"));
+			Assert.That(doc.DocumentDate, Is.EqualTo(DateTime.Parse("23.07.2013")));
+			var l = doc.Lines[0];
+			Assert.That(l.Code, Is.EqualTo("167"));
+			Assert.That(l.Quantity, Is.EqualTo(10));
+			Assert.That(l.SupplierCost, Is.EqualTo(78.03));
+			Assert.That(l.Nds, Is.EqualTo(10));
+			Assert.That(l.NdsAmount, Is.EqualTo(70.94));
+			Assert.That(l.Amount, Is.EqualTo(780.34));
+			Assert.That(l.SupplierCostWithoutNDS, Is.EqualTo(70.94));
+			Assert.That(l.ProducerCost, Is.EqualTo(null));
+			Assert.That(l.ProducerCostWithoutNDS, Is.EqualTo(null));
+			Assert.That(l.SupplierPriceMarkup, Is.EqualTo(null));
+			Assert.That(l.RegistryCost, Is.EqualTo(null));
+			Assert.That(l.RegistryDate, Is.Null);
+			Assert.That(l.SerialNumber, Is.EqualTo("DC4294"));
+			Assert.That(l.BillOfEntryNumber, Is.EqualTo("10130060/120313/0005747/04"));
+			Assert.That(l.Certificates, Is.EqualTo("POCC SI.ФМ08.Д39137"));
+			Assert.That(l.Product, Is.EqualTo("5-нок 50мг Таб. П/о Х50 Б"));
+			Assert.That(l.Producer, Is.EqualTo("Лек д.д., СЛОВЕНИЯ"));
+			Assert.That(l.Period, Is.EqualTo("01.01.2018"));
+			Assert.That(l.EAN13, Is.EqualTo("3838957090976"));
+			Assert.That(l.VitallyImportant, Is.EqualTo(false));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/SiaSamaraParser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Models;
+using Inforoom.PriceProcessor.Waybills.Parser.Helpers;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class SiaSamaraParser : BaseDbfParser
+	{
+		public override DbfParser GetParser()
+		{
+			return new DbfParser()
+				.DocumentHeader(d => d.ProviderDocumentId, "DCODE")
+				.DocumentHeader(d => d.DocumentDate, "DATE_DOC")
+				.Line(l => l.Code, "CODE")
+				.Line(l => l.Quantity, "KOLVO")
+				.Line(l => l.SupplierCost, "PRICE_OPL")
+				.Line(l => l.Nds, "NDS_PR")
+				.Line(l => l.NdsAmount, "NDS_SUM")
+				.Line(l => l.Amount, "SUM_OPL")
+				.Line(l => l.SupplierCostWithoutNDS, "PRICE_BASE")
+				.Line(l => l.ProducerCost, "PRO")
+				.Line(l => l.ProducerCostWithoutNDS, "PRO_NNDS")
+				.Line(l => l.SupplierPriceMarkup, "NC_OPT_PR")
+				.Line(l => l.RegistryCost, "PRICE_REES")
+				.Line(l => l.RegistryDate, "DATE_REES")
+				.Line(l => l.SerialNumber, "SERIES")
+				.Line(l => l.BillOfEntryNumber, "GTD")
+				.Line(l => l.Certificates, "SERT_N")
+				.Line(l => l.Product, "PRODUCT")
+				.Line(l => l.Producer, "PRODUCER")
+				.Line(l => l.Period, "SROK_S")
+				.Line(l => l.EAN13, "EAN13")
+				.Line(l => l.VitallyImportant, "ZVLS")
+				.Line(l => l.CertificateAuthority, "SERT_N_REG");
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("CODE")
+				&& data.Columns.Contains("KOLVO")
+				&& data.Columns.Contains("PRICE_OPL")
+				&& data.Columns.Contains("PRO_NNDS")
+				&& !data.Columns.Contains("CNTR")
+				&& data.Columns.Contains("ZVLS");
+		}
+	}
+}
