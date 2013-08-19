63e58bf801c84de714a93efe6b7b758aba5b2841
Золотарев Александр <a.zolotarev@analit.net>
2013-08-19 10:22:12 +0400

    Написал пиграцию для правильного изменения Ids в реалиях нового формата #18083

---
+++ migrations/959_00_changeCodePages.sql
@@ -0,0 +1,28 @@
+﻿update farm.FormRules
+set PriceFormatId = 19
+where PriceFormatId = 11;
+
+update farm.FormRules
+set PriceEncode = 1251
+where PriceFormatId = 19;
+
+update farm.FormRules
+set PriceFormatId = 19
+where PriceFormatId = 12;
+
+
+update farm.FormRules
+set PriceFormatId = 21
+where PriceFormatId = 13;
+
+update farm.FormRules
+set PriceEncode = 1251
+where PriceFormatId = 21;
+
+update farm.FormRules
+set PriceFormatId = 21
+where PriceFormatId = 14;
+
+update farm.formrules f
+set PriceEncode = 0
+where PriceFormatId not in (8, 19, 21);
