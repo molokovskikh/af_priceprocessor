02340f779e55fff436275663f10a2de4ffae4760
Квасов Роман <r.kvasov@analit.net>
2013-12-20 13:42:21 +0400

    Новшество #21541 Формат накладной для Поставщика АСМ-медикал, Код 5836

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/ASMParserFixture.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using PriceProcessor.Test.TestHelpers;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	public class ASMParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var document = WaybillParser.Parse("n1013273-медикал.dbf");
+			Assert.AreEqual(53, document.Lines.Count);
+			var line = document.Lines[0];
+			Assert.AreEqual("1013273", document.ProviderDocumentId);
+			Assert.AreEqual("1028517", line.Code);
+			Assert.AreEqual("тампоны 60х30мм спирт.стер. софт-зеллин уп №100", line.Product);
+			Assert.AreEqual("466", line.CodeCr);
+			Assert.AreEqual("пауль хартманн", line.Producer);
+			Assert.AreEqual("германия", line.Country);
+			Assert.AreEqual(@"10130142/291013/0007665/1", line.BillOfEntryNumber);
+			Assert.AreEqual("9999791", line.SerialNumber);
+			Assert.AreEqual("РОСС DE.АЕ83.Д13814", line.Certificates);
+			Assert.AreEqual("05.08.2015", line.CertificatesDate);
+			Assert.AreEqual(10, line.Nds);
+			Assert.AreEqual(64.28, line.SupplierCost);
+			Assert.AreEqual(321.4, line.Amount);
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/ASMParser2.cs
@@ -0,0 +1,27 @@
+﻿namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class ASMParser2 : BaseDbfParser2
+	{
+		public override DbfParser GetParser()
+		{
+			return new DbfParser()
+				.DocumentHeader(h => h.ProviderDocumentId, "DOC_ID")
+				.DocumentHeader(h => h.DocumentDate, "DOC_DATE")
+				.Invoice(i => i.RecipientAddress, "STAN")
+				.Line(l => l.Code, "G_CODE")
+				.Line(l => l.Product, "G_NAME")
+				.Line(l => l.CodeCr, "M_CODE")
+				.Line(l => l.Producer, "M_NAME")
+				.Line(l => l.Country, "C_NAME")
+				.Line(l => l.BillOfEntryNumber, "GTD")
+				.Line(l => l.SerialNumber, "SERIES")
+				.Line(l => l.Certificates, "CER_ID")
+				.Line(l => l.CertificatesDate, "CER_EXPR")
+				.Line(l => l.Quantity, "QUANTITY")
+				.Line(l => l.Nds, "PR_NDS")
+				.Line(l => l.SupplierCostWithoutNDS, "CENABEZNDS")
+				.Line(l => l.SupplierCost, "CENASNDS")
+				.Line(l => l.Amount, "SUM");
+		}
+	}
+}
