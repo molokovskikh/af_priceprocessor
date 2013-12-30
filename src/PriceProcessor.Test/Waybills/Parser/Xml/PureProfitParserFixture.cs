8c13ec954b1f4ee65af0562bc606cbb16a028965
Квасов Роман <r.kvasov@analit.net>
2013-12-30 10:11:39 +0400

    Новшество #21747 Формат накладной для Поставщика Чистая выгода, Код 15285

---
+++ src/PriceProcessor.Test/Waybills/Parser/Xml/PureProfitParserFixture.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using PriceProcessor.Test.TestHelpers;
+
+namespace PriceProcessor.Test.Waybills.Parser
+{
+	[TestFixture]
+	public class PureProfitParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var doc = WaybillParser.Parse(@"PureProfit.XML");
+			Assert.AreEqual("404", doc.ProviderDocumentId);
+			Assert.AreEqual("18.05.2012", doc.DocumentDate.Value.ToShortDateString());
+			Assert.AreEqual(12, doc.Lines.Count);
+			var line = doc.Lines[0];
+			Assert.AreEqual("Б0006843", line.Code);
+			Assert.AreEqual("4015400122807", line.EAN13);
+			Assert.AreEqual("P&G гиг.подгузники PAMPERS  (2) Mini 18 шт Слип энд Плей", line.Product);
+			Assert.AreEqual(10, line.Quantity);
+			Assert.AreEqual(118.21, line.SupplierCostWithoutNDS);
+			Assert.AreEqual(130.03, line.SupplierCost);
+			Assert.AreEqual(10, line.Nds);
+			Assert.AreEqual(1300.3, line.Amount);
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/XmlParsers/PureProfitParser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Models;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.XmlParsers
+{
+	public class PureProfitParser : IDocumentParser
+	{
+		public Document Parse(string file, Document document)
+		{
+			var xdocument = XDocument.Load(file);
+			var order = xdocument.XPathSelectElement("Order");
+			document.ProviderDocumentId = (string)order.XPathSelectElement("OrderId");
+			document.DocumentDate = NullableConvert.ToDateTime((string)order.XPathSelectElement("OrderDate"));
+			foreach (var item in order.XPathSelectElements("Items/Item")) {
+				var line = document.NewLine();
+				line.Product = (string)item.XPathSelectElement("GoodsName");
+				line.Code = (string)item.XPathSelectElement("ItemId");
+				line.EAN13 = (string)item.XPathSelectElement("EAN13");
+				line.Quantity = (uint?)item.XPathSelectElement("Quantity");
+				line.SupplierCostWithoutNDS = (decimal?)item.XPathSelectElement("CostClear");
+				line.SupplierCost = (decimal?)item.XPathSelectElement("Cost");
+				line.Nds = (uint?)item.XPathSelectElement("STNDS");
+				line.NdsAmount = (decimal?)item.XPathSelectElement("NDS");
+				line.Amount = (decimal?)item.XPathSelectElement("SUMMA");
+			}
+			return document;
+		}
+
+		public static bool CheckFileFormat(string file)
+		{
+			var document = XDocument.Load(file);
+			return document.XPathSelectElement("Order/Items/Item") != null;
+		}
+	}
+}
