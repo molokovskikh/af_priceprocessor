23b042ce85ade9b9c7cea4d97831daedb289cbf5
Квасов Роман <r.kvasov@analit.net>
2015-01-10 10:09:44 +0300

    Исправил функцию созданию синонимов производителей

--- migrations/1469_03_update_data_automatic_producer_synonym.sql
+++ migrations/1469_03_update_data_automatic_producer_synonym.sql
@@ -1,3 +1,4 @@
 update Farm.AutomaticProducerSynonyms a
 join Farm.SynonymFirmCr s on s.SynonymFirmCrCode = a.ProducerSynonymId
-set a.SynonymText = s.Synonym, a.PriceCode = s.PriceCode;
+set a.SynonymText = s.Synonym, a.PriceCode = s.PriceCode
+where s.CodeFirmCr is null;
---
+++ migrations/1470_00_fix_create_producer_synonym.sql
@@ -0,0 +1,22 @@
+DROP FUNCTION Farm.CreateProducerSynonym;
+CREATE DEFINER=`RootDBMS`@`127.0.0.1` FUNCTION Farm.`CreateProducerSynonym`( PriceId bigint unsigned,  ProducerId bigint unsigned,  Synonym varchar(255),  IsAutomatic bool ) RETURNS bigint(20) unsigned
+    DETERMINISTIC
+    SQL SECURITY INVOKER
+BEGIN
+	declare LastProducerSynonymId bigint unsigned;
+
+	set LastProducerSynonymId = (select SynonymFirmCrCode from farm.SynonymFirmCr ps where ps.Synonym = Synonym and ps.CodeFirmCr <=> ProducerId and ps.PriceCode=PriceId limit 1);
+
+
+	if LastProducerSynonymId is null then
+		insert into farm.SynonymFirmCr (PriceCode, CodeFirmCr, Synonym) values (PriceId, ProducerId, Synonym);
+		set LastProducerSynonymId = last_insert_id();
+		insert farm.UsedSynonymFirmCrLogs (SynonymFirmCrCode) values (LastProducerSynonymId);
+
+		if IsAutomatic and ProducerId is null then
+			insert into AutomaticProducerSynonyms(ProducerSynonymId, PriceCode, SynonymText) values(LastProducerSynonymId, PriceId, Synonym);
+		end if;
+	end if;
+
+	return LastProducerSynonymId;
+END;
