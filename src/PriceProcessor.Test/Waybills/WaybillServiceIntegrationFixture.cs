3d3b1cb23c6289df933b445fbb40eaccc151e0a4
Квасов Роман <r.kvasov@analit.net>
2015-04-02 15:08:14 +0300

    Удалил устаревший тест

--- src/PriceProcessor.Test/Waybills/WaybillServiceIntegrationFixture.cs
+++ src/PriceProcessor.Test/Waybills/WaybillServiceIntegrationFixture.cs
@@ -15,29 +15,6 @@ namespace PriceProcessor.Test.Waybills
 {
 	public class WaybillServiceIntegrationFixture : IntegrationFixture
 	{
-		[Test, Description("Тестирует сохранение отклоненной накладной")]
-		public void RejectWaybillSaveTest()
-		{
-			var log = new DocumentReceiveLog();
-			var client = TestClient.CreateNaked();
-			client.Addresses[0].Enabled = false;
-			Save(client);
-			var supplier = TestSupplier.Create();
-
-			log.Address = session.Query<Address>().First(a => a.Id == client.Addresses[0].Id);
-			log.ClientCode = client.Id;
-			log.Supplier = session.Query<Supplier>().First(a => a.Id == supplier.Id);
-			log.Comment = "комментарий";
-			log.DocumentSize = 12;
-			log.DocumentType = DocType.Waybill;
-			log.FileName = "файл";
-
-			WaybillService.ParseWaybill(log);
-			var savedDoc = session.Query<RejectWaybillLog>().Where(t => t.ClientCode == client.Id && t.Supplier == log.Supplier);
-			Assert.That(savedDoc.Count(), Is.GreaterThan(0));
-			Assert.That(savedDoc.First().RejectReason, Is.EqualTo(RejectReasonType.AddressDisable));
-		}
-
 		[Test, Description("Тестирует сопоставление накладной")]
 		public void DocumentSetIdTest()
 		{
