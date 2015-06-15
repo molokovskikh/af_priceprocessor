3c120925312803e80796a1207e9d0dc232523d97
Екатерина Дудочкина <edudochkina@analit.net>
2015-06-15 13:59:55 +0300

    Разбор отказов

---
+++ src/PriceProcessor.Test/Waybills/Rejects/VremyaFTK1422RejectParserFixture.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Inforoom.PriceProcessor.Models;
+using Inforoom.PriceProcessor.Waybills.Models;
+using Inforoom.PriceProcessor.Waybills.Rejects.Parser;
+using NHibernate.Linq;
+using NPOI.SS.Formula.Functions;
+using NUnit.Framework;
+using PriceProcessor.Test.TestHelpers;
+using PriceProcessor.Test.Waybills.Rejects.Infrastructure;
+using Test.Support;
+using Test.Support.Suppliers;
+
+namespace PriceProcessor.Test.Waybills.Rejects
+{
+	[TestFixture]
+	class VremyaFTK1422RejectParserFixture : BaseRejectFixture
+	{
+		/// <summary>
+		/// Тест к задаче http://redmine.analit.net/issues/33351
+		/// </summary>
+		[Test]
+		public void Parse()
+		{
+			//Создаем лог, а затем отказ
+			var log = CreateRejectLog("38373390_Время ФТК(16814_70645350).dbf");
+			var parser = new VremyaFTK1422RejectParser();
+			var reject = parser.CreateReject(log);
+
+			//Проверяем правильность парсинга			
+			Assert.That(reject.Lines.Count, Is.EqualTo(1));
+			Assert.That(parser.BadLines.Count, Is.EqualTo(0));
+
+			//Выбираем строку и проверяем правильно ли все распарсилось
+			var line = reject.Lines[0];
+			Assert.That(line.Product, Is.EqualTo("Триовит капс. №30"));
+			Assert.That(line.Code, Is.EqualTo("04610"));
+			Assert.That(line.Cost, Is.EqualTo(118.70));
+			Assert.That(line.Ordered, Is.EqualTo(1));
+			Assert.That(line.Rejected, Is.EqualTo(1));
+		}
+	}
+}
