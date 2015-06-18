95460351c596a4bbd871c788f4c222cf8dcf3d92
Квасов Роман <r.kvasov@analit.net>
2015-06-18 11:26:58 +0300

    Исправил тесты

--- src/PriceProcessor.Test/SetupFixture.cs
+++ src/PriceProcessor.Test/SetupFixture.cs
@@ -19,6 +19,8 @@ namespace PriceProcessor.Test
 		public void Setup()
 		{
 			XmlConfigurator.Configure();
+			ConnectionHelper.DefaultConnectionStringName = "db";
+			With.DefaultConnectionStringName = "db";
 			ArchiveHelper.SevenZipExePath = @".\7zip\7z.exe";
 			With.DefaultConnectionStringName = ConnectionHelper.DefaultConnectionStringName;
 			//мы не должны обращаться к настроящему ftp, вместо этого нужно использовать директорию для эмуляции
