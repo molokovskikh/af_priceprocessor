2ca74a56f797d8f87c3bfb46c77f7d00db5d9572
Екатерина Дудочкина <edudochkina@analit.net>
2015-07-07 11:45:06 +0300

    BSSKazan15445 Разбор отказов

---
+++ src/PriceProcessor.Test/Waybills/Rejects/BSSKazan15445RejectParserFixture.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Inforoom.PriceProcessor.Models;
+using Inforoom.PriceProcessor.Waybills.Models;
+using Inforoom.PriceProcessor.Waybills.Rejects.Parser;
+using NHibernate.Linq;
+using NPOI.SS.Formula.Functions;
+using NUnit.Framework;
+using PriceProcessor.Test.TestHelpers;
+using PriceProcessor.Test.Waybills.Rejects.Infrastructure;
+using Test.Support;
+using Test.Support.Suppliers;
+
+namespace PriceProcessor.Test.Waybills.Rejects
+{
+	[TestFixture]
+	class BSSKazan15445RejectParserFixture : BaseRejectFixture
+	{
+		/// <summary>
+		/// Тест к задаче http://redmine.analit.net/issues/33351
+		/// </summary>
+		[Test]
+		public void Parse()
+		{
+			//Создаем лог, а затем отказ
+			var log = CreateRejectLog("38418528_БСС-Казань(14512_70729907).dbf");
+			var parser = new BSSKazan15445RejectParser();
+			var reject = parser.CreateReject(log);
+
+			//Проверяем правильность парсинга			
+			Assert.That(reject.Lines.Count, Is.EqualTo(3));
+			Assert.That(parser.BadLines.Count, Is.EqualTo(0));
+
+			//Выбираем строку и проверяем правильно ли все распарсилось
+			var line = reject.Lines[0];
+			Assert.That(line.Product, Is.EqualTo("Хондроксид мазь 5% 30г"));
+			Assert.That(line.Code, Is.EqualTo("04949"));
+			Assert.That(line.Cost, Is.EqualTo(305.10));
+			Assert.That(line.Ordered, Is.EqualTo(1));
+			Assert.That(line.Rejected, Is.EqualTo(1));
+		}
+	}
+}
