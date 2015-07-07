3b102aeaffa9d17e1c79a11ff44225314839b004
Екатерина Дудочкина <edudochkina@analit.net>
2015-07-07 18:21:06 +0300

    merge commit

---
+++ src/PriceProcessor.Test/Waybills/Rejects/FarmkomplektVoroneth4365RejectParserFixture.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Inforoom.PriceProcessor.Models;
+using Inforoom.PriceProcessor.Waybills.Models;
+using Inforoom.PriceProcessor.Waybills.Rejects.Parser;
+using NHibernate.Linq;
+using NPOI.SS.Formula.Functions;
+using NUnit.Framework;
+using PriceProcessor.Test.TestHelpers;
+using PriceProcessor.Test.Waybills.Rejects.Infrastructure;
+using Test.Support;
+using Test.Support.Suppliers;
+
+namespace PriceProcessor.Test.Waybills.Rejects
+{
+	[TestFixture]
+	class FarmkomplektVoroneth4365RejectParserFixture : BaseRejectFixture
+	{
+		/// <summary>
+		/// Тест к задаче http://redmine.analit.net/issues/33351
+		/// </summary>
+		[Test]
+		public void Parse()
+		{
+			//Создаем лог, а затем отказ
+			var log = CreateRejectLog("38382962_Фармкомплект-Воронеж(Отказ по заявке Аптека).txt");
+			var parser = new FarmkomplektVoronezh4365RejectParser();
+			var reject = parser.CreateReject(log);
+
+			//Проверяем правильность парсинга			
+			Assert.That(reject.Lines.Count, Is.EqualTo(1));
+			Assert.That(parser.BadLines.Count, Is.EqualTo(0));
+
+			//Выбираем строку и проверяем правильно ли все распарсилось
+			var line = reject.Lines[0];
+			Assert.That(line.Product, Is.EqualTo("Супрадин №30 таб. п/о  Dragenopharm"));
+			Assert.That(line.Ordered, Is.EqualTo(2));
+			Assert.That(line.Rejected, Is.EqualTo(2));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Rejects/Parser/FarmkomplektVoronezh4365RejectParser.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Common.Tools;
+using Inforoom.PriceProcessor.Models;
+using NPOI.POIFS.FileSystem;
+
+namespace Inforoom.PriceProcessor.Waybills.Rejects.Parser
+{
+	/// <summary>
+	/// Парсер для поставщика 4365. На 07.07.2015 Типы отказов: txt(2010).
+	/// </summary>
+	public class FarmkomplektVoronezh4365RejectParser : RejectParser
+	{
+		public override void Parse(RejectHeader reject, string filename)
+		{
+			if (filename.Contains(".txt"))
+				ParseTXT(reject, filename);
+			else {
+				Logger.WarnFormat("Файл '{0}' не может быть распарсен, так как парсер {1} не умеет парсить данный формат файла", filename, GetType().Name);
+			}
+		}
+
+		/// <summary>
+		/// Парсер для формата файла TXT
+		/// </summary>
+		protected void ParseTXT(RejectHeader reject, string filename)
+		{
+			using (var reader = new StreamReader(File.OpenRead(filename), Encoding.GetEncoding(1251)))
+			{
+				//считываем весь текст из файла и проверяем на наличие слова Отказы
+				//из-за проблемы с кодировкой в некоторых файлах
+				//это сделано на случай страховки,если вдруг текст в файле все же не будет считываться правильно
+				string file = reader.ReadToEnd();
+				if (!file.Contains("Отказы"))
+					Logger.WarnFormat("Файл '{0}' не может быть распарсен, так как парсер {1} не может парсить данный файл из-за проблемы с кодировкой", filename, GetType().Name);
+				else {
+					var parts = file.Split(new[] { "\r\n" }, StringSplitOptions.None);
+					var rejectFound = true;
+					for(var i = 1; i < parts.Count(); i++)
+					{
+						if (parts[i] == "")
+							rejectFound = false;
+
+						if (!rejectFound)
+							continue;
+
+						var rejectLine = new RejectLine();
+						reject.Lines.Add(rejectLine);
+						var splat = parts[i].Split(new[] { "; заказано " }, StringSplitOptions.None);
+						var product = splat[0];
+						rejectLine.Product = product;
+						var splat2 = splat[1].Split(new[] { " отпущено " }, StringSplitOptions.None);
+						var overed = NullableConvert.ToUInt32(splat2[0]);
+						var released = NullableConvert.ToUInt32(splat2[1]);
+						rejectLine.Ordered = overed;
+						var rejected = overed - released;
+						rejectLine.Rejected = rejected != null ? rejected.Value : 0;
+					}
+				}
+			}
+		}
+	}
+}
