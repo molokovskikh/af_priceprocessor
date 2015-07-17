7d91fd6f7c2115e507409a409f033a0f792e52fe
Екатерина Дудочкина <edudochkina@analit.net>
2015-07-17 17:48:30 +0300

    AralPlus44 Разбор отказов

---
+++ src/PriceProcessor.Test/Waybills/Rejects/AralPlus44RejectParserFixture.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Inforoom.PriceProcessor.Models;
+using Inforoom.PriceProcessor.Waybills.Models;
+using Inforoom.PriceProcessor.Waybills.Rejects.Parser;
+using NHibernate.Linq;
+using NPOI.SS.Formula.Functions;
+using NUnit.Framework;
+using PriceProcessor.Test.TestHelpers;
+using PriceProcessor.Test.Waybills.Rejects.Infrastructure;
+using Test.Support;
+using Test.Support.Suppliers;
+
+namespace PriceProcessor.Test.Waybills.Rejects
+{
+	[TestFixture]
+	class AralPlus44RejectParserFixture : BaseRejectFixture
+	{
+		/// <summary>
+		/// Тест к задаче http://redmine.analit.net/issues/33351
+		/// </summary>
+		[Test]
+		public void Parse()
+		{
+			//Создаем лог, а затем отказ
+			var log = CreateRejectLog("38412792_Арал-Плюс(6832_Фармбытхим_ г.Тула(дп _51367)8924825df).txt");
+			var parser = new AralPlus44RejectParser();
+			var reject = parser.CreateReject(log);
+
+			//Проверяем правильность парсинга			
+			Assert.That(reject.Lines.Count, Is.EqualTo(1));
+			Assert.That(parser.BadLines.Count, Is.EqualTo(0));
+
+			//Выбираем строку и проверяем правильно ли все распарсилось
+			var line = reject.Lines[0];
+			Assert.That(line.Product, Is.EqualTo("Цианокобаламин, 500 мкг амп.№10"));
+			Assert.That(line.Ordered, Is.EqualTo(5));
+			Assert.That(line.Rejected, Is.EqualTo(5));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Rejects/Parser/AralPlus44RejectParser.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Common.Tools;
+using Inforoom.PriceProcessor.Models;
+
+namespace Inforoom.PriceProcessor.Waybills.Rejects.Parser
+{
+	/// <summary>
+	/// Парсер для поставщика 44. На 17.07.2015 Типы отказов: txt(195).
+	/// </summary>
+	public class AralPlus44RejectParser : RejectParser
+	{
+		public override void Parse(RejectHeader reject, string filename)
+		{
+			if (filename.Contains(".txt"))
+				ParseTXT(reject, filename);
+			else
+			{
+				Logger.WarnFormat("Файл '{0}' не может быть распарсен, так как парсер {1} не умеет парсить данный формат файла", filename, GetType().Name);
+			}
+		}
+
+		/// <summary>
+		/// Парсер для формата файла TXT
+		/// </summary>
+		protected void ParseTXT(RejectHeader reject, string filename)
+		{
+			using (var reader = new StreamReader(File.OpenRead(filename), Encoding.GetEncoding(1251)))
+			{
+				string line;
+				while ((line = reader.ReadLine()) != null)
+				{
+					var rejectLine = new RejectLine();
+					reject.Lines.Add(rejectLine);
+					var splat = line.Split('*');
+					rejectLine.Product = splat[0];
+					var ordered = splat[1].Split('>');
+					rejectLine.Ordered = NullableConvert.ToUInt32(ordered[0].Replace("-", ""));
+					var received = NullableConvert.ToUInt32(ordered[1]);
+					var rejected = rejectLine.Ordered - received;
+					rejectLine.Rejected = rejected != null ? rejected.Value : 0;
+				}
+			}
+		}
+	}
+}
