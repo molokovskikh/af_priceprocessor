39bf2f2d2ab35597a4fbccc101794b43e11f3a10
Екатерина Дудочкина <edudochkina@analit.net>
2015-08-18 12:30:47 +0300

    33351 Разбор отказов Katren3492

---
+++ src/PriceProcessor.Test/Waybills/Rejects/Katren3492RejectParserFixture.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Inforoom.PriceProcessor.Models;
+using Inforoom.PriceProcessor.Waybills.Models;
+using Inforoom.PriceProcessor.Waybills.Rejects.Parser;
+using NHibernate.Linq;
+using NPOI.SS.Formula.Functions;
+using NUnit.Framework;
+using PriceProcessor.Test.TestHelpers;
+using PriceProcessor.Test.Waybills.Rejects.Infrastructure;
+using Test.Support;
+using Test.Support.Suppliers;
+
+namespace PriceProcessor.Test.Waybills.Rejects
+{
+	[TestFixture]
+	class Katren3492RejectParserFixture : BaseRejectFixture
+	{
+		/// <summary>
+		/// Для фотмата TXT
+		/// </summary>
+		[Test]
+		public void Parse()
+		{
+			//Создаем лог, а затем отказ
+			var log = CreateRejectLog("kat1018399-otkaz.txt");
+			var parser = new Katren3492RejectParser();
+			var reject = parser.CreateReject(log);
+
+
+			Assert.That(reject.Lines.Count, Is.EqualTo(1));
+			Assert.That(parser.BadLines.Count, Is.EqualTo(0));
+
+			//Выбираем строку и проверяем правильно ли все распарсилось
+			var line = reject.Lines[0];
+			Assert.That(line.Product, Is.EqualTo("ПИРАЦЕТАМ 0,4 N60 КАПС /АКРИХИН/"));
+			Assert.That(line.Ordered, Is.EqualTo(3));
+			Assert.That(line.Rejected, Is.EqualTo(3));
+		}
+	}
+}
--- src/PriceProcessor/Waybills/Rejects/Parser/Katren3492RejectParser.cs
+++ src/PriceProcessor/Waybills/Rejects/Parser/Katren3492RejectParser.cs
@@ -20,6 +20,52 @@ namespace Inforoom.PriceProcessor.Waybills.Rejects.Parser
 	{
 		public override void Parse(RejectHeader reject, string filename)
 		{
-		}
-	}
+          if (filename.Contains(".txt"))
+                ParseTXT(reject, filename);
+            else
+            {
+                Logger.WarnFormat("Файл '{0}' не может быть распарсен, так как парсер {1} не умеет парсить данный формат файла", filename, GetType().Name);
+            }
+        }
+
+        /// <summary>
+        /// Парсер для формата файла TXT
+        /// </summary>
+        protected void ParseTXT(RejectHeader reject, string filename)
+        {
+            using (var reader = new StreamReader(File.OpenRead(filename), Encoding.GetEncoding(1251)))
+            {
+                string line;
+                var rejectFound = false;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.Contains("Следующие расхождения с заявкой:"))
+                    {
+                        rejectFound = true;
+                        continue;
+                    }
+
+                    if (line == "")
+                        rejectFound = false;
+
+                    if (!rejectFound)
+                        continue;
+
+                    var rejectLine = new RejectLine();
+                    var fields = line.Split(new[] { "; - запрошено" }, StringSplitOptions.None);
+                    var product = fields[0].Replace("Отказ по количеству:", "");
+                    var fields2 = fields[1].Split(',');
+                    var fields3 = fields2[0].Replace("шт.", "").Trim();
+                    var ordered = fields3.Split('.');
+                    var fields4 = fields2[1].Replace("к получению", "").Replace("шт.", "");
+                    var received = fields4.Split('.');
+                    reject.Lines.Add(rejectLine);
+                    rejectLine.Product = product.Trim();
+                    rejectLine.Ordered = NullableConvert.ToUInt32(ordered[0]);
+                    var rejected = NullableConvert.ToUInt32(ordered[0]) - NullableConvert.ToUInt32(received[0]);
+                    rejectLine.Rejected = rejected != null ? rejected.Value : 0;
+                }
+            }
+        }
+    }
 }
