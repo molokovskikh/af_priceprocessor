6187c4f7e6701697fcaf34c712b2b19304b3c6b9
Екатерина Дудочкина <edudochkina@analit.net>
2015-09-01 11:34:58 +0300

    i38265 Формат накладных для Поставщика ПУЛЬС Брянск, Код 7975

---
+++ src/PriceProcessor.Test/Waybills/Parser/Txt/PulsBryanskParserFixture.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using PriceProcessor.Test.TestHelpers;
+
+namespace PriceProcessor.Test.Waybills.Parser.Txt
+{
+    [TestFixture]
+    public class PulsBryanskParserFixture
+    {
+        /// <summary>
+        /// К задаче http://redmine.analit.net/issues/38265
+        /// </summary>
+        [Test]
+        public void Parse()
+        {
+            var doc = WaybillParser.Parse("00248858.sst");
+            Assert.That(doc.ProviderDocumentId, Is.EqualTo("00248858"));
+            Assert.That(doc.DocumentDate, Is.EqualTo(Convert.ToDateTime("28.08.2015")));
+
+            var line = doc.Lines[0];
+            Assert.That(line.Code, Is.EqualTo("07698"));
+            Assert.That(line.Product, Is.EqualTo("Антифлу Кидс пор. д/приг. р-ра д/пероральн. прим. х5"));
+            Assert.That(line.Producer, Is.EqualTo("Contract Pharmacal Corp"));
+            Assert.That(line.Country, Is.EqualTo("США"));
+            Assert.That(line.Quantity, Is.EqualTo(1));
+            Assert.That(line.ProducerCostWithoutNDS, Is.EqualTo(114.45));
+            Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(114.45));
+            Assert.That(line.Nds, Is.EqualTo(10));
+            Assert.That(line.NdsAmount, Is.EqualTo(11m));
+            Assert.That(line.Period, Is.EqualTo("01.10.2017"));
+            Assert.That(line.BillOfEntryNumber, Is.EqualTo("10130032/050515/0003256/1"));
+            Assert.That(line.Certificates, Is.EqualTo("031519^01.10.2017^РОСС US.ФM08.Д61049, РОСС US.ФM08.Д53926^22.04.2015^01.10.2017^^^ООО \"Окружной центр контроля качества\" г. Москва"));
+            Assert.That(line.EAN13, Is.EqualTo("4250369500871"));
+            Assert.That(line.VitallyImportant, Is.EqualTo(false));
+            Assert.That(line.ProducerCost, Is.EqualTo(125.9m));
+        }
+    }
+}
---
+++ src/PriceProcessor/Waybills/Parser/TxtParsers/PulsBryanskParser.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Models;
+using Inforoom.PriceProcessor.Waybills.Parser.TxtParsers;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.SstParsers
+{
+    public class PulsBryanskParser : IDocumentParser
+    {
+        public Document Parse(string file, Document document)
+        {
+            using (var reader = new StreamReader(file, Encoding.GetEncoding(1251)))
+            {
+                string readLine;
+                var startReading = false;
+                while ((readLine = reader.ReadLine()) != null)
+                {
+
+                    if (readLine.Trim() == "[Header]")
+                    {
+                        readLine = reader.ReadLine();
+                        var split = readLine.Split(';');
+                        document.ProviderDocumentId = split[0];
+                        document.DocumentDate = NullableConvert.ToDateTime(split[1]);
+                        startReading = false;
+                    }
+
+                    if (readLine.Trim() == "[Body]")
+                    {
+                        startReading = true;
+                        continue;
+                    }
+
+                    if (!startReading)
+                        continue;
+
+                    var sstParser = readLine.Split(';');
+                        var line = document.NewLine();
+                        line.Code = sstParser[0];
+                        line.Product = sstParser[1];
+                        line.Producer = sstParser[2];
+                        line.Country = sstParser[3];
+                        line.Quantity = (uint?)NullableConvert.ToFloatInvariant(sstParser[4]);
+                        line.ProducerCostWithoutNDS = NullableConvert.ToDecimal(sstParser[6], CultureInfo.InvariantCulture);
+                        line.SupplierCostWithoutNDS = NullableConvert.ToDecimal(sstParser[7], CultureInfo.InvariantCulture);
+                        line.Nds = (uint?)NullableConvert.ToFloatInvariant(sstParser[26]);
+                        line.NdsAmount = (uint?)NullableConvert.ToFloatInvariant(sstParser[28]);
+                        line.BillOfEntryNumber = sstParser[11];
+                        line.Certificates = sstParser[12];
+                        line.EAN13 = sstParser[16];
+                        line.Period = sstParser[15];
+                        line.ProducerCost = NullableConvert.ToDecimal(sstParser[5], CultureInfo.InvariantCulture);
+
+                    if (sstParser[25].Contains("0"))
+                        line.VitallyImportant = false;
+                    else if(sstParser[25].Contains("1"))
+                        line.VitallyImportant = true;
+                }
+            }
+            return document;
+        }
+
+
+
+        public static bool CheckFileFormat(string file)
+        {
+            using (var reader = new StreamReader(file, Encoding.GetEncoding(1251)))
+            {
+                var header = reader.ReadLine().Split(';');
+                if (header.Length != 20)
+                    return false;
+                var headerCaption = reader.ReadLine();
+                if (!headerCaption.ToLower().Equals("[header]"))
+                    return false;
+                var header2 = reader.ReadLine().Split(';');
+                if (header2.Length != 19)
+                    return false;
+                var header3 = reader.ReadLine().Split(';');
+                if (header3.Length != 32)
+                    return false;
+                var bodyCaption = reader.ReadLine();
+                if (!bodyCaption.ToLower().Equals("[body]"))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
