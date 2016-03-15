c07e96ab626fa88918b316aaa7749d130235804e
Максименко Герман <g.maksimenko@analit.net>
2016-03-15 18:54:23 +0300

    i48147 Парсер ФармХимКомплект

---
+++ src/PriceProcessor.Test/Waybills/Parser/Xls/PharmChemComplect11261ParserFixture.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using PriceProcessor.Test.TestHelpers;
+
+namespace PriceProcessor.Test.Waybills.Parser.Xls
+{
+	[TestFixture]
+	public class PharmChemComplect11261ParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var document = WaybillParser.Parse("11261.xls");
+			Assert.That(document.ProviderDocumentId, Is.EqualTo("1"));
+			Assert.That(document.DocumentDate, Is.EqualTo(DateTime.Parse("01.01.2016")));
+
+			Assert.That(document.Lines.Count, Is.EqualTo(2));
+			var line = document.Lines[0];
+
+			Assert.That(line.Amount, Is.EqualTo(108900));
+			Assert.That(line.CertificateAuthority, Is.EqualTo("ОГБУЗ Центр Контроля качества и сертификации лек. Средств Костромской области Испытательная контрольно-аналитическая лаборатория"));
+			Assert.That(line.Certificates, Is.EqualTo("1168"));
+			Assert.That(line.CertificatesDate, Is.EqualTo("30.10.2015"));
+			Assert.That(line.CertificatesEndDate, Is.EqualTo(DateTime.Parse("01.07.2019")));
+			Assert.That(line.Nds, Is.EqualTo(10));
+			Assert.That(line.NdsAmount, Is.EqualTo(9900));
+			Assert.That(line.Period, Is.EqualTo("01.04.2020"));
+			Assert.That(line.Product, Is.EqualTo("Магния сульфат д/ин."));
+			Assert.That(line.Quantity, Is.EqualTo(50));
+			Assert.That(line.SerialNumber, Is.EqualTo("73042015"));
+			Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(1980));
+			Assert.That(line.VitallyImportant, Is.EqualTo(false));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/XlsParsers/PharmChemComplect11261XlsParser.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExcelLibrary.BinaryFileFormat;
+using ExcelLibrary.SpreadSheet;
+using Inforoom.PriceProcessor.Waybills.Models;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.XlsParsers
+{
+	public class PharmChemComplect11261XlsParser : IDocumentParser
+	{
+		public Document Parse(string file, Document document)
+		{
+			StringDecoder.DefaultEncoding = Encoding.GetEncoding(1251);
+			var book = Workbook.Load(file);
+			var sheet = book.Worksheets[0];
+
+			document.ProviderDocumentId = sheet.Cells[1, 6].StringValue;
+			document.DocumentDate = sheet.Cells[1, 7].TryToGetValueAsDateTime();
+
+			foreach (var row in sheet.Cells.Rows.Values.Skip(3)) {
+				if ((String.IsNullOrEmpty(row.GetCell(0).StringValue) || (row.GetCell(0).StringValue.Equals("#NULL!"))) &&
+					(String.IsNullOrEmpty(row.GetCell(8).StringValue) || (row.GetCell(8).StringValue.Equals("#NULL!"))) &&
+					(String.IsNullOrEmpty(row.GetCell(9).StringValue) || (row.GetCell(9).StringValue.Equals("#NULL!"))))
+					return document;
+
+				var line = document.NewLine();
+
+				line.Amount = Convert.ToDecimal(row.GetCell(13).Value);					//13 Сумма с НДС
+				line.CertificateAuthority = row.GetCell(6).StringValue;					//6 Орган, выдавший документа качества
+				line.Certificates = row.GetCell(7).StringValue;									//7 Информация о сертификате это строка что то вроде РОСС.NL.ФМ09.Д00778
+				line.CertificatesDate = DateTimeFormat(row.GetCell(4));					//4 Дата выдачи сертификата сертификата
+				line.CertificatesEndDate = row.GetCell(5).TryToGetValueAsDateTime(); //5 Срок действия сертификата, дата окончания
+				line.Nds = Convert.ToUInt32(100 * Convert.ToDecimal(row.GetCell(11).Value));  //11 Ставка налога на добавленную стоимость
+				line.NdsAmount = Convert.ToDecimal(row.GetCell(12).Value);			//12 Сумма НДС
+				line.Period = DateTimeFormat(row.GetCell(3));										//3 Срок годности.А точнее Дата окончания срока годности.
+				line.Product = row.GetCell(0).StringValue.Trim();								//0 Наименование продукта
+				line.Quantity	= Convert.ToUInt32(row.GetCell(8).Value);         //8 Количество
+				line.SerialNumber = row.GetCell(2).StringValue;									//2 Серийный номер продукта
+				line.SupplierCostWithoutNDS = Convert.ToDecimal(row.GetCell(9).Value); //9 Цена поставщика без НДС
+				line.VitallyImportant = row.GetCell(1).StringValue.Contains("да"); //1 Признак ЖНВЛС
+			}
+			return document;
+		}
+
+		public static bool CheckFileFormat(string file)
+		{
+			StringDecoder.DefaultEncoding = Encoding.GetEncoding(1251);
+			var workbook = Workbook.Load(file);
+			var sheet = workbook.Worksheets[0];
+			return (sheet.Cells[4, 1].StringValue.ToLower().Equals("ж/в признак")) &&
+				(sheet.Cells[4, 7].StringValue.ToLower().Equals("сертификат товара")) &&
+				(sheet.Cells[4, 11].StringValue.ToLower().Equals("ндс"));
+		}
+
+		private string DateTimeFormat(Cell c)
+		{
+			var d = c.TryToGetValueAsDateTime();
+			if (d.HasValue)
+				return d.Value.ToShortDateString();
+			else {
+				return "";
+			}
+		}
+
+	}
+}
