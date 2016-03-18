b930dff941389fcb0edf5a4cb54f14b456d34ca9
Максименко Герман <g.maksimenko@analit.net>
2016-03-18 14:54:10 +0300

    i48261 BeautyLife18663Parser

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/BeautyLife18663ParserFixture.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Parser.DbfParsers;
+using NUnit.Framework;
+using PriceProcessor.Test.TestHelpers;
+
+namespace PriceProcessor.Test.Waybills.Parser
+{
+	[TestFixture]
+	public class BeautyLife18663ParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var document = WaybillParser.Parse("2178.DBF");
+
+			Assert.That(document.ProviderDocumentId, Is.EqualTo("00002178"));
+			Assert.That(document.DocumentDate, Is.EqualTo(DateTime.Parse("15.03.2016")));
+			Assert.That(document.Invoice.BuyerName, Is.EqualTo("ГУП РМЭ \"Аптека №9\" (Новый Торъял)"));
+			Assert.That(document.Lines.Count, Is.EqualTo(51));
+
+			var line = document.Lines[0];
+			Assert.That(line.Quantity, Is.EqualTo(2));
+			Assert.That(line.Code, Is.EqualTo("861823"));
+			Assert.That(line.SupplierCost, Is.EqualTo(43.86));
+			Assert.That(line.NdsAmount, Is.EqualTo(13.38));
+			Assert.That(line.Country, Is.EqualTo("Германия"));
+			Assert.That(line.Certificates, Is.EqualTo("РОСС.DE.ПК12.В02601 до 26.09.2016"));
+			Assert.That(line.Amount, Is.EqualTo(87.72));
+			Assert.That(line.Nds, Is.EqualTo(18));
+			Assert.That(line.Product, Is.EqualTo("Натурелла прокладки ежедн. нормал 20 шт"));
+			Assert.That(line.Producer, Is.EqualTo("Always"));
+		}
+
+		[Test]
+		public void Check_file_format()
+		{
+			Assert.IsTrue(BeautyLife18663Parser.CheckFileFormat(Dbf.Load(@"..\..\Data\Waybills\2178.DBF")));
+		}
+
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/BeautyLife18663Parser.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Models;
+using System.Text.RegularExpressions;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class BeautyLife18663Parser : IDocumentParser
+	{
+		protected Encoding Encoding = Encoding.GetEncoding(866);
+
+		protected Regex re = new Regex(@"(?<producer>.+)\s/\s(?<product>.+)");
+
+		public Document Parse(string file, Document document)
+		{
+			var data = Dbf.Load(file, Encoding);
+
+			if (document.Invoice == null)
+				document.SetInvoice();
+
+			document.Lines = data.Rows.Cast<DataRow>().Select(r => {
+				document.ProviderDocumentId = r["NOMNAKL"].ToString();
+				if (!Convert.IsDBNull(r["DATANAKL"]))
+					document.DocumentDate = Convert.ToDateTime(r["DATANAKL"]);
+				document.Invoice.BuyerName = r["KONTR"].ToString();
+
+				var line = document.NewLine();
+				line.Quantity = Convert.ToUInt32(r["KOL"]);
+				line.Code = r["KOD1C"].ToString();
+				line.SupplierCost = Convert.ToDecimal(r["PRICE"], CultureInfo.InvariantCulture);
+				line.NdsAmount = Convert.ToDecimal(r["NDS"], CultureInfo.InvariantCulture);
+				line.Country = r["PROIZV"].ToString();
+				line.Certificates = r["SERT"].ToString();
+				line.Amount = Convert.ToDecimal(r["SUMMMA"], CultureInfo.InvariantCulture);
+				line.SetNds(Convert.ToDecimal(r["STNDS"], CultureInfo.InvariantCulture));
+
+				// NAME - Производитель " / " Название товара (первый слеш с пробелами с двух сторон является разделяющим символом)
+				line.Product = re.Match(r["NAME"].ToString()).Groups["product"].Value;
+				line.Producer = re.Match(r["NAME"].ToString()).Groups["producer"].Value;
+
+				return line;
+			}).ToList();
+			return document;
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("SUMMMA") &&
+				data.Columns.Contains("NOMNAKL") &&
+				data.Columns.Contains("BNDS") &&
+				data.Columns.Contains("DATANAKL");
+		}
+	}
+}
