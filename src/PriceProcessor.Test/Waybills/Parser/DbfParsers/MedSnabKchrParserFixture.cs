07b907ceac4ec0c8a9bb73d6cf4735545cde2407
Борисов Михаил Юрьевич <michaelborisovha@gmail.com>
2016-10-23 00:43:34 +0300

    Новшество #55941
    Есть проблемы с кодировкой

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/MedSnabKchrParserFixture.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using Inforoom.PriceProcessor.Waybills;
+using NUnit.Framework;
+using PriceProcessor.Test.TestHelpers;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	internal class MedSnabKchrParserFixture
+	{
+		/// <summary>
+		/// Тест для задачи http://redmine.analit.net/issues/55941
+		/// </summary>
+		[Test]
+		public void Parse()
+		{
+			var document = WaybillParser.Parse("66426.dbf");
+			Assert.That(document.Parser, Is.EqualTo("MedSnabKchrParser"));
+			Assert.That(document.ProviderDocumentId.Trim(), Is.EqualTo("66426"));
+			Assert.That(document.DocumentDate, Is.EqualTo(new DateTime(2016, 09, 27)));
+
+			var detector = new WaybillFormatDetector();
+			var parsers = detector.GetSuitableParsers(@"..\..\Data\Waybills\66426.dbf", null).ToList();
+			Assert.That(parsers.ToList().Count, Is.EqualTo(1));
+
+			var line0 = document.Lines[0];
+			Assert.That(line0.Code, Is.EqualTo("18036.0000"));
+			Assert.That(line0.Product, Is.EqualTo("╟юфръ ъряыш 10ьу/ьы 20ьы")); //!!!!!!!
+			Assert.That(line0.EAN13, Is.EqualTo("8594739055209"));
+			Assert.That(line0.Quantity, Is.EqualTo(2.0000));
+			Assert.That(line0.Producer, Is.EqualTo("╟хэЄштр/╦хўштр")); //!!!!!!!
+			Assert.That(line0.SupplierCost, Is.EqualTo(171.1700));
+			Assert.That(line0.SupplierCostWithoutNDS, Is.EqualTo(155.6100));
+			Assert.That(line0.VitallyImportant, Is.True);
+			Assert.That(line0.BillOfEntryNumber, Is.EqualTo("10113100/190416/0019473/5"));
+			Assert.That(line0.Certificates, Is.EqualTo("╨╬╤╤ CZ.╘╠08.─21991")); //!!!!!!!
+			Assert.That(line0.CertificatesEndDate, Is.EqualTo(new DateTime(2019, 02, 28)));
+			Assert.That(line0.SerialNumber, Is.EqualTo("3130316"));
+
+			var invoice = document.Invoice;
+			Assert.That(invoice.InvoiceNumber, Is.EqualTo("170560.0000"));
+			Assert.That(invoice.RecipientName, Is.Null);
+			Assert.That(invoice.InvoiceDate, Is.Null);
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/MedSnabKchrParser.cs
@@ -0,0 +1,51 @@
+﻿using System.Data;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class MedSnabKchrParser : BaseDbfParser
+	{
+		public override DbfParser GetParser()
+		{
+			return new DbfParser()
+				.DocumentHeader(h => h.ProviderDocumentId, "TTN")
+				.DocumentHeader(h => h.DocumentDate, "TTN_DATE")
+				.Line(l => l.Code, "SP_PRD_IDV")
+				.Line(l => l.Product, "NAME_POST")
+				.Line(l => l.EAN13, "SCAN_CODE")
+				.Line(l => l.Quantity, "KOL_VO")
+				.Line(l => l.Producer, "PRZV_POST")
+				.Line(l => l.Period, "SGODN")
+				.Line(l => l.SupplierCost, "PR_MAK_NDS")
+				.Line(l => l.SupplierCostWithoutNDS, "PR_MAK")
+				.Line(l => l.VitallyImportant, "ZV")
+				.Line(l => l.BillOfEntryNumber, "GTD")
+				.Line(l => l.Certificates, "SERT")
+				.Line(l => l.CertificatesEndDate, "SERT_DATE")
+				.Line(l => l.CertificateAuthority, "SERT_AUTH")
+				.Line(l => l.SerialNumber, "SERIA")
+
+				.Invoice(i => i.InvoiceNumber, "head_id")
+				.Invoice(i => i.RecipientName, "apt_af");
+			//.Invoice(i => i.InvoiceDate, "BILLDT");
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			var codeIndex = data.Columns.Contains("SP_PRD_IDV");
+			var productIndex = data.Columns.Contains("NAME_POST");
+			var supplierCostIndex = data.Columns.Contains("PR_MAK_NDS");
+			var supplierCostWithoutNdsIndex = data.Columns.Contains("PR_MAK");
+			var quantity = data.Columns.Contains("KOL_VO");
+
+			if (codeIndex && productIndex &&
+			    supplierCostIndex && supplierCostWithoutNdsIndex &&
+			    quantity)
+			{
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
+
