fddcc3d9818b72ae2fdf6d37fc3e8d0636218ec7
Борисов Михаил Юрьевич <michaelborisovha@gmail.com>
2016-11-09 23:28:02 +0300

    Новшество #56357
    Проблемы с кодировкой

---
+++ src/PriceProcessor.Test/Waybills/Parser/DbfParsers/AlenaParserFixture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using PriceProcessor.Test.TestHelpers;
+
+namespace PriceProcessor.Test.Waybills.Parser.DbfParsers
+{
+	[TestFixture]
+	class AlenaParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			/*
+			 * http://redmine.analit.net/issues/56357
+			 */
+			var doc = WaybillParser.Parse("Т-000824.dbf");
+			Assert.That(doc.ProviderDocumentId, Is.EqualTo("БУУТ-000824"));
+			Assert.That(doc.DocumentDate, Is.EqualTo(new DateTime(2016, 11, 02)));
+
+			var line = doc.Lines[0];
+			Assert.That(line.Code, Is.EqualTo("00016466"));
+			Assert.That(line.Product, Is.EqualTo("Нутрилак 0-6 с преб. м/с 350г /уп. 24шт."));
+			Assert.That(line.Quantity, Is.EqualTo(12));
+			Assert.That(line.SupplierCost, Is.EqualTo(270.89));
+			Assert.That(line.SupplierCostWithoutNDS, Is.EqualTo(246.26));
+			Assert.That(line.Nds, Is.EqualTo(10));
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/AlenaParser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Tools;
+using Inforoom.PriceProcessor.Waybills.Models;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
+{
+	public class AlenaParser : IDocumentParser
+	{
+		protected Encoding Encoding = Encoding.GetEncoding(1251);
+		//protected Encoding Encoding = Encoding.GetEncoding(866);
+
+		public Document Parse(string file, Document document)
+		{
+			var data = Dbf.Load(file, Encoding);
+
+			new DbfParser()
+				.DocumentHeader(h => h.ProviderDocumentId, "N_NACL") 
+				.DocumentHeader(h => h.DocumentDate, "D_NACL") 
+				.Line(l => l.Code, "CODE") 
+				.Line(l => l.Product, "GOOD") 
+				.Line(l => l.SupplierCost, "PRICE_SUP") 
+				.Line(l => l.SupplierCostWithoutNDS, "PRICE_NNDS") 
+				.Line(l => l.NdsAmount, "NDS")
+				.Line(l => l.Quantity, "QUANT")
+				.ToDocument(document, data);
+
+			return document;
+		}
+
+		public static bool CheckFileFormat(DataTable data)
+		{
+			return data.Columns.Contains("N_NACL") &&
+				data.Columns.Contains("D_NACL") &&
+				data.Columns.Contains("CODE") &&
+				data.Columns.Contains("GOOD") &&
+				data.Columns.Contains("PRICE_SUP") &&
+				data.Columns.Contains("PRICE_NNDS") &&
+				data.Columns.Contains("QUANT");
+		}
+	}
+}
