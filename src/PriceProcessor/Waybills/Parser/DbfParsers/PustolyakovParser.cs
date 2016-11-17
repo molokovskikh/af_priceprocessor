d86af5db3df17e92975167b335e67d5474a98a98
Борисов Михаил Юрьевич <michaelborisovha@gmail.com>
2016-11-17 23:11:12 +0300

    Ошибка #56873

--- src/PriceProcessor/Waybills/Parser/DbfParsers/PustolyakovParser.cs
+++ src/PriceProcessor/Waybills/Parser/DbfParsers/PustolyakovParser.cs
@@ -1,8 +1,8 @@
-using System.Data;
+﻿using System.Data;
 
 namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
 {
-	public class Flagrans19777Parser : BaseDbfParser
+	public class PustolyakovParser : BaseDbfParser
 	{
 		public override DbfParser GetParser()
 		{
@@ -15,22 +15,23 @@ namespace Inforoom.PriceProcessor.Waybills.Parser.DbfParsers
 				.Line(l => l.Quantity, "QUANT")
 				.Line(l => l.Nds, "NDS")
 				.Line(l => l.NdsAmount, "NDS_SUM")
-				.Line(l => l.SupplierCost, "PRICE_S_N", "PR_SUPWN")
-				.Line(l => l.SupplierCostWithoutNDS, "PRICE", "PRICE_SUP")
+				.Line(l => l.SupplierCost, "PR_SUPWN")
+				.Line(l => l.SupplierCostWithoutNDS, "PRICE_SUP")
 				.Line(l => l.Amount, "SUMMA_N")
 				.Line(l => l.VitallyImportant, "GNVLS")
 				.Line(l => l.RegistryCost, "PRICE_REE")
-				.Line(l => l.Certificates, "SERIAL")
+				.Line(l => l.Certificates, "SERT")
 				.Line(l => l.Period, "DATEB")
 				.Line(l => l.Producer, "PRODUCER")
-				.Line(l => l.Country, "COUNTRY");
+				.Line(l => l.Country, "COUNTRY")
+				.Line(l => l.EAN13, "BARCODE");
 		}
 
 		public static bool CheckFileFormat(DataTable table)
 		{
 			return table.Columns.Contains("N_NACL") && table.Columns.Contains("D_NACL")
 				&& (table.Columns.Contains("PRICE_S_N") || table.Columns.Contains("PR_SUPWN"))
-				&& !table.Columns.Contains("BARCODE") && !table.Columns.Contains("SERT"); ;
+				&& table.Columns.Contains("BARCODE") && table.Columns.Contains("SERT");
 		}
 	}
 }
