00700b064f96cf492b603e2e2dc2bb41a70301b4
Борисов Михаил Юрьевич <michaelborisovha@gmail.com>
2017-04-27 14:45:49 +0300

    Ошибка #79829

---
+++ src/PriceProcessor.Test/Waybills/Parser/Txt/MedlineShustovaParserFixture.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using PriceProcessor.Test.TestHelpers;
+
+namespace PriceProcessor.Test.Waybills.Parser.Txt
+{
+	[TestFixture]
+	class MedlineShustovaParserFixture
+	{
+		[Test]
+		public void Parse()
+		{
+			var doc = WaybillParser.Parse(@"ЗЗН5036023 (1).TXT");
+			Assert.That(doc.Lines.Count, Is.EqualTo(1));
+
+			Assert.That(doc.ProviderDocumentId, Is.EqualTo("17282"));
+			Assert.That(doc.DocumentDate, Is.EqualTo(Convert.ToDateTime("26.04.2017")));
+
+			Assert.That(doc.Invoice.Amount, Is.EqualTo(1784.00));
+			Assert.That(doc.Invoice.SellerINN, Is.EqualTo("3665012499"));
+
+			Assert.That(doc.Lines[0].Code, Is.EqualTo("566017"));
+			Assert.That(doc.Lines[0].Product, Is.EqualTo("Molimed Classic maxi арт.168587~прокладки д/больных недерж.мочи пак.ПЭ 28~ООО \"Пауль Хартманн\" РОССИЯ"));
+			Assert.That(doc.Lines[0].VitallyImportant, Is.EqualTo(false));
+			Assert.That(doc.Lines[0].SerialNumber, Is.EqualTo("600101350"));
+			Assert.That(doc.Lines[0].Period, Is.EqualTo("04.06.2021"));
+			Assert.That(doc.Lines[0].Certificates, Is.EqualTo("РОСС RU ИМ41 Д07095"));
+			Assert.That(doc.Lines[0].CertificatesEndDate, Is.EqualTo(Convert.ToDateTime("26.09.2019")));
+			Assert.That(doc.Lines[0].CertificateAuthority, Is.EqualTo("Орган по сертификации продукции ООО \"Центр сертификации и декларирования\""));
+			Assert.That(doc.Lines[0].Nds, Is.EqualTo(10));
+
+			Assert.That(doc.Lines[0].ProducerCostWithoutNDS, Is.EqualTo(303.80));
+			Assert.That(doc.Lines[0].Amount, Is.EqualTo(1784.00));
+
+		}
+	}
+}
---
+++ src/PriceProcessor/Waybills/Parser/TxtParsers/MedlineShustovaParser.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Inforoom.PriceProcessor.Waybills.Models;
+
+namespace Inforoom.PriceProcessor.Waybills.Parser.TxtParsers
+{
+	public class MedlineShustovaParser : BaseIndexingParser
+	{
+		protected override void SetIndexes()
+		{
+			SellerINNIndex = 0;
+			ProviderDocumentIdIndex = 1;
+			DocumentDateIndex = 2;
+			InvoiceAmountIndex = 3;
+			InvoiceNumberIndex = 6;
+			BuyerAddressIndex = 7;
+			
+			CodeIndex = 0;
+			ProductIndex = 2;
+			AmountIndex = 5;
+			NdsIndex = 6;
+			SerialNumberIndex = 7;
+			PeriodIndex = 8;
+			CertificatesIndex = 10;
+			CertificateAuthorityIndex = 11;
+			CertificatesEndDateIndex = 12;
+			VitallyImportantIndex = 13;
+			ProducerCostWithoutNdsIndex = 14;
+		}
+
+		public override Document Parse(string file, Document document)
+		{
+			SetIndexes();
+			using (var reader = new StreamReader(file, Encoding.GetEncoding(1251)))
+			{
+				var headers = reader.ReadLine().Split('|');
+
+				if (document.Invoice == null)
+					document.SetInvoice();
+				document.ProviderDocumentId = GetString(headers[ProviderDocumentIdIndex]);
+				document.DocumentDate = GetDateTime(headers[DocumentDateIndex]);
+
+				document.Invoice.Amount = GetDecimal(headers[InvoiceAmountIndex]);
+				document.Invoice.InvoiceNumber = GetString(headers[InvoiceNumberIndex]);
+				document.Invoice.BuyerAddress = GetString(headers[BuyerAddressIndex]);
+				document.Invoice.SellerINN = GetString(headers[SellerINNIndex]);
+
+				string line;
+				while ((line = reader.ReadLine()) != null)
+				{
+					//пустые строки игнорируются (последняя в примере была пустой)
+					if (string.IsNullOrEmpty(line) || string.IsNullOrWhiteSpace(line))
+						continue;
+
+					var parts = line.Split('|');
+					var docLine = document.NewLine();
+					docLine.Code = GetString(parts[CodeIndex]);
+					docLine.Product = GetString(parts[ProductIndex]);
+					docLine.VitallyImportant = GetBool(parts[VitallyImportantIndex]);
+					docLine.SerialNumber = GetString(parts[SerialNumberIndex]);
+					docLine.Period = GetString(parts[PeriodIndex]);
+					docLine.Certificates = GetString(parts[CertificatesIndex]);
+					docLine.CertificatesEndDate = GetDateTime(parts[CertificatesEndDateIndex]);
+					docLine.CertificateAuthority = GetString(parts[CertificateAuthorityIndex]);
+					docLine.Nds = (uint?) GetInteger(parts[NdsIndex]);
+					docLine.ProducerCostWithoutNDS = GetDecimal(parts[ProducerCostWithoutNdsIndex]);
+					docLine.Amount = GetDecimal(parts[AmountIndex]);
+				}
+			}
+			return document;
+		}
+
+		public static bool CheckFileFormat(string file)
+		{
+			using (var reader = new StreamReader(file, Encoding.GetEncoding(1251)))
+			{
+				var headers = reader.ReadLine().Split('|');
+				if (headers.Length != 8)
+					return false;
+
+				DateTime dt;
+				if (!DateTime.TryParse(headers[2], out dt))
+					return false;
+
+				string line = reader.ReadLine();
+				var lines = line.Split('|');
+				if (lines.Length != 15)
+					return false;
+				if (!DateTime.TryParse(lines[8], out dt))
+					return false;
+				return true;
+			}
+		}
+	}
+}
