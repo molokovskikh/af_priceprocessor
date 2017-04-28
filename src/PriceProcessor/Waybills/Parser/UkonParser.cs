39aa9067d189cf1ca33d419c711936fd568e065f
Борисов Михаил Юрьевич <michaelborisovha@gmail.com>
2017-04-28 11:12:45 +0300

    Revert "Ошибка #69027" Изменения приводили к неправильной обработке, задача закрыта за ненадобностью This reverts commit 12c1a917205da34d8733443eadc28b2dbdedd37e.

--- src/PriceProcessor/Waybills/Parser/UkonParser.cs
+++ src/PriceProcessor/Waybills/Parser/UkonParser.cs
@@ -222,8 +222,7 @@ namespace Inforoom.PriceProcessor.Waybills.Parser.SstParsers
 				// http://redmine.analit.net/issues/53074
 				if (header.Length == 9 && body.Length == 22) {
 					var s = "";
-					var startIndexOfEmptyCells = 4; // индекс ячейки, скоторой и далее все ячейки пустые 
-					for (int i = startIndexOfEmptyCells; i < 9; i++)
+					for (int i = 3; i < 9; i++)
 						s += header[i];
 					if (s.Length == 0)
 						return false;
