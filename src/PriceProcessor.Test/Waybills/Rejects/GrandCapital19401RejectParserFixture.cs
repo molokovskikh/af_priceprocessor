b4032c09372d8b3853752b402ebd458b261ee7e8
ayakimenko <138@analit.net>
2017-05-02 11:22:09 +0300

    i79671 Парсер отказа 'GrandCapital19401RejectParser'

---
+++ src/PriceProcessor.Test/Waybills/Rejects/GrandCapital19401RejectParserFixture.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Inforoom.PriceProcessor.Models;
+using Inforoom.PriceProcessor.Waybills.Models;
+using Inforoom.PriceProcessor.Waybills.Rejects.Parser;
+using NHibernate.Linq;
+using NPOI.SS.Formula.Functions;
+using NUnit.Framework;
+using PriceProcessor.Test.TestHelpers;
+using PriceProcessor.Test.Waybills.Rejects.Infrastructure;
+using Test.Support;
+using Test.Support.Suppliers;
+
+namespace PriceProcessor.Test.Waybills.Rejects
+{
+	[TestFixture]
+	class GrandCapital19401RejectParserFixture : BaseRejectFixture
+	{
+		/// <summary>
+		/// Тест к задаче http://redmine.analit.net/issues/79671
+		/// Для формата txt
+		/// </summary>
+		[Test]
+		public void Parse()
+		{
+			//Создаем лог, а затем отказ
+			var log = CreateRejectLog("i79671-6-001411_97214065.txt");
+			var parser = new GrandCapital19401RejectParser();
+			var reject = parser.CreateReject(log);
+
+			//Проверяем правильность парсинга
+			Assert.That(reject.Lines.Count, Is.EqualTo(1));
+			Assert.That(parser.BadLines.Count, Is.EqualTo(0));
+
+			//Выбираем строку и проверяем правильно ли все распарсилось
+			var line = reject.Lines[0];
+			Assert.That(line.Code, Is.EqualTo("014567"));
+			Assert.That(line.Product, Is.EqualTo("Флуконазол капс. 150мг №1"));
+			Assert.That(line.Rejected, Is.EqualTo(20));
+		}
+	}
+}
